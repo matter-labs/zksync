ac6a28a427c92b8c5e7beea6c0ea3f7ba29435c5
Alex <gluk64@gmail.com>
2018-11-23 10:08:13 +0800

    verifier contract generated, everything works

--- bellman-demo/README.md
+++ bellman-demo/README.md
@@ -1,7 +1,25 @@
-# Demo circuit
+# Demo circuits
 
-Usage:
+This project contains usage demonstration for `bellman` zkSNARK proving framework.  
+We use elliptic curve BN256, for which pairings can be efficiently performed in Ethereum Virtual Machine.
+
+## Project structure
+
+- `examples/xor.rs`: simple XOR circuit
+- `examples/sha256.rs`: sha256 preimage (using gadgets from `sapling-crypto` crate)
+
+## Usage:
 
 ```$bash
-cargo run
+cargo run --example xor
+cargo run --example sha256
+
 ```
+
+## Verification in EVM contract:
+
+```$bash
+cargo run --example sha256 > demo.sol
+```
+
+Now deploy `DemoVerifier` contract from `demo.sol` (e.g. in [remix](https://remix.ethereum.org)) and run method `verify()`.
--- bellman-demo/examples/xor.rs
+++ bellman-demo/examples/xor.rs
@@ -1,17 +1,13 @@
-// https://github.com/ebfull/bellman-demo
-
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 extern crate bellman;
 extern crate pairing;
 extern crate rand;
-
-#[macro_use]
 extern crate ff;
+extern crate bellman_demo;
 
 use bellman::{Circuit, ConstraintSystem, SynthesisError};
 use ff::{Field, PrimeField};
 use pairing::{Engine};
+use pairing::bn256::{Bn256, Fr};
 
 trait OptionExt<T> {
     fn grab(&self) -> Result<T, SynthesisError>;
@@ -23,17 +19,20 @@ impl<T: Copy> OptionExt<T> for Option<T> {
     }
 }
 
-struct DemoCircuit<E: Engine> {
+struct XorCircuit<E: Engine> {
     a: Option<E::Fr>,
     b: Option<E::Fr>,
     c: Option<E::Fr>,
 }
 
-// Implementation of our circuit.
-impl<E: Engine> Circuit<E> for DemoCircuit<E> {
+// Implementation of our circuit:
+// Given a bit `c`, prove that we know bits `a` and `b` such that `c = a xor b`
+impl<E: Engine> Circuit<E> for XorCircuit<E> {
     fn synthesize<CS: ConstraintSystem<E>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
-        // variables: a, b
+
         // public input: c
+        // variables (witness): a, b
+
         // constraint system:
         // a * a = a
         // b * b = b
@@ -59,36 +58,33 @@ impl<E: Engine> Circuit<E> for DemoCircuit<E> {
             |lc| lc + b,
             |lc| lc + a + b - c,
         );
-
         Ok(())
     }
 }
 
 // Create some parameters, create a proof, and verify the proof.
 fn main() {
-    use pairing::bls12_381::{Bls12, Fr};
     use rand::thread_rng;
-    use std::marker::PhantomData;
 
     use bellman::groth16::{
-        create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof, Proof,
+        create_random_proof, generate_random_parameters, prepare_verifying_key, verify_proof
     };
 
     let rng = &mut thread_rng();
 
     let params = {
-        let c = DemoCircuit::<Bls12> {
+        let c = XorCircuit::<Bn256> {
             a: None,
             b: None,
             c: None,
         };
-
         generate_random_parameters(c, rng).unwrap()
     };
 
     let pvk = prepare_verifying_key(&params.vk);
 
-    let c = DemoCircuit {
+    // here we allocate actual variables
+    let c = XorCircuit {
         a: Some(Fr::one()),
         b: Some(Fr::zero()),
         c: Some(Fr::one()),
@@ -97,7 +93,20 @@ fn main() {
     // Create a groth16 proof with our parameters.
     let proof = create_random_proof(c, &params, rng).unwrap();
 
-    assert!(verify_proof(&pvk, &proof, &[Fr::one()]).unwrap());
+    // `inputs` slice contains public parameters encoded as field elements Fr
 
-    println!("success!");
+    // incorrect input tests
+    let inputs = &[Fr::from_str("5").unwrap()];
+    let success = verify_proof(&pvk, &proof, inputs).unwrap();
+    assert!(!success); // fails, because 5 is not 1 or 0
+
+    let inputs = &[Fr::zero()];
+    let success = verify_proof(&pvk, &proof, inputs).unwrap();
+    assert!(!success); // fails because 0 != 0 xor 1
+
+    // correct input test
+    let inputs = &[Fr::one()];
+    let success = verify_proof(&pvk, &proof, inputs).unwrap();
+    assert!(success);
+    println!("{}", bellman_demo::generate_demo_contract(&params.vk, &proof, inputs));
 }
