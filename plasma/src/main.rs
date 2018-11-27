103a9dd88d370ad957140bf9ef3e3da336eeb17d
Alex <gluk64@gmail.com>
2018-11-27 20:13:07 +0200

    test circuit removed

--- plasma/src/main.rs
+++ plasma/src/main.rs
@@ -10,11 +10,9 @@ extern crate sapling_crypto;
 
 mod plasma_state;
 mod sparse_merkle_tree;
-mod update_circuit;
 
 use sparse_merkle_tree::SparseMerkleTree;
 
 fn main() {
     //let plasma_params = plasma_state::PlasmaBN256{};
-    update_circuit::test_circuit();
 }
