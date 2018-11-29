944c2827b1642517e92ebef85f850aaaf5123369
Alex <gluk64@gmail.com>
2018-11-29 11:16:00 +0200

    more

--- plasma/contracts/contracts/Plasma.sol
+++ plasma/contracts/contracts/Plasma.sol
@@ -2,24 +2,31 @@ pragma solidity ^0.5.0;
 
 contract Plasma {
 
+    // For testing purposes: only single operator may submit new blocks
+    address public operator;
+
+    constructor() public {
+        operator = msg.sender;
+    }
+
+    // Public API
+
+    // Deposit ERC20 tokens
+    function deposit(address /*_from*/, uint /*_amount*/) public {
+
+    }
+
+
+
+    // Implementation
+
     enum Circuit {
         DEPOSIT,
         UPDATE,
         WITHDRAWAL
     }
 
-    // for testing purposes only owner may submit proofs
-    address public owner;
-
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    modifier restricted() {
-        if (msg.sender == owner) _;
-    }
-
-    // Verifier stub
+    // Verifier stub: this part with hardcoded keys will be generated from Groth16 CRS
     function verify(Circuit /*circuit*/, uint256[8] memory /*proof*/, uint256[] memory /*inputs*/)
         internal pure returns (bool valid)
     {
