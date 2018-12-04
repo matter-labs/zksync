1b346989b74f2b9955afe374fe10c9b6322da1ad
Alex <gluk64@gmail.com>
2018-12-05 01:02:50 +0800

    server stub

--- plasma/src/server/server.rs
+++ plasma/src/server/server.rs
@@ -4,24 +4,32 @@ use pairing::bn256::{Bn256, Fr};
 use sapling_crypto::alt_babyjubjub::{JubjubEngine};
 
 use super::plasma_state;
+use super::super::circuit::plasma_constants;
+
 use super::super::sparse_merkle_tree::{parallel_smt, pedersen_hasher::PedersenHasher};
+use super::super::eth;
 
 type Account = plasma_state::Account<Bn256>;
 type Block = plasma_state::Block<Bn256>;
 
 type ParallelBalanceTree = parallel_smt::SparseMerkleTree<Account, Fr, PedersenHasher<Bn256>>;
 
-/// Coordinate tx processing and generation of proofs
+/// Coordinator of tx processing and generation of proofs
 pub struct PlasmaServer {
 
     /// Accounts stored in a sparse Merkle tree
     balance_tree: ParallelBalanceTree,
 
     /// Current block number
-    block_number:   u32,
+    block_number: u32,
 
-    // /// Current root hash
-    root_hash:      Fr,
+    /// Cache of the current root hash
+    root_hash:    Fr,
+
+    /// ETH web3 client
+    eth:          eth::Client,
+
+    // TODO: add web server
 }
 
 impl plasma_state::State<Bn256> for PlasmaServer {
@@ -40,5 +48,28 @@ impl plasma_state::State<Bn256> for PlasmaServer {
 }
 
 impl PlasmaServer {
+
+    /// Create new plasma server
+    pub fn new() -> Self {
+        let eth = eth::Client::new(eth::PROD_PLASMA);
+
+        let mut balance_tree = ParallelBalanceTree::new(*plasma_constants::BALANCE_TREE_DEPTH);
+
+        // TODO: load balances from the database here (for demo, simulate this by inserting random accounts)
+        
+        let root_hash = balance_tree.root_hash();
+
+        Self{
+            block_number: 0, // we start with block zero
+            eth, 
+            balance_tree,
+            root_hash,
+        }
+    }
+
+    /// Start main coordination event loop in the current thread
+    pub fn start(&mut self) {
+
+    }
 
 }
