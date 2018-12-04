dd2994fd9692a1a3119bbb5112c19279a0d36bbf
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-12-04 19:40:03 +0300

    add primitives

--- plasma/src/primitives.rs
+++ plasma/src/primitives.rs
@@ -25,6 +25,39 @@ impl<Fr: PrimeField> GetBitsFixed for Fr {
     }
 }
 
+use web3::types::U256;
+use ff::{ScalarEngine};
+use pairing::{Engine, CurveAffine};
+use pairing::bn256::{Bn256, G1Uncompressed, G2Uncompressed};
+
+pub fn serialize_g1_for_ethereum(point: <Bn256 as Engine>::G1Affine) -> (U256, U256) {
+        let uncompressed = point.into_uncompressed();
+
+        let x = U256::from_big_endian(& uncompressed.as_ref()[0..32]);
+        let y = U256::from_big_endian(& uncompressed.as_ref()[32..64]);
+
+        (x, y)
+}
+
+pub fn serialize_g2_for_ethereum(point: <Bn256 as Engine>::G2Affine) -> ((U256, U256), (U256, U256)) {
+        let uncompressed = point.into_uncompressed();
+
+        let x_0 = U256::from_big_endian(& uncompressed.as_ref()[0..32]);
+        let x_1 = U256::from_big_endian(& uncompressed.as_ref()[32..64]);
+        let y_0 = U256::from_big_endian(& uncompressed.as_ref()[64..92]);
+        let y_1 = U256::from_big_endian(& uncompressed.as_ref()[92..128]);
+
+        ((x_0, x_1), (y_0, y_1))
+}
+
+pub fn serialize_fe_for_ethereum(field_element: <Bn256 as ScalarEngine>::Fr) -> U256 {
+        let mut be_bytes = [0u8; 32];
+        field_element.into_repr().write_be(& mut be_bytes[..]).expect("get new root BE bytes");
+        let u256 = U256::from_big_endian(&be_bytes[..]);
+        
+        u256
+}
+
 #[test]
 fn test_get_bits() {
     use pairing::bn256::{Fr};
