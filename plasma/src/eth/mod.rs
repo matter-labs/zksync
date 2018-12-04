0ec0a049c5460e1c085f5970720d0cbd8d60bdcd
Alex <gluk64@gmail.com>
2018-12-04 17:09:41 +0800

    dependencies added

--- plasma/src/eth/mod.rs
+++ plasma/src/eth/mod.rs
@@ -1,3 +1,15 @@
+extern crate ethereum_types;
+extern crate ethabi;
+extern crate ethkey;
+extern crate web3;
+
+// extern crate rustc_serialize;
+// extern crate serde;
+// extern crate serde_derive;
+// extern crate serde_json;
+
+use self::web3::futures::Future;
+
 pub struct ETHNode {
 
 }
@@ -15,4 +27,13 @@ impl ETHNode {
     pub fn prove_block() {
 
     }
+}
+
+#[test]
+fn test_web3() {
+    let (_eloop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
+    let web3 = web3::Web3::new(transport);
+    let accounts = web3.eth().accounts().wait().unwrap();
+
+    println!("Accounts: {:?}", accounts);
 }
