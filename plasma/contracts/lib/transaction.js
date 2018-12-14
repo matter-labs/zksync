9ae05075546b4564cd2d749e043abe651d00fd7c
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-12-14 16:28:10 +0300

    implement integer parsing

--- plasma/contracts/lib/transaction.js
+++ plasma/contracts/lib/transaction.js
@@ -57,6 +57,84 @@ function parseSignature(obj, curve) {
     return {R: R, S: S};
 }
 
+function floatToInteger(
+    floatBytes,
+    exp_bits,
+    mantissa_bits,
+    exp_base
+)
+{
+    assert(floatBytes.length*8 == (exp_bits + mantissa_bits));
+    const floatHolder = new BN(floatBytes, 16, "be"); // keep bit order
+    const totalBits = floatBytes.length*8 - 1; // starts from zero
+    let expBase = new BN(exp_base);
+    let exponent = new BN(0);
+    let exp_power_of_to = new BN(1);
+    const two = new BN(2);
+    for (let i = 0; i < exp_bits; i++) {
+        if (floatHolder.testn(totalBits - i)) {
+            exponent = exponent.add(exp_power_of_to);
+        }
+        // console.log(exponent.toString(10));
+        exp_power_of_to = exp_power_of_to.mul(two);
+    }
+    exponent = expBase.pow(exponent);
+    let mantissa = new BN(0);
+    let mantissa_power_of_to = new BN(1);
+    for (let i = 0; i < mantissa_bits; i++) {
+        if (floatHolder.testn(totalBits - exp_bits - i)) {
+            mantissa = mantissa.add(mantissa_power_of_to);
+        }
+        // console.log(mantissa.toString(10));
+        mantissa_power_of_to = mantissa_power_of_to.mul(two);
+    }
+    return exponent.mul(mantissa);
+}
+
+function integerToFloat(
+    integer,
+    exp_bits,
+    mantissa_bits,
+    exp_base
+)
+{
+    const maxMantissa = (new BN(1)).ushln(mantissa_bits).subn(1);
+    const maxExponent = (new BN(exp_base)).pow((new BN(1)).ushln(exp_bits).subn(1));
+    assert(integer.lte(maxMantissa.mul(maxExponent)));
+    // try to get the best precision
+    let power = integer.div(maxMantissa);
+    const exponentBase = new BN(exp_base);
+    let exponent = new BN(0);
+    while (power.gt(exponentBase)) {
+        power = power.div(exponentBase);
+        exponent = exponent.addn(1);
+    }
+    if (maxMantissa.mul(exponentBase.pow(exponent)).lt(integer)) {
+        exponent = exponent.addn(1);
+    }
+    power = exponentBase.pow(exponent);
+    let mantissa = integer.div(power);
+    // pack
+    assert((mantissa_bits + exp_bits) % 8 === 0);
+    const totalBits = mantissa_bits + exp_bits - 1;
+    const encoding = new BN(0);
+    for (let i = 0; i < exp_bits; i++) {
+        if (exponent.testn(i)) {
+            encoding.bincn(totalBits - i);
+        }
+        // console.log(encoding.toString(2))
+    }
+    for (let i = 0; i < mantissa_bits; i++) {
+        if (mantissa.testn(i)) {
+            encoding.bincn(totalBits - exp_bits - i);
+        }
+        // console.log(encoding.toString(2))
+    }
+    // console.log(encoding.toString(2))
+    return encoding.toBuffer("be", (exp_bits + mantissa_bits)/8);
+}
+
+
 function main() {
     const sk = (new BN(elliptic.rand(32), 16, "be")).umod(altBabyJubjub.n);
     const pub = altBabyJubjub.g.mul(sk);
@@ -68,6 +146,18 @@ function main() {
     const parsedSig = parseSignature(sigJSON, altBabyJubjub);
     const isValidParsed = verify(message, parsedSig, pub, altBabyJubjub);
     assert(isValidParsed);
+
+    // exp = 1, mantissa = 1 => 10
+    // const floatBytes = (new BN("1000010000000000", 2)).toBuffer();
+    // const integer = floatToInteger(floatBytes, 5, 11, 10);
+    // assert(integer.toString(10) === "10");
+    const someInt = new BN("1234556678")
+    const encoding = integerToFloat(someInt, 5, 11, 10);
+    const decoded = floatToInteger(encoding, 5, 11, 10);
+    assert(decoded.lte(someInt));
+    console.log("encoded-decoded = " + decoded.toString(10));
+    console.log("original = " + someInt.toString(10));
+
 }
 
 main();
