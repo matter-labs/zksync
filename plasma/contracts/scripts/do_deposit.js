f5f0eb3e8c4623b7573e1adc261f363d639cb30c
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-12-24 13:36:30 +0300

    prepare to refactor scripts for both local and remote work

--- plasma/contracts/scripts/do_deposit.js
+++ plasma/contracts/scripts/do_deposit.js
@@ -7,13 +7,21 @@ const transactionLib = require("../lib/transaction");
 const ethUtils = require("ethereumjs-util");
 const BN = require("bn.js");
 
-const rpcEndpoint = "http://127.0.0.1:8545";
-const contractAddress = "0x4169D71D56563eA9FDE76D92185bEB7aa1Da6fB8";
+// const rpcEndpoint = "http://127.0.0.1:8545";
+// const contractAddress = "0x4169D71D56563eA9FDE76D92185bEB7aa1Da6fB8";
+const rpcEndpoint = "https://rinkeby.infura.io/48beda66075e41bda8b124c6a48fdfa0";
+const contractAddress = "0xe8D0E0c58857f5bb83Ee8ccE2D42f144Bb7263aC";
+// rinkeby 0xe8D0E0c58857f5bb83Ee8ccE2D42f144Bb7263aC
 const privateKey = "0xaf355f2914da0ea0361a5f93ca4224f60ae6a00d648e63f8fdebacc26e5a06f3";
 
 async function depositInto(acccountString, amountString) {
     let provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
     let walletWithProvider = new ethers.Wallet(privateKey, provider);
+    if (process.env.MNEMONIC !== undefined) {
+        console.log("Using mnemonics");
+        walletWithProvider = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+        walletWithProvider = walletWithProvider.connect(provider);
+    }
     const senderAddress = await walletWithProvider.getAddress();
     console.log("Sending from address " + senderAddress)
     let contract = new ethers.Contract(contractAddress, abi_string, walletWithProvider);
@@ -30,7 +38,7 @@ async function depositInto(acccountString, amountString) {
     let {x, y} = newKey.publicKey;
     x = "0x" + x.toString(16);
     y = "0x" + y.toString(16);
-    const txAmount = ethers.utils.parseEther("1.0");
+    const txAmount = ethers.utils.parseEther("0.001");
     console.log("Tx amount in wei = " + txAmount.toString(10));
     const tx = await contract.deposit([x, y], 0, {value: txAmount});
     console.log("Result = ", tx.hash);
--- plasma/contracts/scripts/get_logs.js
+++ plasma/contracts/scripts/get_logs.js
@@ -5,8 +5,11 @@ const transactionLib = require("../lib/transaction");
 const ethUtils = require("ethereumjs-util");
 const BN = require("bn.js");
 
-const rpcEndpoint = "http://127.0.0.1:8545";
-const contractAddress = "0x4169D71D56563eA9FDE76D92185bEB7aa1Da6fB8";
+// const rpcEndpoint = "http://127.0.0.1:8545";
+// const contractAddress = "0x4169D71D56563eA9FDE76D92185bEB7aa1Da6fB8";
+
+const rpcEndpoint = "https://rinkeby.infura.io/48beda66075e41bda8b124c6a48fdfa0";
+const contractAddress = "0xe8D0E0c58857f5bb83Ee8ccE2D42f144Bb7263aC";
 
 async function getLogs(batchNumberString) {
     let provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
@@ -14,10 +17,20 @@ async function getLogs(batchNumberString) {
     let contract = new ethers.Contract(contractAddress, abi_string, provider);
     const depositBatchSize = await contract.DEPOSIT_BATCH_SIZE();
     const totalDepositRequests = await contract.totalDepositRequests();
+    console.log("Total deposits happened = " + totalDepositRequests.toString(10));
     const totalBatches = totalDepositRequests.div(depositBatchSize);
+    console.log("Current batch = " + totalBatches.toString(10));
     for (let i = 0; i < totalBatches.toNumber(); i++) {
+        console.log("Trying to get all logs for deposit batch " + i);
         let filter = contract.filters.LogDepositRequest("0x" + (new BN(i)).toString(16), null, null);
-        let events = await provider.getLogs(filter);
+        // need to explicitly set block range
+        let fullFilter = {
+            fromBlock: 1,
+            toBlock: 'latest',
+            address: filter.address,
+            topics: filter.topics
+        };
+        let events = await provider.getLogs(fullFilter);
         console.log(JSON.stringify(events));
     }
 }
