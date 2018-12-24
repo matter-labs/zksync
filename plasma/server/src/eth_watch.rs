658dfc7ee12c592c34bdc469b35c774779c0c96f
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-12-24 11:43:12 +0300

    can not run watcher as test for debug purposes, but looks like ganache does not provide logs for old blocks

--- plasma/server/src/eth_watch.rs
+++ plasma/server/src/eth_watch.rs
@@ -1,5 +1,4 @@
 extern crate rustc_hex;
-// extern crate tokio_core;
 extern crate web3;
 
 use ff::{Field, PrimeField, PrimeFieldRepr};
@@ -8,16 +7,15 @@ use std::env;
 use std::str::FromStr;
 use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::Sender;
-use super::state_keeper::{StateProcessingRequest, BlockSource};
+use super::models::{StateProcessingRequest};
 use plasma::models::{Block, DepositBlock, DepositTx, Engine, Fr, ExitBlock, ExitTx};
-use bigdecimal::{Num, BigDecimal, FromPrimitive, ToPrimitive};
+use bigdecimal::{Num, BigDecimal, ToPrimitive};
 use plasma::models::params;
 
 use std::time;
-use rustc_hex::{FromHex, ToHex};
 use web3::contract::{Contract, Options};
-use web3::futures::{Future, Stream};
-use web3::types::{Address, U256, H160, H256, U128, FilterBuilder, BlockNumber};
+use web3::futures::{Future};
+use web3::types::{U256, H160, H256, U128, FilterBuilder, BlockNumber};
 use sapling_crypto::jubjub::{edwards, Unknown};
 
 type ABI = (&'static [u8], &'static str);
