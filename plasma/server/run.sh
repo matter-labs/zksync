05d4f42eefd17348d57bec7154d35ddddde0aa63
Alex <gluk64@gmail.com>
2018-12-26 19:40:56 +0700

    local geth

--- plasma/server/run.sh
+++ plasma/server/run.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+ if [ "$#" -eq  "0" ]
+   then
+    echo "starting for rinkeby"
+    export WEB3_URL=https://rinkeby.infura.io/v3/734de4d8205641beba7e48ec1a205c86
+ else
+    echo "starting local"
+    export WEB3_URL=http://localhost:8545
+fi
+ 
 export SENDER_ACCOUNT=b4aaffeAAcb27098d9545A3C0e36924Af9EeDfe0
-export WEB3_URL=https://rinkeby.infura.io/v3/734de4d8205641beba7e48ec1a205c86
 export PRIVATE_KEY=12B7678FF12FE8574AB74FFD23B5B0980B64D84345F9D637C2096CA0EF587806 
+
 # export PRIVATE_KEY=12B7678FF12FE8574AB74FFD23B5B0980B64D84345F9D637C2096CA0EF587805 
 export CHAIN_ID=4
 
