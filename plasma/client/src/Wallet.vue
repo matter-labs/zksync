48f1fdd23d3aefba0bfca1eb6e61d6fa87d3344e
Alex <gluk64@gmail.com>
2018-12-27 17:58:21 +0700

    responsive

--- plasma/client/src/Wallet.vue
+++ plasma/client/src/Wallet.vue
@@ -6,7 +6,8 @@
         <b-navbar-brand>Plasma Wallet</b-navbar-brand>
         <b-collapse is-nav id="nav_collapse">
             <b-navbar-nav>
-                <b-nav-item href="#">Send</b-nav-item>
+                <b-nav-item href="#" active>Account</b-nav-item>
+                <b-nav-item href="#" disabled>Transactions</b-nav-item>
             </b-navbar-nav>
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
@@ -18,20 +19,25 @@
     <br>
     <b-container class="bv-example-row">
         <b-row>
-            <b-col cols="8">
+            <b-col sm="8" class="mb-4">
                 <b-card header="Your account">
                     <b-form>
                         <b-form-input v-model="store.account" type="text" readonly bg-variant="light" class="mr-2"></b-form-input>
                     </b-form>
                 </b-card>
             </b-col>
-            <b-col cols="4">
+            <b-col sm="4" class="mb-4">
                 <b-card header="Account info">
                     <b-row>
-                        <b-col sm="5">Balance:</b-col> <b-col sm="6">0 ETH</b-col>
+                        <b-col>Balance:</b-col> <b-col>0 ETH</b-col>
                     </b-row>
                     <b-row>                    
-                        <b-col sm="5">Nonce:</b-col> <b-col sm="6">0</b-col>
+                        <b-col>Nonce:</b-col> <b-col>0</b-col>
+                    </b-row>
+                    <br>
+                    <b-row>
+                        <b-col sm class="mb-2"><b-btn variant="outline-secondary" class="w-100">Deposit</b-btn></b-col>
+                        <b-col sm class="mb-2"><b-btn variant="outline-secondary" class="w-100">Withdraw</b-btn></b-col>
                     </b-row>
                 </b-card>
             </b-col>
