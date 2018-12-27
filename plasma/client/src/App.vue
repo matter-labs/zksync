e9c4ab2a5c802c21bd7d03fc269e5461450fee75
Alex <gluk64@gmail.com>
2018-12-27 10:53:33 +0700

    initial commit

---
+++ plasma/client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <h1>Welcome to Plasma Wallet</h1>
+        <h2>{{ counter }}</h2>
+        <b-button @click="counter++">Click</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            counter: 0,
+        }
+    },
+    created() {
+        this.counter += 2
+    },
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1, h2 {
+    font-weight: normal;
+}
+
+</style>
---
+++ plasma/client/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
