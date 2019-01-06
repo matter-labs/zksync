7029d3a29e2eb4d20d6fb10e171b1da3f00e18ff
Alex <gluk64@gmail.com>
2019-01-06 14:16:45 +0100

    /var/log/plasma.log

--- plasma/server/launch.sh
+++ plasma/server/launch.sh
@@ -5,5 +5,5 @@ ps auxw | grep target/release/server | grep -v grep > /dev/null
 if [ $? != 0 ]
 then
     export PATH="$HOME/.cargo/bin:$PATH"
-    ./run.sh 2>&1 | tee log.log
+    ./run.sh 2>&1 | tee /var/log/plasma.log
 fi
