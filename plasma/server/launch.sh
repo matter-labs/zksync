f6b4038b08ef0fae72c99c66b50548a70cf4017c
Alex <gluk64@gmail.com>
2019-01-06 14:28:17 +0100

    safecheck

--- plasma/server/launch.sh
+++ plasma/server/launch.sh
@@ -4,6 +4,8 @@ ps auxw | grep target/release/server | grep -v grep > /dev/null
 
 if [ $? != 0 ]
 then
+    cp /var/log/plasma.log plasma-`date +%Y-%m-%d-%H%M%S`.log
+
     export PATH="$HOME/.cargo/bin:$PATH"
     ./run.sh 2>&1 | tee /var/log/plasma.log
 fi
