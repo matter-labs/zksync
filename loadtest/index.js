e91e16217c639f8754313348179c8fae3240c032
Alex <gluk64@gmail.com>
2019-04-20 15:17:02 +0800

    structure

--- loadtest/index.js
+++ loadtest/index.js
@@ -15,6 +15,16 @@ class Client {
         console.log(`creating client #${this.id}`)
     }
 
+    async prepare() {
+        // create deterministic wallet by id from seed
+        // if franklin not exists
+            // if wallet empty
+                // transfer funds from source account
+                // wait for receipt
+            // deposit funds into franklin
+            // wait for receipt
+    }
+
     async send() {
         let account = await franklin.getAccount(this.id);
         console.log(`client #${this.id}: tx `, account)
@@ -24,11 +34,23 @@ class Client {
 
 let clients = []
 
-for (let i=0; i<nClients; i++) {
-    clients.push(new Client(i))
-}
+async function test() {
 
-async function main() {
+    console.log('creating clients...')
+    for (let i=0; i<nClients; i++) {
+        clients.push(new Client(i))
+    }
+
+    console.log('preparing clients...')
+    let promises = []
+    for (let i=0; i<nClients; i++) {
+        promises.push( clients[i].prepare() )
+    }
+
+    console.log('waiting until the clients are ready...')
+    await Promise.all(promises)
+
+    console.log('starting the test...')
     while(true) {
         var nextTick = new Date(new Date().getTime() + 1000);
         for (let i=0; i<tps; i++) {
@@ -42,4 +64,4 @@ async function main() {
     }
 }
 
-main()
+test()
