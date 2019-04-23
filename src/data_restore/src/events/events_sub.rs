3adcc3a003934651e7fdb8b03420247e2893a467
BaldyAsh <antongrigorjev2010@gmail.com>
2019-04-23 16:37:37 +0300

    perform storage for blocks

--- src/data_restore/src/events/events_sub.rs
+++ src/data_restore/src/events/events_sub.rs
@@ -10,63 +10,49 @@ pub enum InfuraEndpoint {
     Rinkeby
 }
 
+#[derive(Debug)]
 pub struct LogBlockData {
-    pub block_num: u32,
-    pub transaction_hash: String
+    pub block_num: H256,
+    pub transaction_hash: H256
 }
 
+#[derive(Debug)]
 pub struct EventsFranklin {
     pub committed_blocks: Vec<LogBlockData>,
     pub verified_blocks: Vec<LogBlockData>,
-    pub eloop: Core,
 }
 
 impl EventsFranklin {
     pub fn new() -> Self {
         let this = Self {
             committed_blocks: vec![],
-            verified_blocks: vec![],
-            eloop: Core::new().unwrap(),
+            verified_blocks: vec![]
         };
         this
     }
 
-    fn logs_subscription(
-        &mut self, 
-        franklin_address: Address,
-        franklin_genesis_block: u64,
-        topics: Vec<&str>,
-        w3: Rc<web3::Web3<web3::transports::WebSocket>>,
-    ) -> impl Future<Item = (), Error = ()> {
-        println!("subscribing to franklin logs {:?}...", topics);
-
-        let mut topics_vec: Vec<web3::types::H256> = vec![];
-        for i in 0..topics.len() {
-            let topic_h256: web3::types::H256 = self.get_topic_keccak_hash(topics[i]);
-            topics_vec.push(topic_h256);
+    pub fn get_committed_blocks(&self) -> Vec<LogBlockData> {
+        let mut blocks: Vec<LogBlockData> = vec![];
+        for block in self.committed_blocks.iter() {
+            let copied_block = LogBlockData { 
+                block_num: block.block_num.clone(),
+                transaction_hash: block.transaction_hash.clone()
+            };
+            blocks.push(copied_block);
         }
+        blocks
+    }
 
-        let filter = FilterBuilder::default()
-            .address(vec![franklin_address])
-            .from_block(franklin_genesis_block.into())
-            .topics(
-                Some(topics_vec),
-                None,
-                None,
-                None,
-            )
-            .build();
-
-        w3.eth_subscribe()
-            .subscribe_logs(filter)
-            .and_then(|sub| {
-                sub.for_each(|log| {
-                    println!("---");
-                    println!("got log from subscription: {:?}", log);
-                    Ok(())
-                })
-            })
-            .map_err(|e| eprintln!("franklin log err: {}", e))
+    pub fn get_verified_blocks(&self) -> Vec<LogBlockData> {
+        let mut blocks: Vec<LogBlockData> = vec![];
+        for block in self.verified_blocks.iter() {
+            let verified_block = LogBlockData { 
+                block_num: block.block_num.clone(),
+                transaction_hash: block.transaction_hash.clone()
+            };
+            blocks.push(verified_block);
+        }
+        blocks
     }
 
     fn keccak256_hash(&mut self, bytes: &[u8]) -> Vec<u8> {
@@ -105,30 +91,63 @@ impl EventsFranklin {
         let franklin_genesis_block = 0;
 
         // Setup loop and web3
-        let handle = self.eloop.handle();
+        let mut eloop = Core::new().unwrap();
+        let handle = eloop.handle();
         let w3 = Rc::new(web3::Web3::new(
             web3::transports::WebSocket::with_event_loop(enpoint, &handle)
                 .unwrap(),
         ));
 
         // Subscription
-        let subscribe_franklin_logs_future = self.logs_subscription(
-            franklin_address,
-            franklin_genesis_block,
-            topics,
-            w3.clone()
-        );
-        let all_futures = futures::future::lazy(|| {
-            subscribe_franklin_logs_future
-            // subscribe_franklin_block_committed_logs_future.join(subscribe_franklin_block_verified_logs_future)
-        });
+        println!("subscribing to franklin logs {:?}...", topics);
+
+        let mut topics_vec: Vec<web3::types::H256> = vec![];
+        for i in 0..topics.len() {
+            let topic_h256: web3::types::H256 = self.get_topic_keccak_hash(topics[i]);
+            topics_vec.push(topic_h256);
+        }
+
+        let filter = FilterBuilder::default()
+            .address(vec![franklin_address])
+            .from_block(franklin_genesis_block.into())
+            .topics(
+                Some(topics_vec),
+                None,
+                None,
+                None,
+            )
+            .build();
+
+        let future = w3.eth_subscribe()
+            .subscribe_logs(filter)
+            .and_then(|sub| {
+                sub.for_each(|log| {
+                    println!("---");
+                    println!("got log from subscription: {:?}", log);
+                    let mut block: LogBlockData = LogBlockData {
+                        block_num: H256::zero(),
+                        transaction_hash : H256::zero()
+                    };
+                    match log.transaction_hash {
+                        Some(hash) => {
+                            block.block_num = log.topics[1];
+                            block.transaction_hash = hash;
+                            self.committed_blocks.push(block);
+                            println!("-");
+                            println!("Blocks in storage: {:?}", self.committed_blocks);
+                        },
+                        None    => println!("No tx hash"),
+                    };
+                    Ok(())
+                })
+            })
+            .map_err(|e| eprintln!("franklin log err: {}", e));
 
         // Run eloop
-        if let Err(_err) = self.eloop.run(all_futures) {
+        if let Err(_err) = eloop.run(future) {
             eprintln!("ERROR");
         }
     }
 }
-
 
 
