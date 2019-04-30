d72bfead33e1f42ac2dfb516e584b1648795fbbb
BaldyAsh <antongrigorjev2010@gmail.com>
2019-04-30 14:36:36 +0300

    added tests and some fixes

--- src/data_restore/src/lib.rs
+++ src/data_restore/src/lib.rs
@@ -10,3 +10,20 @@ pub mod events;
 pub mod franklin_transaction;
 pub mod blocks;
 pub mod helpers;
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use web3::types::U256;
+    use tokio_core::reactor::Core;
+
+    #[test]
+    fn test_events() {
+        let mut events = events::EventsFranklin::get_past_state_with_blocks_delta(helpers::InfuraEndpoint::Rinkeby, U256::from(2404)).unwrap();
+        println!("Committed old: {:?}", events.committed_blocks);
+        println!("Verified old: {:?}", events.verified_blocks);
+        let mut eloop = Core::new().unwrap();
+        events.make_new_sorted_logs_subscription(&mut eloop);
+    }
+}
