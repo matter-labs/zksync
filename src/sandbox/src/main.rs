ed112a526c9b4dd57f7f6ad6c5da317922fbec17
Alex <gluk64@gmail.com>
2019-05-06 12:24:56 +0200

    comments and fixes

--- src/sandbox/src/main.rs
+++ src/sandbox/src/main.rs
@@ -19,11 +19,13 @@ use std::sync::{Arc, RwLock, Mutex};
 use std::collections::HashMap;
 
 struct NonceReadyFuture{
-    account:    u32,
-    nonce:      u32,
-    futures:    NonceFutures,
+    account:            u32,
+    nonce:              u32,
+    futures:            NonceFutures,
+    immediate_result:   Option<Result<(), CurrentNonceIsHigher>>,
 }
 
+#[derive(Clone, Debug)]
 pub struct CurrentNonceIsHigher;
 
 type NonceFuture = Future<Item=(), Error=CurrentNonceIsHigher>;
@@ -44,35 +46,68 @@ struct NonceFutures(Arc<RwLock<Data>>);
 
 impl NonceFutures {
     fn await(&self, account: u32, nonce: u32) -> impl Future<Item=(), Error=CurrentNonceIsHigher> {
+
+        // get mutex access to inner data
         let data = &mut self.0.as_ref().write().unwrap();
-        let key = (account, nonce);
 
-        // TODO: check status, return properly
+        let next_nonce = *data.nonces.get(&account).unwrap_or(&0);
+        // if next_nonce.is_none() {
+        //     // so that we iterate through the notify listing starting not with 0, 
+        //     // but with the first requested nonce
+        //     data.nonces.insert(account, nonce);
+        // }
+        //println!("nonce = {}, next_nonce = {}", nonce, next_nonce);
 
-        data.futures.get(&key)
-        .map(|f| f.clone())
-        .unwrap_or_else( || {
-            let future = NonceReadyFuture{
+        // return immediate result if it can be deducted now
+        if next_nonce > nonce {
+            NonceReadyFuture{
                 account,
                 nonce,
                 futures: self.clone(),
-            }
-            .shared();
-            let r = future.clone();
-            data.futures.insert(key, future);
-            r
-        })
-        .map(|_|()).map_err(|_|CurrentNonceIsHigher) // map to original Future types
+                immediate_result: Some(Err(CurrentNonceIsHigher)),
+            }.shared()
+        } else if next_nonce == nonce {
+                NonceReadyFuture{
+                account,
+                nonce,
+                futures: self.clone(),
+                immediate_result: Some(Ok(())),
+            }.shared()
+        } else {
+            // otherwise add future to the list to be notified
+            let key = (account, nonce);
+            data.futures.get(&key)
+            .map(|f| f.clone())
+            .unwrap_or_else( || {
+                let future = NonceReadyFuture{
+                    account,
+                    nonce,
+                    futures: self.clone(),
+                    immediate_result: None,
+                }
+                .shared();
+                let r = future.clone();
+                data.futures.insert(key, future);
+                r
+            })
+        }
+        .map_err(|_|CurrentNonceIsHigher)
+        .map(|_|())
     }
 
     fn set(&mut self, account: u32, new_nonce: u32) {
+        // get mutex access to inner data
         let data = &mut self.0.as_ref().write().unwrap();
+        
         let old_nonce = *data.nonces.get(&account).unwrap_or(&new_nonce);
         data.nonces.insert(account, new_nonce);
 
+        // notify all tasks which are waiting
         for nonce in old_nonce ..= new_nonce {
+            println!("notify {}?", nonce);
             let key = (account, nonce);
             if let Some(task) = data.tasks.remove(&key) {
+                println!("yes");
                 task.notify();
                 data.futures.remove(&key);
             }
@@ -85,14 +120,25 @@ impl Future for NonceReadyFuture{
     type Error = CurrentNonceIsHigher;
 
     fn poll(&mut self) -> Poll<(), Self::Error> {
+
+        // return immediate result if present
+        if let Some(result) = self.immediate_result.clone() {
+            return result.map(|_| Async::Ready(()));
+        }
+
+        // get mutex access to inner data
         let data = &mut self.futures.0.as_ref().write().unwrap();
-        let key = (self.account, self.nonce);
+
         let next = *data.nonces.get(&self.account).unwrap_or(&0);
+        //println!("poll next = {}, self.nonce = {}", next, self.nonce);
+
         if next > self.nonce {
             Err(CurrentNonceIsHigher)
         } else if next == self.nonce {
             Ok(Async::Ready(()))
         } else {
+            // add task to notify when awaited nonce is ready
+            let key = (self.account, self.nonce);
             data.tasks.insert(key, task::current());
             Ok(Async::NotReady)
         }
@@ -106,7 +152,7 @@ fn main() {
         let (i, mut nm) = acc;
         println!("i = {}", i);
 
-        if i == 1 {
+        if i == 2 {
             nm.set(1, 2);
         }
 
@@ -118,11 +164,11 @@ fn main() {
     tokio::run(future::lazy(move || {
         tokio::spawn(task.map(|_| ()));
 
-        for i in 0..2 {
-            let task = nm.await(1, 2)
-                .timeout(Duration::from_millis(5000))
+        for i in 0..=4 {
+            let task = nm.await(1, i)
+                .timeout(Duration::from_millis(3000))
                 .map(|_| println!("success!"))
-                .or_else(|_| {println!("timout"); future::ok(())} );
+                .or_else(|e| {println!("error: {:?}", e); future::ok(())} );
             tokio::spawn(task);
         }
 
