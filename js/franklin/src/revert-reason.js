aa6007632f26295954a8231d96ac1a5ec85e7f52
Alex <gluk64@gmail.com>
2019-05-07 20:15:42 +0200

    revert reason script made flexible

--- js/franklin/src/revert-reason.js
+++ js/franklin/src/revert-reason.js
@@ -1,7 +1,7 @@
 const ethers = require('ethers')
-const provider = ethers.getDefaultProvider('rinkeby')
+//const provider = ethers.getDefaultProvider('rinkeby')
 //const provider = new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/48beda66075e41bda8b124c6a48fdfa0')
-//const provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_URL)
+const provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_URL)
 
 function hex_to_ascii(str1) {
 	var hex  = str1.toString();
@@ -16,10 +16,16 @@ async function reason() {
     var args = process.argv.slice(2)
     let hash = args[0]
     console.log('tx hash:', hash)
+    console.log('provider:', process.env.WEB3_URL)
+
     let tx = await provider.getTransaction(hash)
-    let code = await provider.call(tx, tx.blockNumber)
-    let reason = hex_to_ascii(code.substr(138))
-    console.log('revert reason:', reason)
+    if (!tx) {
+        console.log('tx not found')
+    } else {
+        let code = await provider.call(tx, tx.blockNumber)
+        let reason = hex_to_ascii(code.substr(138))
+        console.log('revert reason:', reason)
+    }
 }
 
 reason()
