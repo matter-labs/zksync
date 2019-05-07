b12eba741f4f628562483fe65e2031a92ccd28a6
Alex <gluk64@gmail.com>
2019-05-07 17:09:53 +0200

    styling

--- js/explorer/src/Home.vue
+++ js/explorer/src/Home.vue
@@ -10,12 +10,30 @@
     <b-container>
         <b-row>
             <b-col sm="6" class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <b-card title="Blocks">
-                </b-card>
+                  <b-card no-body header="<b>Blocks</b>">
+                    <b-list-group flush>
+                    <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+                    <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+                    <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+                    </b-list-group>
+
+                    <b-card-body>
+                    <b-button block variant="outline-primary">View more</b-button>
+                    </b-card-body>
+                </b-card>        
             </b-col>
             <b-col sm="6" class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                <b-card title="Transactions">
-                </b-card>
+                <b-card no-body header="<b>Transactions</b>">
+                    <b-list-group flush>
+                    <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+                    <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+                    <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+                    </b-list-group>
+
+                    <b-card-body>
+                    <b-button block variant="outline-primary">View more</b-button>
+                    </b-card-body>
+                </b-card> 
             </b-col>
         </b-row>
     </b-container>
@@ -39,7 +57,11 @@ h1, h2, h3, h4 {
     font-weight: bold;
 }
 
-h4 {
-    font-size: 1rem;
+body {
+    font-size: 0.9rem;
+}
+
+.btn {
+    font-size: 0.8rem;
 }
 </style>
