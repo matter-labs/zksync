850860034dccdf1f387d52a44097c051e41fab28
Alex <gluk64@gmail.com>
2019-05-07 20:50:25 +0200

    do not repeat files!

--- docker/prover/prover-entry.sh
+++ docker/prover/prover-entry.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-mkdir -p keys
-
-for i in $KEY_FILES; do
-    echo "Downloading $SPACE_URL/$i"
-    if ! [ -f keys/$i ]; then
-        curl -o keys/$i $SPACE_URL/$i
-    fi
-done
+. /bin/.load_keys
 
 echo key download complete, starting prover
 
