afe7376180eb317e7ff7a0807feb1b7bedab6d1e
Alex <gluk64@gmail.com>
2019-05-08 13:14:32 +0200

    fix for docker

--- docker/prover/prover-entry.sh
+++ docker/prover/prover-entry.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# remove quotes for docker-compose
+export KEY_FILES=`echo $KEY_FILES | sed -e 's/"\(.*\)/\1/g' -e 's/"$//g'`
+echo KEY_FILES=$KEY_FILES
+
 echo NODE_NAME=$NODE_NAME
 echo POD_NAME=$POD_NAME
 
