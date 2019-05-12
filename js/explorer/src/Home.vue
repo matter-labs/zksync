9f0a42465c978945acaccfa55a6db371e0d877f6
Alex <gluk64@gmail.com>
2019-05-12 21:46:17 +0200

    link to client

--- js/explorer/src/Home.vue
+++ js/explorer/src/Home.vue
@@ -6,9 +6,7 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-            <b-nav-item href="#">Blocks</b-nav-item>
-            <b-nav-item href="#">Transactions</b-nav-item>
-            <b-nav-item href="#">MatterMask</b-nav-item>
+            <b-nav-item href="https://demo.matter-labs.io" target="blanc">MatterMask</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
             <b-nav-item-dropdown text="Rinkeby" right>
