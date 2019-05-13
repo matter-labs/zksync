13db05f066519d6d0e97015867f9494e56b206e7
Alex <gluk64@gmail.com>
2019-05-13 17:43:25 +0200

    link to client

--- js/explorer/src/Home.vue
+++ js/explorer/src/Home.vue
@@ -6,7 +6,7 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-            <b-nav-item href="https://demo.matter-labs.io" target="blanc">MatterMask</b-nav-item>
+            <b-nav-item href="/client/" target="blanc">MatterMask</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
             <b-nav-item-dropdown text="Rinkeby" right>
