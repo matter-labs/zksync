8fb5677970f7b1178974618c27b35b2fb90702b0
BaldyAsh <antongrigorjev2010@gmail.com>
2019-07-19 01:39:50 +0300

    deploying, adding token, deposit and withdraw ether and erc20

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,40 +1,35 @@
-// import Contract2 from './build/Franklin'
-
 const franklinContract = require('../build/Franklin');
 const {
     deployContract
 } = require('ethereum-waffle');
 const ethers = require('ethers');
-const ERC20MintableContract = function () {
-    let contract = require('openzeppelin-solidity/build/contracts/ERC20Mintable');
-    contract.evm = {bytecode: contract.bytecode};
-    return contract
-}();
 const erc20token = require('./erc20_token.js');
+const ether = require('./ether.js');
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_URL);
 const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, "m/44'/60'/0'/0/1").connect(provider); ////"fine music test violin matrix prize squirrel panther purchase material script deal"
 
-async function deployFranklin(wallet, franklinContract) {
+async function deployFranklin(wallet, franklin) {
     try {
-        let contract = await deployContract(wallet, franklinContract, [ethers.constants.HashZero, wallet.address, wallet.address], {
+        let contract = await deployContract(wallet, franklin, [ethers.constants.HashZero, wallet.address, wallet.address], {
             gasLimit: 8000000
         });
-        console.log("Franklin address:",contract.address);
+        console.log("Franklin address:" + contract.address);
 
         return contract
     } catch (err) {
-        console.log("Error:", err);
+        console.log("Error:" + err);
     }
 }
-
 
 async function main() {
     try {
         let franklinDeployedContract = await deployFranklin(wallet, franklinContract);
-        let erc20Token = await erc20token.deployAndAddToFranklin(ERC20MintableContract, wallet, franklinDeployedContract);
+        let erc20DeployedToken = await erc20token.deployAndAddToFranklin(wallet, franklinDeployedContract);
+        await ether.deposit("0.1", wallet, franklinDeployedContract);
+        await ether.withdraw("0.05", franklinDeployedContract);
     } catch (err) {
-        console.log("Error:", err);
+        console.log("Error:" + err);
     }
 }
 
--- contracts/scripts/erc20_token.js
+++ contracts/scripts/erc20_token.js
@@ -1,19 +1,68 @@
-// import Contract2 from './build/Franklin'
-
 const {
     deployContract
 } = require('ethereum-waffle');
+const ERC20MintableContract = function () {
+    let contract = require('openzeppelin-solidity/build/contracts/ERC20Mintable');
+    contract.evm = {bytecode: contract.bytecode};
+    return contract
+}();
 
-async function deployAndAddToFranklin(tokenContract, wallet, franklinContract) {
+async function deployAndAddToFranklin(wallet, franklin) {
     try {
-        let erc20 = await deployContract(wallet, tokenContract, []);
-        console.log("Test ERC20 address:", erc20.address);
-        await franklinContract.addToken(erc20.address)
+        let erc20 = await deployContract(wallet, ERC20MintableContract, []);
+        console.log("Test ERC20 address:" + erc20.address);
+        await franklin.addToken(erc20.address)
+        return erc20
     } catch (err) {
-        console.log("Error:", err);
+        console.log("Error:" + err);
+    }
+}
+
+async function addToFranklin(tokenAddress, franklin) {
+    try {
+        await franklin.addToken(tokenAddress)
+    } catch (err) {
+        console.log("Error:" + err);
+    }
+}
+
+async function deposit(token, amount, wallet, franklin) {
+    try {
+        if (amount == 0) {
+            throw "Sending nothing is meanless";
+        }
+        let balance = await token.balanceOf(wallet.address);
+        let depositAmount = ethers.utils.parseEther(amount);
+        if (balance <= depositAmount) {
+            throw "Too much tokens sending";
+        }
+
+        let tx = await franklin.depositERC20(token.address, depositAmount);
+
+        console.log("Sent token in Transaction: " + tx.hash);
+    } catch (error) {
+        console.log("Error in sending token:" + error);
+    }
+}
+
+// TODO: - dont work
+async function withdraw(tokenAddress, amount, franklin) {
+    try {
+        let withdrawAmount = ethers.utils.parseEther(amount);
+        let overrides = {
+            gasLimit: 10000000
+        };
+        let tx = await franklin.withdrawERC20(tokenAddress, withdrawAmount, overrides);
+
+        console.log("Withdrew token in Transaction: " + tx.hash);
+    } catch (error) {
+        console.log("Error in withdrawing token:" + error);
     }
 }
 
 module.exports = {
-    deployAndAddToFranklin
+    deployAndAddToFranklin,
+    addToFranklin,
+    deposit,
+    withdraw
 }
