64db4521836722c14759610def5acd9fb1c011a4
Vitaly Drogan <vitaliy.drogan@gmail.com>
2019-07-29 16:57:32 +0300

    Test transfert to new

--- js/franklin_lib/scripts/loadtest.ts
+++ js/franklin_lib/scripts/loadtest.ts
@@ -10,14 +10,17 @@ function sleep(ms) {
 async function main() {
     let Account1Address = Buffer.from('dead00000000000000000000000000000000000000000000000000', 'hex');
     let Account2Address = Buffer.from('beef00000000000000000000000000000000000000000000000000', 'hex');
+    let Account3Address = Buffer.from('babe00000000000000000000000000000000000000000000000000', 'hex');
     let feeCollectorAddress = Buffer.from('000000000000000000000000000000000000000000000000000000', 'hex');
     let acc1 = new Wallet(Account1Address);
     let acc2 = new Wallet(Account2Address);
+    let acc3 = new Wallet(Account3Address);
     let feeAccount = new Wallet(feeCollectorAddress);
 
     console.log('Before deposit');
     console.log('Account 1', await acc1.getState());
     console.log('Account 2', await acc2.getState());
+    console.log('Account 3', await acc3.getState());
     console.log('FeeAccount', await feeAccount.getState());
 
     await acc1.deposit(0, new BN(100 + 45 + 8), new BN(10));
@@ -27,14 +30,17 @@ async function main() {
     console.log('After deposit');
     console.log('Account 1', await acc1.getState());
     console.log('Account 2', await acc2.getState());
+    console.log('Account 3', await acc3.getState());
     console.log('FeeAccount', await feeAccount.getState());
 
     await acc1.transfer(acc2.address, 0, new BN(45), new BN(8));
+    await acc2.transfer(acc3.address, 0, new BN(90), new BN(10));
 
     await sleep(10000);
     console.log('After Transfer');
     console.log('Account 1', await acc1.getState());
     console.log('Account 2', await acc2.getState());
+    console.log('Account 3', await acc3.getState());
     console.log('FeeAccount', await feeAccount.getState());
 }
 
--- js/franklin_lib/tests/util.test.ts
+++ js/franklin_lib/tests/util.test.ts
@@ -4,18 +4,16 @@ import { floatToInteger, integerToFloat } from '../src/utils';
 
 describe('Packing and unpacking', function() {
     it('Test round-trip', function() {
-        let initial = new BN('12340000000000');
-        let packed = integerToFloat(initial, 4, 4, 10);
-        let unpacked = floatToInteger(packed, 4, 4, 10);
-        expect(initial.eq(unpacked));
-    });
+        let initial_fee = new BN('12000000000');
+        let packed_fee = integerToFloat(initial_fee, 4, 4, 10);
+        console.log('Fee: ', initial_fee.toString(), ' ', packed_fee);
+        let unpacked_fee = floatToInteger(packed_fee, 4, 4, 10);
+        expect(initial_fee.eq(unpacked_fee)).to.equal(true);
 
-    it('Print values', function() {
-        let amount = new BN('12340000000000000000000');
-        let packed_amount = integerToFloat(amount, 9, 15, 10);
-        console.log(packed_amount);
-        let fee = new BN('456000');
-        let packed_fee = integerToFloat(fee, 4, 4, 10);
-        console.log(packed_fee);
+        let initial_amount = new BN('987650000000000000000');
+        let packed_amount = integerToFloat(initial_amount, 5, 19, 10);
+        console.log('Amount: ', initial_amount.toString(), ' ', packed_amount);
+        let unpacked_amount = floatToInteger(packed_amount, 5, 19, 10);
+        expect(initial_amount.eq(unpacked_amount)).to.equal(true);
     });
 });
