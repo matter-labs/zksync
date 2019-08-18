cc7444bd244fb0921b2a2afdc5bb1077d972f08b
Vitaly Drogan <vitaliy.drogan@gmail.com>
2019-08-18 18:24:32 +0300

    Post contract source on tesseracts on deploy

--- contracts/scripts/testnet-deploy.ts
+++ contracts/scripts/testnet-deploy.ts
@@ -1,10 +1,11 @@
 import {ethers} from "ethers";
-import {addTestERC20Token, deployFranklin} from "../src.ts/deploy";
+import {addTestERC20Token, deployFranklin, franklinContractCode, postContractToTesseracts} from "../src.ts/deploy";
 
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_URL);
     const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, "m/44'/60'/0'/0/1").connect(provider);
     const franklin = await deployFranklin(wallet, process.env.GENESIS_ROOT);
+    await postContractToTesseracts(franklinContractCode, "Franklin", franklin.address);
     await franklin.setValidator(process.env.OPERATOR_ETH_ADDRESS, true);
     await addTestERC20Token(wallet, franklin);
 }
