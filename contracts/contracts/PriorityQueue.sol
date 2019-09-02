1d00b619503a7a8a8ee63d0496850c5b57a647c3
BaldyAsh <antongrigorjev2010@gmail.com>
2019-09-02 16:20:37 +0300

    arbitrary requests

--- contracts/contracts/PriorityQueue.sol
+++ contracts/contracts/PriorityQueue.sol
@@ -5,53 +5,43 @@ contract PriorityQueue {
     /// Request credentials structere
     /// Allows to identify (find and delete) the request from mapping
     /// Fields:
-    /// - signatureHash - hash of user signature from request
+    /// - identifier - incremented request identifier
     /// - expirationBlock - number of Ethereum block when request becomes expired
     struct RequestCreds {
-        bytes20 signatureHash;
+        uint identifier;
         uint expirationBlock;
     }
 
-    /// New deposit request event
-    /// Emitted when deposit request is placed into mapping
-    event NewDepositRequest(
-        address indexed sender,
-        address indexed toAccount,
-        uint16 token,
-        uint112 amount,
-        bytes20 indexed signatureHash,
-        uint expirationBlock
-    );
-
-    /// New exit request event
-    /// Emitted when exit request is placed into mapping
-    event NewExitRequest(
-        address indexed accountAddress,
-        address indexed ethereumAddress,
-        uint16 token,
-        bytes20 indexed signatureHash,
-        uint expirationBlock
+    /// New request event
+    /// Emitted when a request is placed into mapping
+    event NewRequest(
+        uint indexed identifier,
+        bytes pubData,
+        uint indexed expirationBlock
     );
 
     /// Removed request event
-    /// Emitted when any request (deposit or exit) is deleted from mapping
+    /// Emitted when a request is deleted from mapping
     event RemovedRequest(
-        bytes20 indexed signatureHash
+        uint indexed identifier
     );
 
     /// Franklin contract address
     address private franklinAddress;
 
     /// Requests credentials mapping (request number - request creds)
-    /// Contains creds of unsatisfied requests (deposits and exits). Numbers are in order of requests receiving
-    mapping(uint32 => RequestCreds) public requestsCreds;
+    /// Contains creds of unsatisfied requests. Numbers are in order of requests receiving
+    mapping(uint => RequestCreds) public requestsCreds;
 
-    /// Requests existance mapping (signature hash - existance flag)
-    /// Anyone can get request existance (true if exists) by user signature hash from this request
-    mapping(bytes20 => bool) public requestsExistance;
+    /// Requests existance mapping (request identifier - existance flag)
+    /// Anyone can get request existance (true if exists) by this request identifier
+    mapping(uint => bool) public requestsExistance;
 
     /// Total number of requests
-    uint32 public totalRequests;
+    uint public totalRequests;
+
+    /// Incremented requests identifier
+    uint private counter;
 
     /// Only Franklin contract permission modifier
     modifier onlyFranklin() {
@@ -64,75 +54,36 @@ contract PriorityQueue {
         franklinAddress = _franklinAddress;
     }
 
-    /// Add exit request external function
+    /// Add request external function
     /// Params:
-    /// - accountAddress - address of Franklin account from which the funds must be withdrawn
-    /// - ethereumAddress - address of root-chain account to which the funds must be sent
-    /// - token - chosen token to withdraw
-    /// - signatureHash - user signature hash
-    function addExitRequest(
-        address accountAddress,
-        address ethereumAddress,
-        uint16 token,
-        bytes20 signatureHash
+    /// - pubData - public data (payload) of this request
+    function addRequest(
+        bytes calldata pubData
     ) external {
-        require(!requestsExistance[signatureHash], "Exit request from this sender for chosen token exists");
+        uint identifier = counter;
         uint expirationBlock = block.number+250;
         requestsCreds[totalRequests] = RequestCreds(
-            signatureHash,
+            identifier,
             expirationBlock
         );
-        requestsExistance[signatureHash] = true;
+        requestsExistance[identifier] = true;
         totalRequests++;
-        emit NewExitRequest(
-            accountAddress,
-            ethereumAddress,
-            token,
-            signatureHash,
-            expirationBlock
-        );
-    }
-
-    /// Add deposit request external function
-    /// Params:
-    /// - sender - address of sender
-    /// - toAccount - address of Franklin account to which the funds must be sent
-    /// - token - chosen token to deposit
-    /// - amount - amount of the chosen token
-    /// - signatureHash - user signature hash
-    function addDepositRequest(
-        address sender,
-        address toAccount,
-        uint16 token,
-        uint112 amount,
-        bytes20 signatureHash
-    ) external {
-        require(!requestsExistance[signatureHash], "Deposit request from this sender for chosen token and value exists");
-        uint expirationBlock = block.number+250;
-        requestsCreds[totalRequests] = RequestCreds(
-            signatureHash,
-            expirationBlock
-        );
-        requestsExistance[signatureHash] = true;
-        totalRequests++;
-        emit NewDepositRequest(
-            sender,
-            toAccount,
-            token,
-            amount,
-            signatureHash,
+        counter++;
+        emit NewRequest(
+            identifier,
+            pubData,
             expirationBlock
         );
     }
 
     /// Remove request external function. Can be used only from Franklin contract
     /// Params:
-    /// - signatureHash - user signature hash that is used to identify the request
-    function removeRequest(bytes20 signatureHash) external onlyFranklin {
-        require(requestsExistance[signatureHash], "Exit request from this sender for chosen token doesn't exists");
-        delete requestsExistance[signatureHash];
+    /// - identifier - request identifier
+    function removeRequest(uint identifier) external onlyFranklin {
+        require(requestsExistance[identifier], "This request doesn't exists");
+        delete requestsExistance[identifier];
         for (uint32 i = 0; i < totalRequests; i++) {
-            if (requestsCreds[i].signatureHash == signatureHash) {
+            if (requestsCreds[i].identifier == identifier) {
                 delete requestsCreds[i];
                 for (uint32 j = i; j < totalRequests-1; j++) {
                     requestsCreds[j] = requestsCreds[j+1];
@@ -143,7 +94,7 @@ contract PriorityQueue {
         }
         totalRequests--;
         emit RemovedRequest(
-            signatureHash
+            identifier
         );
     }
 
