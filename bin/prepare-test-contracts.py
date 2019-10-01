e6cd6499dc0e69728053b88ee58f52727c54ed07
BaldyAsh <antongrigorjev2010@gmail.com>
2019-10-01 18:37:10 +0300

    now test contracts are in generated folder

--- bin/prepare-test-contracts.py
+++ bin/prepare-test-contracts.py
@@ -2,21 +2,23 @@
 
 import shutil
 import subprocess
+import os
 
-shutil.copy2('./contracts/contracts/Governance.sol', './contracts/contracts/GovernanceTest.sol')
-subprocess.call(["sed", "-i", "", 's/Governance/GovernanceTest/', "./contracts/contracts/GovernanceTest.sol"])
+os.mkdir("./contracts/contracts/generated")
 
-shutil.copy2('./contracts/contracts/Franklin.sol', './contracts/contracts/FranklinTest.sol')
+shutil.copy2('./contracts/contracts/Governance.sol', './contracts/contracts/generated/GovernanceTest.sol')
+subprocess.call(["sed", "-i", "", 's/Governance/GovernanceTest/', "./contracts/contracts/generated/GovernanceTest.sol"])
 
-subprocess.call(["sed", "-i", "", 's/Franklin/FranklinTest/', "./contracts/contracts/FranklinTest.sol"])
-subprocess.call(["sed", "-i", "", 's/Governance/GovernanceTest/', "./contracts/contracts/FranklinTest.sol"])
-subprocess.call(["sed", "-i", "", 's/Verifier/VerifierTest/', "./contracts/contracts/FranklinTest.sol"])
-subprocess.call(["sed", "-i", "", 's/PriorityQueue/PriorityQueueTest/', "./contracts/contracts/FranklinTest.sol"])
-subprocess.call(["sed", "-i", "", 's/60/1/', "./contracts/contracts/FranklinTest.sol"])
-subprocess.call(["sed", "-i", "", 's/100/1/', "./contracts/contracts/FranklinTest.sol"])
+shutil.copy2('./contracts/contracts/Franklin.sol', './contracts/contracts/generated/FranklinTest.sol')
 
+subprocess.call(["sed", "-i", "", 's/Franklin/FranklinTest/', "./contracts/contracts/generated/FranklinTest.sol"])
+subprocess.call(["sed", "-i", "", 's/Governance/GovernanceTest/', "./contracts/contracts/generated/FranklinTest.sol"])
+subprocess.call(["sed", "-i", "", 's/Verifier/VerifierTest/', "./contracts/contracts/generated/FranklinTest.sol"])
+subprocess.call(["sed", "-i", "", 's/PriorityQueue/PriorityQueueTest/', "./contracts/contracts/generated/FranklinTest.sol"])
+subprocess.call(["sed", "-i", "", 's/60/1/', "./contracts/contracts/generated/FranklinTest.sol"])
+subprocess.call(["sed", "-i", "", 's/100/1/', "./contracts/contracts/generated/FranklinTest.sol"])
 
-shutil.copy2('./contracts/contracts/PriorityQueue.sol', './contracts/contracts/PriorityQueueTest.sol')
+shutil.copy2('./contracts/contracts/PriorityQueue.sol', './contracts/contracts/generated/PriorityQueueTest.sol')
 
-subprocess.call(["sed", "-i", "", 's/PriorityQueue/PriorityQueueTest/', "./contracts/contracts/PriorityQueueTest.sol"])
-subprocess.call(["sed", "-i", "", 's/250/16/', "./contracts/contracts/PriorityQueueTest.sol"])
+subprocess.call(["sed", "-i", "", 's/PriorityQueue/PriorityQueueTest/', "./contracts/contracts/generated/PriorityQueueTest.sol"])
+subprocess.call(["sed", "-i", "", 's/250/16/', "./contracts/contracts/generated/PriorityQueueTest.sol"])
