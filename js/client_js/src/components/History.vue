442a2f43fc6b932c8ae450313ce736583b1d24cd
Oleg Syniakevych <olegsyniakevych@gmail.com>
2019-10-11 04:27:23 +0300

    fix

--- js/client_js/src/components/History.vue
+++ js/client_js/src/components/History.vue
@@ -1,6 +1,7 @@
 <template>
     <b-container>
         <!-- <b-pagination v-if="ready" v-model="currentPage" per-page="10" :total-rows="transactions.length" @change="onPageChanged"></b-pagination> -->
+        <img v-if="loading" style="margin-right: 1.5em" src="../assets/loading.gif" width="100em">
         <HistoryRow v-for="txx in transactions" :tx="txx" :key="txx.elem_id"></HistoryRow>
         <!-- <b-pagination v-if="ready" v-model="currentPage" :per-page="perPage" :total-rows="rows" @change="onPageChanged"></b-pagination> -->
     </b-container>
@@ -23,9 +24,11 @@ export default {
         currentPage: 1,
 
         intervalHandle: null,
+        loading: true,
     }),
     async created() {
         await this.load();
+        this.loading = false;
         this.intervalHandle = setInterval(() => this.load(), timeConstants.transactionsRefresh);
     },
     destroyed() {
