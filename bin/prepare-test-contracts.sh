32e618d349815704c6a5eb73ce0d531697a85f93
Vitaly Drogan <vitaliy.drogan@gmail.com>
2019-10-15 15:09:21 +0300

    Make linux compatible

--- bin/prepare-test-contracts.sh
+++ bin/prepare-test-contracts.sh
@@ -9,20 +9,20 @@ cp ./contracts/contracts/Franklin.sol ./contracts/contracts/generated/FranklinTe
 cp ./contracts/contracts/PriorityQueue.sol ./contracts/contracts/generated/PriorityQueueTest.sol
 cp ./contracts/contracts/Verifier.sol ./contracts/contracts/generated/VerifierTest.sol
 
-sed -i '' 's/Governance/GovernanceTest/' ./contracts/contracts/generated/GovernanceTest.sol
+sed 's/Governance/GovernanceTest/' -i ./contracts/contracts/generated/GovernanceTest.sol
 
-sed -i '' 's/.\/Bytes/..\/Bytes/' ./contracts/contracts/generated/FranklinTest.sol
-sed -i '' 's/Governance/GovernanceTest/' ./contracts/contracts/generated/FranklinTest.sol
-sed -i '' 's/Verifier/VerifierTest/' ./contracts/contracts/generated/FranklinTest.sol
-sed -i '' 's/Franklin/FranklinTest/' ./contracts/contracts/generated/FranklinTest.sol
-sed -i '' 's/PriorityQueue/PriorityQueueTest/' ./contracts/contracts/generated/FranklinTest.sol
-sed -i '' 's/60/1/' ./contracts/contracts/generated/FranklinTest.sol
-sed -i '' 's/100/1/' ./contracts/contracts/generated/FranklinTest.sol
+sed 's/.\/Bytes/..\/Bytes/' -i ./contracts/contracts/generated/FranklinTest.sol
+sed 's/Governance/GovernanceTest/' -i ./contracts/contracts/generated/FranklinTest.sol
+sed 's/Verifier/VerifierTest/' -i ./contracts/contracts/generated/FranklinTest.sol
+sed 's/Franklin/FranklinTest/' -i ./contracts/contracts/generated/FranklinTest.sol
+sed 's/PriorityQueue/PriorityQueueTest/' -i ./contracts/contracts/generated/FranklinTest.sol
+sed 's/60/1/' -i ./contracts/contracts/generated/FranklinTest.sol
+sed 's/100/1/' -i ./contracts/contracts/generated/FranklinTest.sol
 
-sed -i '' 's/.\/Bytes/..\/Bytes/' ./contracts/contracts/generated/PriorityQueueTest.sol
-sed -i '' 's/PriorityQueue/PriorityQueueTest/' ./contracts/contracts/generated/PriorityQueueTest.sol
-sed -i '' 's/250/16/' ./contracts/contracts/generated/PriorityQueueTest.sol
+sed 's/.\/Bytes/..\/Bytes/' -i ./contracts/contracts/generated/PriorityQueueTest.sol
+sed 's/PriorityQueue/PriorityQueueTest/' -i ./contracts/contracts/generated/PriorityQueueTest.sol
+sed 's/250/16/' -i ./contracts/contracts/generated/PriorityQueueTest.sol
 
-sed -i '' 's/.\/VerificationKey/..\/VerificationKey/' ./contracts/contracts/generated/VerifierTest.sol
-sed -i '' 's/Verifier/VerifierTest/' ./contracts/contracts/generated/VerifierTest.sol
-sed -i '' 's/\/\/ Start/return true;/' ./contracts/contracts/generated/VerifierTest.sol
+sed 's/.\/VerificationKey/..\/VerificationKey/' -i ./contracts/contracts/generated/VerifierTest.sol
+sed 's/Verifier/VerifierTest/' -i ./contracts/contracts/generated/VerifierTest.sol
+sed 's/\/\/ Start/return true;/' -i ./contracts/contracts/generated/VerifierTest.sol
