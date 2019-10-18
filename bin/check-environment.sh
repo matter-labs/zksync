1f945872980a6cdf32662762f4ed0aff5d936a66
Vitaly Drogan <vitaliy.drogan@gmail.com>
2019-10-18 19:15:11 +0300

    Add env check script

---
+++ bin/check-environment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+check_tool() {
+    which $1 > /dev/null || (echo $1 not found && exit 1)
+}
+
+echo Checking environment
+
+check_tool yarn
+check_tool node
+node --version | grep "v10.*" > /dev/null  || (echo "Node version 10 is supported" && exit 1)
+check_tool docker
+check_tool docker-compose
+check_tool envsubst
+check_tool cargo
+check_tool jq
+check_tool psql
+check_tool pg_isready
+check_tool diesel
+
+( (sed --version | grep GNU) &> /dev/null || (gsed --version | grep GNU) &> /dev/null ) || (echo "sed or gsed should be GNU-sed" && exit 1)
+
+echo Environment is fine
