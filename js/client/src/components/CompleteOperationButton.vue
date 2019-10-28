34a04bdc30744d1d0dd707120a797a6d496d95ae
Oleg Syniakevych <olegsyniakevych@gmail.com>
2019-10-28 23:31:54 +0200

    Button shows loading right after clicking

--- js/client/src/components/CompleteOperationButton.vue
+++ js/client/src/components/CompleteOperationButton.vue
@@ -24,6 +24,7 @@ export default {
     methods: {
         async completeOperation() {
             this.$emit('withdrawOnchainEvent', this.op);
+            completionStatus = 'loading';
         },
     },
 }
