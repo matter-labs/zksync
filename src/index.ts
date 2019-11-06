85bab903039c9e49d33775233775f6d382ff9231
Vitalii Drohan <vitaliy.drogan@gmail.com>
2019-11-06 19:22:35 +0200

    Build

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Wallet } from "./wallet";
+import { WSTransport, HTTPTransport } from "./transport";
+import { SidechainProvider } from "./provider";
+
+export { Wallet, SidechainProvider, WSTransport, HTTPTransport };
--- src/types.ts
+++ src/types.ts
@@ -1,71 +1,71 @@
-import { utils } from 'ethers';
+import { utils } from "ethers";
 
 export type Address = string;
 
 export interface Token {
-    id: number;
-    address: string;
-    symbol?: string;
+  id: number;
+  address: string;
+  symbol?: string;
 }
 // token, symbol/eth erc20 contract address, token id
 export type TokenLike = Token | string | number;
 
-export type Nonce = number | 'commited';
+export type Nonce = number | "commited";
 
 export interface SidechainAccountBalance {
-    address: Address;
-    nonce: number;
-    balances: any[];
+  address: Address;
+  nonce: number;
+  balances: any[];
 }
 
 export interface SidechainAccountState {
-    id?: number;
-    commited: SidechainAccountBalance;
-    verified: SidechainAccountBalance;
-    pending_txs: any[];
+  id?: number;
+  commited: SidechainAccountBalance;
+  verified: SidechainAccountBalance;
+  pending_txs: any[];
 }
 
 export interface ETHAccountState {
-    onchainBalances: utils.BigNumber[];
-    contractBalances: utils.BigNumber[];
+  onchainBalances: utils.BigNumber[];
+  contractBalances: utils.BigNumber[];
 }
 
 export interface SidechainInfo {
-    contract_address: string;
-    tokens: [Token];
+  contract_address: string;
+  tokens: [Token];
 }
 
 export interface DepositTx {
-    to: Address;
-    amount: utils.BigNumberish;
-    token: Token;
+  to: Address;
+  amount: utils.BigNumberish;
+  token: Token;
 }
 
 export interface TransferTx {
-    from: Address;
-    to: Address;
-    token: number;
-    amount: utils.BigNumberish;
-    fee: utils.BigNumberish;
-    nonce: number;
+  from: Address;
+  to: Address;
+  token: number;
+  amount: utils.BigNumberish;
+  fee: utils.BigNumberish;
+  nonce: number;
 }
 
 export interface WithdrawTx {
-    account: Address;
-    eth_address: string;
-    token: number;
-    amount: utils.BigNumberish;
-    fee: utils.BigNumberish;
-    nonce: number;
+  account: Address;
+  eth_address: string;
+  token: number;
+  amount: utils.BigNumberish;
+  fee: utils.BigNumberish;
+  nonce: number;
 }
 
 export interface CloseTx {
-    account: Address;
-    nonce: number;
+  account: Address;
+  nonce: number;
 }
 
 export interface FullExitReq {
-    token: number;
-    eth_address: string;
-    nonce: number;
+  token: number;
+  eth_address: string;
+  nonce: number;
 }
