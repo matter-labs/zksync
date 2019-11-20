860611a644e2b2b539c5f3da930bbe033899f85a
Vitalii Drohan <vitaliy.drogan@gmail.com>
2019-11-20 14:43:50 +0200

    Add zksync.js based integration test

---
+++ js/tests/simple-integration-test.ts
@@ -0,0 +1,148 @@
+import {
+    depositFromETH,
+    SyncWallet,
+    ETHProxy, SyncProvider
+} from "zksync";
+import { ethers, utils } from "ethers";
+
+import {Token} from "zksync/build/types";
+
+let syncProvider: SyncProvider;
+
+async function getOperatorBalance(token: Token, type: "committed" | "verified" = "committed") {
+    const accountState = await syncProvider.getState(process.env.OPERATOR_FRANKLIN_ADDRESS);
+    if (token != "ETH") {
+        token = token.toLowerCase();
+    }
+    let balance;
+    if (type == "committed") {
+        balance = accountState.committed.balances[token] || "0";
+    } else {
+        balance = accountState.verified.balances[token] || "0";
+    }
+    return utils.bigNumberify(balance);
+}
+
+async function testDeposit(ethWallet: ethers.Wallet, syncWallet: SyncWallet, token: Token, amount: utils.BigNumberish) {
+    const balanceBeforeDep = await syncWallet.getBalance(token);
+    const depositHandle = await depositFromETH(
+        ethWallet,
+        syncWallet,
+        token,
+        amount,
+        utils.parseEther("0.1")
+    );
+    await depositHandle.waitCommit();
+    const balanceAfterDep = await syncWallet.getBalance(token);
+
+    if (!balanceAfterDep.sub(balanceBeforeDep).eq(amount)) {
+        throw new Error("Deposit checks failed");
+    }
+}
+
+async function testTransfer(syncWallet1: SyncWallet, syncWallet2: SyncWallet, token: Token, amount: utils.BigNumber, fee: utils.BigNumber) {
+    const wallet1BeforeTransfer = await syncWallet1.getBalance(token);
+    const wallet2BeforeTransfer = await syncWallet2.getBalance(token);
+    const operatorBeforeTransfer = await getOperatorBalance(token);
+    const transferToNewHandle = await syncWallet1.syncTransfer(
+        syncWallet2.address(),
+        token,
+        amount,
+        fee
+    );
+    await transferToNewHandle.waitCommit();
+    const wallet1AfterTransfer = await syncWallet1.getBalance(token);
+    const wallet2AfterTransfer = await syncWallet2.getBalance(token);
+    const operatorAfterTransfer = await getOperatorBalance(token);
+
+    let transferCorrect = true;
+    transferCorrect = transferCorrect && wallet1BeforeTransfer.sub(wallet1AfterTransfer).eq(amount.add(fee));
+    transferCorrect = transferCorrect && wallet2AfterTransfer.sub(wallet2BeforeTransfer).eq(amount);
+    transferCorrect = transferCorrect && operatorAfterTransfer.sub(operatorBeforeTransfer).eq(fee);
+    if (!transferCorrect) {
+        throw new Error("Transfer checks failed");
+    }
+}
+
+async function testWithdraw(ethWallet: ethers.Wallet, syncWallet: SyncWallet, token: Token, amount: utils.BigNumber, fee: utils.BigNumber) {
+    const wallet2BeforeWithdraw = await syncWallet.getBalance(token);
+    const operatorBeforeWithdraw = await getOperatorBalance(token);
+    const withdrawHandle = await syncWallet.withdrawTo(
+        ethWallet.address,
+        token,
+        amount,
+        fee
+    );
+    await withdrawHandle.waitVerify();
+    const wallet2AfterWithdraw = await syncWallet.getBalance(token);
+    const operatorAfterWithdraw = await getOperatorBalance(token);
+
+    let withdrawCorrect = true;
+    withdrawCorrect = withdrawCorrect && wallet2BeforeWithdraw.sub(wallet2AfterWithdraw).eq(amount.add(fee));
+    withdrawCorrect = withdrawCorrect && operatorAfterWithdraw.sub(operatorBeforeWithdraw).eq(fee);
+
+    if (!withdrawCorrect) {
+        throw new Error("Withdraw checks failed");
+    }
+}
+
+async function moveFunds(wallet1: ethers.Wallet, syncWallet1: SyncWallet, wallet2: ethers.Wallet, syncWallet2: SyncWallet, token: Token, depositAmountETH: string) {
+    const depositAmount = utils.parseEther(depositAmountETH);
+
+    // we do two transfers to test transfer to new and ordinary transfer.
+    const transfersFee = depositAmount.div(25);
+    const transfersAmount = depositAmount.div(2).sub(transfersFee);
+
+    const withdrawFee = transfersAmount.div(20);
+    const withdrawAmount = transfersAmount.sub(withdrawFee);
+
+    await testDeposit(wallet1, syncWallet1, token, depositAmount);
+    console.log(`Deposit ok, Token: ${token}`);
+    await testTransfer(syncWallet1, syncWallet2, token, transfersAmount, transfersFee);
+    console.log(`Transfer to new ok, Token: ${token}`);
+    await testTransfer(syncWallet1, syncWallet2, token, transfersAmount, transfersFee);
+    console.log(`Transfer ok, Token: ${token}`);
+    await testWithdraw(wallet2, syncWallet2, token, withdrawAmount, withdrawFee);
+    console.log(`Withdraw ok, Token: ${token}`);
+}
+
+(async () => {
+    const WEB3_URL = process.env.WEB3_URL;
+// Mnemonic for eth wallet.
+    const MNEMONIC = process.env.MNEMONIC;
+    const ERC_20TOKEN = process.env.TEST_ERC20;
+
+    syncProvider = await SyncProvider.newWebsocketProvider();
+
+    const ethersProvider = new ethers.providers.JsonRpcProvider(WEB3_URL);
+    const ethProxy = new ETHProxy(ethersProvider, syncProvider.contractAddress);
+
+    const ethWallet = ethers.Wallet.fromMnemonic(
+        MNEMONIC,
+        "m/44'/60'/0'/0/1"
+    ).connect(ethersProvider);
+    const syncWallet = await SyncWallet.fromEthWallet(
+        ethWallet,
+        syncProvider,
+        ethProxy
+    );
+
+    const ethWallet2 = ethers.Wallet.createRandom().connect(ethersProvider);
+    const syncWallet2 = await SyncWallet.fromEthWallet(
+        ethWallet2,
+        syncProvider,
+        ethProxy
+    );
+
+    const ethWallet3 = ethers.Wallet.createRandom().connect(ethersProvider);
+    const syncWallet3 = await SyncWallet.fromEthWallet(
+        ethWallet3,
+        syncProvider,
+        ethProxy
+    );
+
+    await moveFunds(ethWallet, syncWallet, ethWallet2, syncWallet2, ERC_20TOKEN, "42.0");
+    await moveFunds(ethWallet, syncWallet, ethWallet3, syncWallet3, "ETH", "42.0");
+
+    await syncProvider.disconnect();
+})();
