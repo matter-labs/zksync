1226caf90537f92923846bb99179bbb9b830b9bf
Oleg Syniakevych <olegsyniakevych@gmail.com>
2019-12-06 18:00:05 +0200

    fix gas price calculation

--- js/client/src/WalletDecorator.js
+++ js/client/src/WalletDecorator.js
@@ -437,6 +437,27 @@ export class WalletDecorator {
         }
     }
 
+    async revertReason(tx_hash) {
+        const tx = await this.wallet.ethWallet.provider.getTransaction(tx_hash);
+        const code = await this.wallet.ethWallet.provider.call(tx, tx.blockNumber);
+
+        console.log({tx, code});
+
+        if (code == '0x') {
+            return '';
+        } else {
+            return code
+                .substr(138)
+                .match(/../g)
+                .map(h => parseInt(h, 16))
+                .map(String.fromCharCode)
+                .join('')
+                .split('')
+                .filter(c => /\w/.test(c))
+                .join('');
+        }
+    }
+
     async * verboseDeposit(options) {
         let token = this.tokenFromName(options.token);
         let amount = utils.bigNumberify(options.amount);
@@ -597,11 +618,14 @@ export class WalletDecorator {
                 yield error(`Transaction ${tx_hash_html} failed with empty revert reason.`);
             } else {
                 const reason = code
-                    .substr(138)
-                    .match(/../g)
-                    .map(h => parseInt(h, 16))
-                    .map(String.fromCharCode)
-                    .join('');
+                .substr(138)
+                .match(/../g)
+                .map(h => parseInt(h, 16))
+                .map(String.fromCharCode)
+                .join('')
+                .split('')
+                .filter(c => /\w/.test(c))
+                .join('');
                 yield error(`Transaction ${tx_hash_html} failed with <code>${reason}<code>.`);
             }
         }
