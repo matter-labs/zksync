e3a46597754c1931f5a0a910f30121acae17f976
Alex Gluchowski <ag@matterlabs.dev>
2020-02-13 14:47:46 +0100

    priority queue event source switched to franklin

--- core/models/src/abi.rs
+++ core/models/src/abi.rs
@@ -5,7 +5,6 @@ use std::str::FromStr;
 
 const ZKSYNC_CONTRACT_FILE: &str = "contracts/build/Franklin.json";
 const GOVERNANCE_CONTRACT_FILE: &str = "contracts/build/Governance.json";
-const PRIORITY_QUEUE_CONTRACT_FILE: &str = "contracts/build/PriorityQueue.json";
 const IERC20_CONTRACT_FILE: &str = "contracts/build/IERC20.json";
 
 fn read_file_to_json_value(path: &str) -> io::Result<serde_json::Value> {
@@ -30,15 +29,6 @@ pub fn governance_contract() -> Contract {
         .expect("couldn't get abi from GOVERNANCE_CONTRACT_FILE")
         .to_string();
     Contract::load(abi_string.as_bytes()).expect("governance contract abi")
-}
-
-pub fn priority_queue_contract() -> Contract {
-    let abi_string = read_file_to_json_value(PRIORITY_QUEUE_CONTRACT_FILE)
-        .expect("couldn't read PRIORITY_QUEUE_CONTRACT_FILE")
-        .get("abi")
-        .expect("couldn't get abi from PRIORITY_QUEUE_CONTRACT_FILE")
-        .to_string();
-    Contract::load(abi_string.as_bytes()).expect("priority queue contract abi")
 }
 
 pub fn erc20_contract() -> Contract {
