344fce915e0448d2e149838ee557521fde574e37
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-02-21 14:51:15 +0200

    remove build

--- js/client/vue.config.js
+++ js/client/vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/client/'
         : '/',
-    chainWebpack: config => config.optimization.minimize(process.env.NODE_ENV === 'production'),
+    chainWebpack: config => {
+        config.optimization.minimize(process.env.NODE_ENV === 'production');
+        config.resolve.symlinks(false);
+    },
 };
--- js/zksync.js/build/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Wallet } from "./wallet";
-import { Provider, ETHProxy, getDefaultProvider } from "./provider";
-import { Signer } from "./signer";
-import { closestPackableTransactionAmount, closestPackableTransactionFee } from "./utils";
-import * as types from "./types";
-import * as utils from "./utils";
-import * as crypto from "./crypto";
-export { Wallet, Signer, Provider, ETHProxy, closestPackableTransactionFee, closestPackableTransactionAmount, getDefaultProvider, types, utils, crypto };
