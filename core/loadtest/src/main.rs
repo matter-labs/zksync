a09573e2a48eb2770bcb46f366adaec1e12eb12e
furkhat <vailodf@gmail.com>
2020-02-25 14:25:27 +0600

    remove unused deps & use gwei value & rpc addr from env

--- core/loadtest/src/main.rs
+++ core/loadtest/src/main.rs
@@ -1,6 +1,9 @@
+//! Load test meant to run against running node.
+//! Runs scenario of deposits, withdraws and transfers. Scenario detailization
+//! specified as input json file. Transactions send concurrently. Program exits
+//! successfully if all transactions get verified within configured timeout.
 // Built-in
-use std::fs::File;
-use std::io::prelude::*;
+use std::ops::Mul;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::{Duration, Instant};
@@ -24,6 +27,32 @@ use models::node::tx::TxHash;
 use testkit::eth_account::EthereumAccount;
 use testkit::zksync_account::ZksyncAccount;
 
+const DEPOSIT_TIMEOUT_SEC: u64 = 5 * 60;
+
+fn main() {
+    env_logger::init();
+
+    let config = ConfigurationOptions::from_env();
+    let filepath = env::args().nth(1).expect("test spec file not given");
+    let test_spec = read_test_spec(filepath);
+    let (_el, transport) = Http::new(&config.web3_url).expect("http transport start");
+    let test_accounts = Arc::new(construct_test_accounts(
+        &test_spec.input_accounts,
+        transport,
+        &config,
+    ));
+    let rpc_addr = env::var("HTTP_RPC_API_ADDR").expect("HTTP_RPC_API_ADDR is missing");
+    info!("sending transactions");
+    let sent_txs = block_on(send_transactions(&test_accounts, &test_spec, &rpc_addr));
+    info!("waiting for all transactions to be verified");
+    block_on(wait_for_verify(
+        sent_txs,
+        Duration::from_secs(test_spec.verify_timeout_sec),
+        &rpc_addr,
+    ));
+    info!("loadtest completed.");
+}
+
 #[derive(Deserialize, Debug)]
 struct AccountInfo {
     pub address: String,
@@ -32,16 +61,16 @@ struct AccountInfo {
 
 #[derive(Deserialize)]
 struct TestSpec {
-    deposit_initial: u64,
-    n_deposits: i32,
-    deposit_from_amount: u64,
-    deposit_to_amount: u64,
-    n_transfers: i32,
-    transfer_from_amount: u64,
-    transfer_to_amount: u64,
-    n_withdraws: i32,
-    withdraw_from_amount: u64,
-    withdraw_to_amount: u64,
+    deposit_initial_gwei: u64,
+    n_deposits: u32,
+    deposit_from_amount_gwei: u64,
+    deposit_to_amount_gwei: u64,
+    n_transfers: u32,
+    transfer_from_amount_gwei: u64,
+    transfer_to_amount_gwei: u64,
+    n_withdraws: u32,
+    withdraw_from_amount_gwei: u64,
+    withdraw_to_amount_gwei: u64,
     verify_timeout_sec: u64,
     input_accounts: Vec<AccountInfo>,
 }
@@ -52,19 +81,13 @@ struct TestAccount {
     eth_nonce: Mutex<u32>,
 }
 
+#[derive(Default)]
 struct SentTransactions {
     op_serial_ids: Mutex<Vec<u32>>,
     tx_hashes: Mutex<Vec<TxHash>>,
 }
 
 impl SentTransactions {
-    fn new() -> Self {
-        Self {
-            op_serial_ids: Mutex::new(Vec::new()),
-            tx_hashes: Mutex::new(Vec::new()),
-        }
-    }
-
     fn add_op_id(&self, v: u32) {
         let mut vect = self.op_serial_ids.lock().unwrap();
         vect.push(v);
@@ -81,34 +104,9 @@ impl SentTransactions {
     }
 }
 
-fn main() {
-    env_logger::init();
-
-    let config = ConfigurationOptions::from_env();
-    let filepath = env::args().nth(1).expect("test spec file not given");
-    let test_spec = read_test_spec(filepath);
-    let (_el, transport) = Http::new(&config.web3_url).expect("http transport start");
-    let test_accounts = Arc::new(construct_test_accounts(
-        &test_spec.input_accounts,
-        transport,
-        &config,
-    ));
-    info!("sending transactions");
-    let sent_txs = block_on(send_transactions(&test_accounts, &test_spec));
-    info!("waiting for all transactions to be verified");
-    block_on(wait_for_verify(
-        sent_txs,
-        Duration::from_secs(test_spec.verify_timeout_sec),
-    ));
-    info!("loadtest completed.");
-}
-
 // reads accounts from a file.
 fn read_test_spec(filepath: String) -> TestSpec {
-    let mut f = File::open(filepath).expect("no input file");
-    let mut buffer = String::new();
-    f.read_to_string(&mut buffer)
-        .expect("failed to read accounts");
+    let buffer = std::fs::read_to_string(filepath).expect("failed to read file");
     serde_json::from_str(&buffer).expect("failed to parse accounts")
 }
 
@@ -148,13 +146,17 @@ fn construct_test_accounts(
 }
 
 // sends confugured deposits, withdraws and transfers from each account concurrently.
-async fn send_transactions(test_accounts: &[TestAccount], ctx: &TestSpec) -> SentTransactions {
-    let sent_txs = SentTransactions::new();
+async fn send_transactions(
+    test_accounts: &[TestAccount],
+    ctx: &TestSpec,
+    rpc_addr: &str,
+) -> SentTransactions {
+    let sent_txs = SentTransactions::default();
     try_join_all(
         test_accounts
             .iter()
             .enumerate()
-            .map(|(i, _)| send_transactions_from_acc(i, &test_accounts, &ctx, &sent_txs))
+            .map(|(i, _)| send_transactions_from_acc(i, &test_accounts, &ctx, &sent_txs, rpc_addr))
             .collect::<Vec<_>>(),
     )
     .await
@@ -168,25 +170,32 @@ async fn send_transactions_from_acc(
     test_accounts: &[TestAccount],
     ctx: &TestSpec,
     sent_txs: &SentTransactions,
+    rpc_addr: &str,
 ) -> Result<(), failure::Error> {
     let test_acc = &test_accounts[index];
     let addr_hex = hex::encode(test_acc.eth_acc.address);
     update_eth_nonce(test_acc).await?;
-    let op_id = deposit_single(test_acc, BigDecimal::from(ctx.deposit_initial)).await?;
+    let wei_in_gwei = BigDecimal::from(1_000_000_000);
+    let op_id = deposit_single(
+        test_acc,
+        BigDecimal::from(ctx.deposit_initial_gwei).mul(&wei_in_gwei),
+        rpc_addr,
+    )
+    .await?;
     info!("account {} made initial deposit", addr_hex);
     sent_txs.add_op_id(op_id);
-    change_pubkey(test_acc)?;
+    change_pubkey(test_acc, rpc_addr)?;
     let futs_deposits = try_join_all((0..ctx.n_deposits).map(|_i| {
-        let amount = rand_amount(ctx.deposit_from_amount, ctx.deposit_to_amount);
-        deposit_single(test_acc, amount)
+        let amount = rand_amount(ctx.deposit_from_amount_gwei, ctx.deposit_to_amount_gwei);
+        deposit_single(test_acc, amount.mul(&wei_in_gwei), rpc_addr)
     }));
     let futs_withdraws = try_join_all((0..ctx.n_withdraws).map(|_i| {
-        let amount = rand_amount(ctx.withdraw_from_amount, ctx.withdraw_to_amount);
-        withdraw_single(test_acc, amount)
+        let amount = rand_amount(ctx.withdraw_from_amount_gwei, ctx.withdraw_to_amount_gwei);
+        withdraw_single(test_acc, amount.mul(&wei_in_gwei), rpc_addr)
     }));
     let futs_transfers = try_join_all((0..ctx.n_transfers).map(|_i| {
-        let amount = rand_amount(ctx.transfer_from_amount, ctx.transfer_to_amount);
-        transfer_single(index, test_accounts, amount)
+        let amount = rand_amount(ctx.transfer_from_amount_gwei, ctx.transfer_to_amount_gwei);
+        transfer_single(index, test_accounts, amount.mul(&wei_in_gwei), rpc_addr)
     }));
     let (deposit_ids, withdraw_hashes, transfer_hashes) =
         try_join!(futs_deposits, futs_withdraws, futs_transfers)?;
@@ -202,9 +211,8 @@ async fn send_transactions_from_acc(
 
 // generates random amount for transaction within given range [from, to).
 fn rand_amount(from: u64, to: u64) -> BigDecimal {
-    let smallest_rand_step = 100_000_000;
     let amount = rand::thread_rng().gen_range(from, to);
-    BigDecimal::from(amount - amount % smallest_rand_step)
+    BigDecimal::from(amount)
 }
 
 // updates current ethereum nonces from eth node.
@@ -220,16 +228,20 @@ async fn update_eth_nonce(test_acc: &TestAccount) -> Result<(), failure::Error> 
     Ok(())
 }
 
-fn change_pubkey(ta: &TestAccount) -> Result<TxHash, failure::Error> {
-    send_tx(FranklinTx::ChangePubKey(Box::new(
-        ta.zk_acc.create_change_pubkey_tx(None, true, false),
-    )))
+fn change_pubkey(ta: &TestAccount, rpc_addr: &str) -> Result<TxHash, failure::Error> {
+    send_tx(
+        FranklinTx::ChangePubKey(Box::new(
+            ta.zk_acc.create_change_pubkey_tx(None, true, false),
+        )),
+        rpc_addr,
+    )
 }
 
 // deposits to contract and waits for node to execute it.
 async fn deposit_single(
     test_acc: &TestAccount,
     deposit_amount: BigDecimal,
+    rpc_addr: &str,
 ) -> Result<u32, failure::Error> {
     let nonce = {
         let mut n = test_acc.eth_nonce.lock().unwrap();
@@ -242,10 +254,11 @@ async fn deposit_single(
         .await?;
     let mut executed = false;
     // 5 min wait
-    let n_checks = 5 * 60;
+    let start = Instant::now();
+    let timeout = Duration::from_secs(DEPOSIT_TIMEOUT_SEC);
     let check_period = Duration::from_secs(1);
-    for _i in 0..n_checks {
-        let (ex, _) = ethop_info(po.serial_id).await?;
+    while start.elapsed() < timeout {
+        let (ex, _) = ethop_info(po.serial_id, rpc_addr).await?;
         if ex {
             executed = true;
             break;
@@ -262,6 +275,7 @@ async fn deposit_single(
 async fn withdraw_single(
     test_acc: &TestAccount,
     amount: BigDecimal,
+    rpc_addr: &str,
 ) -> Result<TxHash, failure::Error> {
     let tx = FranklinTx::Withdraw(Box::new(test_acc.zk_acc.sign_withdraw(
         0, // ETH
@@ -271,7 +285,7 @@ async fn withdraw_single(
         None,
         true,
     )));
-    send_tx(tx)
+    send_tx(tx, rpc_addr)
 }
 
 // sends transfer tx to a random receiver.
@@ -279,6 +293,7 @@ async fn transfer_single(
     index_from: usize,
     test_accounts: &[TestAccount],
     amount: BigDecimal,
+    rpc_addr: &str,
 ) -> Result<TxHash, failure::Error> {
     let from = &test_accounts[index_from];
     let step = rand::thread_rng().gen_range(1, test_accounts.len());
@@ -291,7 +306,7 @@ async fn transfer_single(
         None,
         true,
     )));
-    send_tx(tx)
+    send_tx(tx, rpc_addr)
 }
 
 #[derive(Serialize)]
@@ -314,13 +329,13 @@ impl SubmitTxMsg {
 }
 
 // sends tx to server json rpc endpoint.
-fn send_tx(tx: FranklinTx) -> Result<TxHash, failure::Error> {
+fn send_tx(tx: FranklinTx, rpc_addr: &str) -> Result<TxHash, failure::Error> {
     let tx_hash = tx.hash();
     let msg = SubmitTxMsg::new(tx);
 
     let client = reqwest::Client::new();
     let mut res = client
-        .post("http://localhost:3030")
+        .post(rpc_addr)
         .json(&msg)
         .send()
         .expect("failed to submit tx");
@@ -332,13 +347,13 @@ fn send_tx(tx: FranklinTx) -> Result<TxHash, failure::Error> {
 }
 
 // waits for all priority operations and transactions to become part of some block and get verified.
-async fn wait_for_verify(sent_txs: SentTransactions, timeout: Duration) {
+async fn wait_for_verify(sent_txs: SentTransactions, timeout: Duration, rpc_addr: &str) {
     let start = Instant::now();
     let serial_ids = sent_txs.op_serial_ids.lock().unwrap();
     let sleep_period = Duration::from_millis(500);
     for &id in serial_ids.iter() {
         loop {
-            let (executed, verified) = ethop_info(id as u64)
+            let (executed, verified) = ethop_info(id as u64, rpc_addr)
                 .await
                 .expect("[wait_for_verify] call ethop_info");
             if executed && verified {
@@ -354,7 +369,7 @@ async fn wait_for_verify(sent_txs: SentTransactions, timeout: Duration) {
     let tx_hashes = sent_txs.tx_hashes.lock().unwrap();
     for hash in tx_hashes.iter() {
         loop {
-            let verified = tx_info(hash.clone())
+            let verified = tx_info(hash.clone(), rpc_addr)
                 .await
                 .expect("[wait_for_verify] call tx_info");
             if verified {
@@ -389,12 +404,12 @@ impl EthopInfo {
 }
 
 // requests and returns a tuple (executed, verified) for operation with given serial_id
-async fn ethop_info(serial_id: u64) -> Result<(bool, bool), failure::Error> {
+async fn ethop_info(serial_id: u64, rpc_addr: &str) -> Result<(bool, bool), failure::Error> {
     let msg = EthopInfo::new(serial_id);
 
     let client = reqwest::Client::new();
     let mut res = client
-        .post("http://localhost:3030")
+        .post(rpc_addr)
         .json(&msg)
         .send()
         .expect("failed to send ethop_info");
@@ -436,12 +451,12 @@ impl TxInfo {
 }
 
 // requests and returns whether transaction is verified or not.
-async fn tx_info(tx_hash: TxHash) -> Result<bool, failure::Error> {
+async fn tx_info(tx_hash: TxHash, rpc_addr: &str) -> Result<bool, failure::Error> {
     let msg = TxInfo::new(tx_hash);
 
     let client = reqwest::Client::new();
     let mut res = client
-        .post("http://localhost:3030")
+        .post(rpc_addr)
         .json(&msg)
         .send()
         .expect("failed to send tx_info");
