52b57395d0ef7b0deb29a7f854841f4b9e354cd2
Igor Aleksanov <popzxc@yandex.ru>
2020-03-04 08:05:55 +0300

    Use fragile access in non-crucial contexts

--- core/storage/src/lib.rs
+++ core/storage/src/lib.rs
@@ -71,6 +71,14 @@ impl ConnectionPool {
         Self { pool }
     }
 
+    /// Creates a `StorageProcessor` entity over a recoverable connection.
+    /// Upon a database outage connection will block the thread until
+    /// it will be able to recover the connection (or, if connection cannot
+    /// be restored after several retries, this will be considered as
+    /// irrecoverable database error and result in panic).
+    ///
+    /// This method is intended to be used in crucial contexts, where the
+    /// database access is must-have (e.g. block committer).
     pub fn access_storage(&self) -> Result<StorageProcessor, PoolError> {
         let connection = self.pool.get()?;
         connection.deref().enable_retrying();
@@ -78,6 +86,9 @@ impl ConnectionPool {
         Ok(StorageProcessor::from_pool(connection))
     }
 
+    /// Creates a `StorageProcessor` entity using non-recoverable connection, which
+    /// will not handle the database outages. This method is intended to be used in
+    /// non-crucial contexts, such as API endpoint handlers.
     pub fn access_storage_fragile(&self) -> Result<StorageProcessor, PoolError> {
         let connection = self.pool.get()?;
         connection.deref().disable_retrying();
