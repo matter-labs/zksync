f9d2ac8a458a127effa208293babe780e3fa46ba
Igor Aleksanov <popzxc@yandex.ru>
2020-03-10 14:49:57 +0300

    Add doc-comments for account module

--- core/storage/src/chain/account/mod.rs
+++ core/storage/src/chain/account/mod.rs
@@ -17,11 +17,14 @@ mod stored_state;
 pub(crate) use self::restore_account::restore_account;
 pub use self::stored_state::StoredAccountState;
 
+/// Account schema contains interfaces to interact with the stored
+/// ZKSync accounts.
 pub struct AccountSchema<'a>(pub &'a StorageProcessor);
 
 impl<'a> AccountSchema<'a> {
-    // Verified, commited states.
+    /// Obtains both committed and verified state for the account by its address.
     pub fn account_state_by_address(&self, address: &Address) -> QueryResult<StoredAccountState> {
+        // Find the account in `account_creates` table.
         let account_create_record = account_creates::table
             .filter(account_creates::address.eq(address.as_bytes().to_vec()))
             .filter(account_creates::is_create.eq(true))
@@ -29,6 +32,8 @@ impl<'a> AccountSchema<'a> {
             .first::<StorageAccountCreation>(self.0.conn())
             .optional()?;
 
+        // If account wasn't found, we return no state for it.
+        // Otherwise we obtain the account ID for the state lookup.
         let account_id = if let Some(account_create_record) = account_create_record {
             account_create_record.account_id as AccountId
         } else {
@@ -38,6 +43,7 @@ impl<'a> AccountSchema<'a> {
             });
         };
 
+        // Load committed & verified states, and return them.
         let committed = self
             .last_committed_state_for_account(account_id)?
             .map(|a| (account_id, a));
@@ -50,53 +56,73 @@ impl<'a> AccountSchema<'a> {
         })
     }
 
+    /// Loads the last committed (e.g. just added but no necessarily verified) state for
+    /// account given its ID.
     pub fn last_committed_state_for_account(
         &self,
         account_id: AccountId,
     ) -> QueryResult<Option<Account>> {
-        self.0.conn().transaction(|| {
-            let (last_block, account) = self.get_account_and_last_block(account_id)?;
+        // Get the last certain state of the account.
+        // Note that `account` can be `None` here (if it wasn't verified yet), since
+        // we will update the committed changes below.
+        let (last_block, account) = self.get_account_and_last_block(account_id)?;
 
-            let account_balance_diff: Vec<StorageAccountUpdate> = {
-                account_balance_updates::table
-                    .filter(account_balance_updates::account_id.eq(&(i64::from(account_id))))
-                    .filter(account_balance_updates::block_number.gt(&last_block))
-                    .load::<StorageAccountUpdate>(self.0.conn())?
-            };
+        // Collect the diffs that we have to apply to the account.
+        let (account_balance_diff, account_creation_diff) = self
+            .0
+            .conn()
+            .transaction::<_, diesel::result::Error, _>(|| {
+                // From `account_balance_updates` load entries with the same ID and height
+                // greater than for the last verified block.
+                let account_balance_diff: Vec<StorageAccountUpdate> = {
+                    account_balance_updates::table
+                        .filter(account_balance_updates::account_id.eq(&(i64::from(account_id))))
+                        .filter(account_balance_updates::block_number.gt(&last_block))
+                        .load::<StorageAccountUpdate>(self.0.conn())?
+                };
 
-            let account_creation_diff: Vec<StorageAccountCreation> = {
-                account_creates::table
-                    .filter(account_creates::account_id.eq(&(i64::from(account_id))))
-                    .filter(account_creates::block_number.gt(&last_block))
-                    .load::<StorageAccountCreation>(self.0.conn())?
-            };
+                // The same as above, but for `account_creates` table.
+                let account_creation_diff: Vec<StorageAccountCreation> = {
+                    account_creates::table
+                        .filter(account_creates::account_id.eq(&(i64::from(account_id))))
+                        .filter(account_creates::block_number.gt(&last_block))
+                        .load::<StorageAccountCreation>(self.0.conn())?
+                };
 
-            let account_diff = {
-                let mut account_diff = Vec::new();
-                account_diff.extend(
-                    account_balance_diff
-                        .into_iter()
-                        .map(StorageAccountDiff::from),
-                );
-                account_diff.extend(
-                    account_creation_diff
-                        .into_iter()
-                        .map(StorageAccountDiff::from),
-                );
-                account_diff.sort_by(StorageAccountDiff::cmp_order);
-                account_diff
+                Ok((account_balance_diff, account_creation_diff))
+            })?;
+
+        // Chain the diffs, converting them into `StorageAccountDiff`.
+        let account_diff = {
+            let mut account_diff = Vec::new();
+            account_diff.extend(
+                account_balance_diff
                     .into_iter()
-                    .map(|upd| upd.into())
-                    .collect::<AccountUpdates>()
-            };
-
-            Ok(account_diff
+                    .map(StorageAccountDiff::from),
+            );
+            account_diff.extend(
+                account_creation_diff
+                    .into_iter()
+                    .map(StorageAccountDiff::from),
+            );
+            account_diff.sort_by(StorageAccountDiff::cmp_order);
+            account_diff
                 .into_iter()
-                .map(|(_, upd)| upd)
-                .fold(account, Account::apply_update))
-        })
+                .map(Into::into)
+                .collect::<AccountUpdates>()
+        };
+
+        // Apply all the diffs to obtain the most recent account state.
+        let account_state = account_diff
+            .into_iter()
+            .map(|(_, upd)| upd)
+            .fold(account, Account::apply_update);
+
+        Ok(account_state)
     }
 
+    /// Loads the last verified state for the account (e.g. the one obtained in the last block
+    /// which was both committed and verified).
     pub fn last_verified_state_for_account(
         &self,
         account_id: AccountId,
@@ -105,10 +131,13 @@ impl<'a> AccountSchema<'a> {
         Ok(account)
     }
 
+    /// Obtains the last verified state of the account.
     fn get_account_and_last_block(
         &self,
         account_id: AccountId,
     ) -> QueryResult<(i64, Option<Account>)> {
+        // `accounts::table` is updated only after the block verification, so we should
+        // just load the account with the provided ID.
         let maybe_account = self.0.conn().transaction(|| {
             accounts::table
                 .find(i64::from(account_id))
