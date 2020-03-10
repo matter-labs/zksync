1d96dd9a2178fc083e649d6b34e88907328f2680
Igor Aleksanov <popzxc@yandex.ru>
2020-03-10 15:24:57 +0300

    Add comments for the prover module

--- core/storage/src/prover/mod.rs
+++ core/storage/src/prover/mod.rs
@@ -12,21 +12,31 @@ use crate::StorageProcessor;
 
 pub mod records;
 
+/// Prover schema is capable of handling the prover-related informations,
+/// such as started prover jobs, registered provers and proofs for blocks.
 pub struct ProverSchema<'a>(pub &'a StorageProcessor);
 
 impl<'a> ProverSchema<'a> {
+    /// Given the block size, chooses the next block to prove for the certain prover.
+    /// Returns `None` if either there are no blocks of given size to prove, or
+    /// there is already an ongoing job for non-proved block.
     pub fn prover_run_for_next_commit(
         &self,
         worker_: &str,
         prover_timeout: time::Duration,
         block_size: usize,
     ) -> QueryResult<Option<ProverRun>> {
+        // Select the block to prove.
         let job: Option<BlockNumber> = self
             .0
             .conn()
             .transaction(|| {
                 sql_query("LOCK TABLE prover_runs IN EXCLUSIVE MODE").execute(self.0.conn())?;
 
+                // Find the block that satisfies the following criteria:
+                // - Block number is greater than the index of last verified block.
+                // - There is no proof for block.
+                // - Either there is no ongoing job for the block, or the job exceeded the timeout.
                 let query = format!(" \
                     WITH unsized_blocks AS ( \
                         SELECT * FROM operations o \
@@ -46,10 +56,13 @@ impl<'a> ProverSchema<'a> {
                     prover_timeout_secs=prover_timeout.as_secs(), block_size=block_size
                 );
 
+                // Return the index of such a block.
                 diesel::sql_query(query).get_result::<Option<IntegerNumber>>(self.0.conn())
             })?
             .map(|i| i.integer_value as BlockNumber);
 
+        // If there is a block to prove, create a job and store it
+        // in the `prover_runs` table; otherwise do nothing and return `None`.
         if let Some(block_number_) = job {
             use crate::schema::prover_runs::dsl::*;
             let inserted: ProverRun = insert_into(prover_runs)
@@ -64,6 +77,7 @@ impl<'a> ProverSchema<'a> {
         }
     }
 
+    /// Updates the state of ongoing prover job.
     pub fn record_prover_is_working(&self, job_id: i32) -> QueryResult<()> {
         use crate::schema::prover_runs::dsl::*;
 
@@ -74,6 +88,7 @@ impl<'a> ProverSchema<'a> {
             .map(|_| ())
     }
 
+    /// Adds a prover to the database.
     pub fn register_prover(&self, worker_: &str, block_size_: usize) -> QueryResult<i32> {
         use crate::schema::active_provers::dsl::*;
         let inserted: ActiveProver = insert_into(active_provers)
@@ -85,6 +100,7 @@ impl<'a> ProverSchema<'a> {
         Ok(inserted.id)
     }
 
+    /// Gets a prover descriptor by its numeric ID.
     pub fn prover_by_id(&self, prover_id: i32) -> QueryResult<ActiveProver> {
         use crate::schema::active_provers::dsl::*;
 
@@ -94,6 +110,7 @@ impl<'a> ProverSchema<'a> {
         Ok(ret)
     }
 
+    /// Marks the prover as stopped.
     pub fn record_prover_stop(&self, prover_id: i32) -> QueryResult<()> {
         // FIXME(popzxc): It seems that it isn't actually checked if the prover has been stopped
         // anywhere. And also it doesn't seem that prover can be restored from the stopped
@@ -108,7 +125,7 @@ impl<'a> ProverSchema<'a> {
             .map(|_| ())
     }
 
-    /// Store the timestamp of the prover finish and the proof
+    /// Stores the proof for a block.
     pub fn store_proof(
         &self,
         block_number: BlockNumber,
@@ -122,6 +139,7 @@ impl<'a> ProverSchema<'a> {
         insert_into(proofs).values(&to_store).execute(self.0.conn())
     }
 
+    /// Gets the stored proof for a block.
     pub fn load_proof(&self, block_number: BlockNumber) -> QueryResult<EncodedProof> {
         use crate::schema::proofs::dsl;
         let stored: StoredProof = dsl::proofs
