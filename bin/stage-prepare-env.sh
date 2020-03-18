11257068c4eebe49deb01d4cb1c4aed3f90be238
furkhat <vailodf@gmail.com>
2020-03-18 16:17:54 +0200

    set stage keys dir and block size chunks

--- bin/stage-prepare-env.sh
+++ bin/stage-prepare-env.sh
@@ -10,11 +10,12 @@ python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE ETH_NETWORK=rin
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE DATABASE_URL=$1
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE WEB3_URL=$2
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE API_SERVER=https://stage-api.zksync.dev
-python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE BLOCK_SIZE_CHUNKS=50
+python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE BLOCK_CHUNK_SIZES=6,12
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE ACCOUNT_TREE_DEPTH=16
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE ZKSYNC_ACTION=dont_ask
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE PROVER_SERVER_URL=http://stage-server:8088
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE KEYS_SPACE_URL=https://keys-multitoken.fra1.digitaloceanspaces.com/stage
 python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE CONTRACT_ADDR=$3
+python3 $ZKSYNC_HOME/bin/replace-env-variable.py $STAGE_ENV_FILE KEY_DIR=stage
 
 zksync env stage
