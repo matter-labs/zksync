5e77769ee70c5cc32ff4b661d7602ef085ee01e4
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-03-20 14:08:56 +0200

    wait 30 confirmations

--- core/server/src/eth_watch.rs
+++ core/server/src/eth_watch.rs
@@ -1,3 +1,11 @@
+//! Ethereum watcher polls ethereum node for new events
+//! such as PriorityQueue events or NewToken events.
+//! New events are accepted to the ZK Sync network only after sufficient confirmations.
+//!
+//! Poll interval is configured using `ETH_POLL_INTERVAL` constant.
+//! Number of confirmations are configured using `CONFIRMATIONS_FOR_ETH_EVENT`.
+//!
+
 // Built-in deps
 use std::collections::HashMap;
 use std::convert::TryFrom;
@@ -16,11 +24,13 @@ use web3::{Transport, Web3};
 use models::abi::{governance_contract, zksync_contract};
 use models::config_options::ConfigurationOptions;
 use models::node::{Nonce, PriorityOp, PubKeyHash, TokenId};
-use models::params::PRIORITY_EXPIRATION;
+use models::params::{CONFIRMATIONS_FOR_ETH_EVENT, PRIORITY_EXPIRATION};
 use models::TokenAddedEvent;
 use storage::ConnectionPool;
 use tokio::{runtime::Runtime, time};
 use web3::transports::EventLoopHandle;
+
+const ETH_POLL_INTERVAL: Duration = Duration::from_secs(3);
 
 pub enum EthWatchRequest {
     PollETHNode,
@@ -40,7 +50,8 @@ pub enum EthWatchRequest {
 pub struct EthWatch<T: Transport> {
     gov_contract: (ethabi::Contract, Contract<T>),
     zksync_contract: (ethabi::Contract, Contract<T>),
-    processed_block: u64,
+    /// The last block of the ethereum network known to ethereum watcher.
+    last_ethereum_block: u64,
     eth_state: ETHState,
     web3: Web3<T>,
     _web3_event_loop_handle: EventLoopHandle,
@@ -87,7 +98,7 @@ impl<T: Transport> EthWatch<T> {
         Self {
             gov_contract,
             zksync_contract,
-            processed_block: 0,
+            last_ethereum_block: 0,
             eth_state: ETHState {
                 tokens: HashMap::new(),
                 priority_queue: HashMap::new(),
@@ -170,12 +181,17 @@ impl<T: Transport> EthWatch<T> {
             .collect()
     }
 
-    async fn restore_state_from_eth(&mut self, block: u64) {
+    async fn restore_state_from_eth(&mut self, current_ethereum_block: u64) {
+        let new_block_with_accepted_events =
+            current_ethereum_block.saturating_sub(CONFIRMATIONS_FOR_ETH_EVENT);
+        let previous_block_with_accepted_events =
+            new_block_with_accepted_events.saturating_sub(PRIORITY_EXPIRATION);
+
         // restore priority queue
         let prior_queue_events = self
             .get_priority_op_events(
-                BlockNumber::Number(block.saturating_sub(PRIORITY_EXPIRATION)),
-                BlockNumber::Number(block),
+                BlockNumber::Number(previous_block_with_accepted_events),
+                BlockNumber::Number(new_block_with_accepted_events),
             )
             .await
             .expect("Failed to restore priority queue events from ETH");
@@ -187,7 +203,10 @@ impl<T: Transport> EthWatch<T> {
 
         // restore token list from governance contract
         let new_tokens = self
-            .get_new_token_events(BlockNumber::Earliest, BlockNumber::Number(block))
+            .get_new_token_events(
+                BlockNumber::Earliest,
+                BlockNumber::Number(new_block_with_accepted_events),
+            )
             .await
             .expect("Failed to restore token list from ETH");
         for token in new_tokens.into_iter() {
@@ -198,19 +217,24 @@ impl<T: Transport> EthWatch<T> {
         trace!("ETH state: {:#?}", self.eth_state);
     }
 
-    async fn process_new_blocks(&mut self, last_block: u64) -> Result<(), failure::Error> {
-        debug_assert!(self.processed_block < last_block);
+    async fn process_new_blocks(&mut self, current_eth_block: u64) -> Result<(), failure::Error> {
+        debug_assert!(self.last_ethereum_block < current_eth_block);
+
+        let previous_block_with_accepted_events =
+            (self.last_ethereum_block + 1).saturating_sub(CONFIRMATIONS_FOR_ETH_EVENT);
+        let new_block_with_accepted_events =
+            current_eth_block.saturating_sub(CONFIRMATIONS_FOR_ETH_EVENT);
 
         let new_tokens = self
             .get_new_token_events(
-                BlockNumber::Number(self.processed_block + 1),
-                BlockNumber::Number(last_block),
+                BlockNumber::Number(previous_block_with_accepted_events),
+                BlockNumber::Number(new_block_with_accepted_events),
             )
             .await?;
         let priority_op_events = self
             .get_priority_op_events(
-                BlockNumber::Number(self.processed_block + 1),
-                BlockNumber::Number(last_block),
+                BlockNumber::Number(previous_block_with_accepted_events),
+                BlockNumber::Number(new_block_with_accepted_events),
             )
             .await?;
 
@@ -225,7 +249,7 @@ impl<T: Transport> EthWatch<T> {
             self.eth_state
                 .add_new_token(token.id as TokenId, token.address);
         }
-        self.processed_block = last_block;
+        self.last_ethereum_block = current_eth_block;
 
         Ok(())
     }
@@ -297,8 +321,9 @@ impl<T: Transport> EthWatch<T> {
             .await
             .expect("Block number")
             .as_u64();
-        self.processed_block = block;
-        self.restore_state_from_eth(block).await;
+        self.last_ethereum_block = block;
+        self.restore_state_from_eth(block.saturating_sub(CONFIRMATIONS_FOR_ETH_EVENT as u64))
+            .await;
 
         while let Some(request) = self.eth_watch_req.next().await {
             match request {
@@ -310,7 +335,7 @@ impl<T: Transport> EthWatch<T> {
                         continue;
                     };
 
-                    if block > self.processed_block {
+                    if block > self.last_ethereum_block {
                         self.process_new_blocks(block)
                             .await
                             .map_err(|e| warn!("Failed to process new blocks {}", e))
@@ -365,7 +390,7 @@ pub fn start_eth_watch(
     runtime.spawn(eth_watch.run());
 
     runtime.spawn(async move {
-        let mut timer = time::interval(Duration::from_secs(5));
+        let mut timer = time::interval(ETH_POLL_INTERVAL);
 
         loop {
             timer.tick().await;
