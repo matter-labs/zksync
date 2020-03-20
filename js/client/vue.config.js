e27a02647119ec66e243b10817c03bd04fdcc044
Oleg Syniakevych <olegsyniakevych@gmail.com>
2020-03-20 18:48:43 +0200

    Use link for zksync-crypto in zksync.js

--- js/client/vue.config.js
+++ js/client/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/client/'
-        : '/',    
+        : '/',
     chainWebpack: config => {
         config.optimization.minimize(process.env.NODE_ENV === 'production');
         config.resolve.symlinks(false);
