0d00799023cf7ca13a05dadbbc1366272254491c
Ihor Barenblat <ib@matterlabs.dev>
2020-03-22 17:02:07 +0200

    Moved Upgradeable.sol into Proxy.sol

--- contracts/contracts/Proxy.sol
+++ contracts/contracts/Proxy.sol
@@ -1,17 +1,62 @@
 pragma solidity 0.5.16;
 
-import "./Upgradeable.sol";
+import "./Ownable.sol";
 
 
 /// @title Proxy Contract
 /// @author Matter Labs
-contract Proxy is Upgradeable {
+contract Proxy is Ownable {
+
+    /// @notice Storage position of "target" (actual implementation address)
+    bytes32 private constant targetPosition = keccak256("target");
 
     /// @notice Contract constructor
-    /// @dev Calls Upgradeable contract constructor
+    /// @dev Calls Ownable contract constructor and initialize target
     /// @param target Initial implementation address
     /// @param targetInitializationParameters Target initialization parameters
-    constructor(address target, bytes memory targetInitializationParameters) Upgradeable(target, targetInitializationParameters) public {}
+    constructor(address target, bytes memory targetInitializationParameters) Ownable(msg.sender) public {
+        setTarget(target);
+        (bool initializationSuccess, ) = getTarget().delegatecall(
+            abi.encodeWithSignature("initialize(bytes)", targetInitializationParameters)
+        );
+        require(initializationSuccess, "uin11"); // uin11 - target initialization failed
+    }
+
+    /// @notice Intercepts initialization calls
+    function initialize(bytes calldata) external pure {
+        revert("ini11"); // ini11 - interception of initialization call
+    }
+
+    /// @notice Returns target of contract
+    /// @return Actual implementation address
+    function getTarget() public view returns (address target) {
+        bytes32 position = targetPosition;
+        assembly {
+            target := sload(position)
+        }
+    }
+
+    /// @notice Sets new target of contract
+    /// @param _newTarget New actual implementation address
+    function setTarget(address _newTarget) internal {
+        bytes32 position = targetPosition;
+        assembly {
+            sstore(position, _newTarget)
+        }
+    }
+
+    /// @notice Upgrades target
+    /// @param newTarget New target
+    /// @param newTargetInitializationParameters New target initialization parameters
+    function upgradeTarget(address newTarget, bytes calldata newTargetInitializationParameters) external {
+        requireMaster(msg.sender);
+
+        setTarget(newTarget);
+        (bool initializationSuccess, ) = getTarget().delegatecall(
+            abi.encodeWithSignature("initialize(bytes)", newTargetInitializationParameters)
+        );
+        require(initializationSuccess, "ufu11"); // ufu11 - target initialization failed
+    }
 
     /// @notice Performs a delegatecall to the contract implementation
     /// @dev Fallback function allowing to perform a delegatecall to the given implementation
