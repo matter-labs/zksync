78bba681a37d2af4f16c9cb3690d9c112049b963
Igor Aleksanov <popzxc@yandex.ru>
2020-03-31 15:54:25 +0300

    Add benches for primitives

--- core/models/benches/criterion/primitives/mod.rs
+++ core/models/benches/criterion/primitives/mod.rs
@@ -1,5 +1,118 @@
-use criterion::{criterion_group, Criterion};
+// External uses
+use bigdecimal::BigDecimal;
+use criterion::{black_box, criterion_group, BatchSize, Bencher, Criterion, Throughput};
+// Local uses
+use models::primitives::{
+    big_decimal_to_u128, bytes_into_be_bits, get_bits_le_fixed_u128, pack_bits_into_bytes,
+    pack_bits_into_bytes_in_order, u128_to_bigdecimal, BitIteratorLe, GetBits,
+};
 
-pub fn bench_primitives(_c: &mut Criterion) {}
+/// Input size for byte slices (module-wide for calculating the throughput).
+const BYTE_SLICE_SIZE: usize = 512;
+
+fn bench_u64_get_bits_le(b: &mut Bencher<'_>) {
+    let value: u64 = 0xDEAD_BEEF_DEAD_BEEF;
+
+    b.iter(|| {
+        let _ = black_box(value).get_bits_le();
+    });
+}
+
+fn bench_get_bits_le_fixed_u128(b: &mut Bencher<'_>) {
+    let value: u128 = 0xDEAD_BEEF_DEAD_BEEF_DEAD_BEEF_DEAD_BEEF;
+    let n = 128;
+
+    b.iter(|| {
+        let _ = get_bits_le_fixed_u128(black_box(value), n);
+    });
+}
+
+fn bench_big_decimal_to_u128(b: &mut Bencher<'_>) {
+    let value: BigDecimal = 0x0EAD_BEEF.into();
+
+    b.iter(|| {
+        let _ = big_decimal_to_u128(&black_box(value.clone()));
+    });
+}
+
+fn bench_u128_to_bigdecimal(b: &mut Bencher<'_>) {
+    let value: u128 = 0xDEAD_BEEF_DEAD_BEEF_DEAD_BEEF_DEAD_BEEF;
+
+    b.iter(|| {
+        let _ = u128_to_bigdecimal(black_box(value));
+    });
+}
+
+fn bench_bytes_into_be_bits(b: &mut Bencher<'_>) {
+    let value: Vec<u8> = vec![0xAB; BYTE_SLICE_SIZE];
+
+    let value_ref: &[u8] = value.as_ref();
+
+    b.iter(|| {
+        let _ = bytes_into_be_bits(black_box(value_ref));
+    });
+}
+
+fn bench_pack_bits_into_bytes(b: &mut Bencher<'_>) {
+    let value: Vec<bool> = vec![true; BYTE_SLICE_SIZE * 8];
+
+    let setup = || value.clone();
+
+    b.iter_batched(
+        setup,
+        |value| {
+            let _ = pack_bits_into_bytes(black_box(value));
+        },
+        BatchSize::SmallInput,
+    );
+}
+
+fn bench_pack_bits_into_bytes_in_order(b: &mut Bencher<'_>) {
+    let value: Vec<bool> = vec![true; BYTE_SLICE_SIZE * 8];
+
+    let setup = || value.clone();
+
+    b.iter_batched(
+        setup,
+        |value| {
+            let _ = pack_bits_into_bytes_in_order(black_box(value));
+        },
+        BatchSize::SmallInput,
+    );
+}
+
+fn bench_bit_iterator_le_next(b: &mut Bencher<'_>) {
+    let value: Vec<u64> = vec![0xDEAD_BEEF_DEAD_BEEF; BYTE_SLICE_SIZE / 8];
+
+    let setup = || BitIteratorLe::new(&value);
+
+    b.iter_batched(
+        setup,
+        |mut bit_iterator| {
+            while let Some(_) = bit_iterator.next() {
+                // Do nothing, we're just draining the iterator.
+            }
+        },
+        BatchSize::SmallInput,
+    );
+}
+
+pub fn bench_primitives(c: &mut Criterion) {
+    c.bench_function("u64_get_bits_le", bench_u64_get_bits_le);
+    c.bench_function("get_bits_le_fixed_u128", bench_get_bits_le_fixed_u128);
+    c.bench_function("big_decimal_to_u128", bench_big_decimal_to_u128);
+    c.bench_function("u128_to_bigdecimal", bench_u128_to_bigdecimal);
+
+    let mut group = c.benchmark_group("Bit Converters");
+
+    group.throughput(Throughput::Bytes(BYTE_SLICE_SIZE as u64));
+    group.bench_function("bytes_into_be_bits", bench_bytes_into_be_bits);
+    group.bench_function("pack_bits_into_bytes", bench_pack_bits_into_bytes);
+    group.bench_function(
+        "pack_bits_into_bytes_in_order",
+        bench_pack_bits_into_bytes_in_order,
+    );
+    group.bench_function("BitIterator::next", bench_bit_iterator_le_next);
+}
 
 criterion_group!(primitives_benches, bench_primitives);
