f5b429312f0f22dc425ad2b73a69948e584591e0
furkhat <vailodf@gmail.com>
2020-04-09 00:41:18 +0300

    election routine syntax fixes

--- core/server/src/leader_election.rs
+++ core/server/src/leader_election.rs
@@ -1,9 +1,9 @@
 //! Leader election is a always live routine that continuously votes to become the leader.
 
-use std::time::Instant;
-use std::thread;
-use std::sync::mpsc;
 use models::node::config::LEADER_ELECTION_ITERVAL;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Instant;
 
 /// Continuously votes to be the leader and exits when it becomes the leader.
 /// Voting happens with `LEADER_ELECTION_ITERVAL`.
@@ -16,12 +16,18 @@ pub fn vote_to_be_the_leader(
     name: String,
     connection_pool: storage::ConnectionPool,
 ) -> Result<(), failure::Error> {
-    let mut won_election = false;
-    for !won_election {
+    let st = connection_pool
+        .access_storage()
+        .map_err(|e| failure::format_err!("could not to access store: {}", e))?;
+    loop {
+        let won_election = st
+            .leader_election_schema()
+            .vote(&name)
+            .map_err(|e| failure::format_err!("could not to vote for leader: {}", e))?;
+        if won_election {
+            break;
+        }
         thread::sleep(LEADER_ELECTION_ITERVAL);
-        let st = connection_pool.access_storage().map_err(|e| failure::format_err!("could not to access store: {}", e))?;
-        won_election = st.leader_election_schema()
-                .vote(&name)
-                .map_err(|e| failure::format_err!("could not to vote for leader: {}", e))?;
     }
+    Ok(())
 }
--- core/server/src/lib.rs
+++ core/server/src/lib.rs
@@ -10,9 +10,10 @@ pub mod block_proposer;
 pub mod committer;
 pub mod eth_sender;
 pub mod eth_watch;
+pub mod leader_election;
 pub mod mempool;
+pub mod observer_mode;
 pub mod prover_server;
 pub mod state_keeper;
-pub mod observer_mode;
 
 use crypto_exports::franklin_crypto;
