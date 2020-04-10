17c17045a0d2e6c6f7b056cbdace1d4d980d4c69
Igor Aleksanov <popzxc@yandex.ru>
2020-04-10 15:57:59 +0300

    Add echoes into dummy-prover-disable.sh

--- bin/dummy-prover-disable.sh
+++ bin/dummy-prover-disable.sh
@@ -3,8 +3,12 @@
 # Set the `DUMMY_VERIFIER` constant value in the contract to `false`.
 ssed -E "s/(.*constant DUMMY_VERIFIER)(.*)\;/\1 = false\;/" -i $ZKSYNC_HOME/contracts/contracts/Verifier.sol
 
+echo "Disabled the Dummy Prover in the contract..."
+
 # Reset the database and redeploy contracts.
 zksync build-contracts   
 zksync db-reset
 zksync genesis
 zksync redeploy
+
+echo "All done"
