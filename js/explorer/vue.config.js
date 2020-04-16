fabad184ff64db14804dc9bc526056b8a4a06379
Oleg Syniakevych <olegsyniakevych@gmail.com>
2020-04-16 13:14:15 +0300

    Change base url in explorer

--- js/explorer/vue.config.js
+++ js/explorer/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/explorer/'
+        ? '/'
         : '/',
     chainWebpack: config => {
         config.optimization.minimize(process.env.NODE_ENV === 'production');
