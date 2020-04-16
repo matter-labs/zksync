d9fe002a9441a38995925ad220f962b7725bace4
Oleg Syniakevych <olegsyniakevych@gmail.com>
2020-04-16 19:36:44 +0300

    Remove redirect from nginx.conf

--- js/explorer/vue.config.js
+++ js/explorer/vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/'
-        : '/',
+    publicPath: '/',
     chainWebpack: config => {
         config.optimization.minimize(process.env.NODE_ENV === 'production');
         config.resolve.symlinks(false);
