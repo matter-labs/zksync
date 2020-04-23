fadc3fe1d09c92487913e7ccca1f59d5dfc6e834
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-04-24 01:33:29 +0300

    Fix full exit, add test

--- core/circuit/src/witness/full_exit.rs
+++ core/circuit/src/witness/full_exit.rs
@@ -16,6 +16,7 @@ pub struct FullExitData {
     pub token: u32,
     pub account_address: u32,
     pub eth_address: Fr,
+    pub full_exit_amount: Fr,
 }
 pub struct FullExitWitness<E: JubjubEngine> {
     pub before: OperationBranch<E>,
@@ -68,8 +69,12 @@ pub fn apply_full_exit_tx(
         token: u32::from(full_exit.priority_op.token),
         account_address: full_exit.priority_op.account_id,
         eth_address: eth_address_to_fr(&full_exit.priority_op.eth_address),
+        full_exit_amount: full_exit
+            .withdraw_amount
+            .clone()
+            .map(|amount| Fr::from_str(&amount.to_string()).unwrap())
+            .unwrap_or_else(Fr::zero),
     };
-
     // le_bit_vector_into_field_element()
     apply_full_exit(tree, &full_exit, is_success)
 }
@@ -88,22 +93,6 @@ pub fn apply_full_exit(
     assert_eq!(capacity, 1 << franklin_constants::account_tree_depth());
     let account_address_fe = Fr::from_str(&full_exit.account_address.to_string()).unwrap();
     let token_fe = Fr::from_str(&full_exit.token.to_string()).unwrap();
-
-    //applying full_exit
-    let amount_to_exit = {
-        let (_, _, balance, _) = apply_leaf_operation(
-            tree,
-            full_exit.account_address,
-            full_exit.token,
-            |_| {},
-            |_| {},
-        );
-        if is_success {
-            balance
-        } else {
-            Fr::zero()
-        }
-    };
 
     let (account_witness_before, account_witness_after, balance_before, balance_after) = {
         if is_success {
@@ -159,7 +148,7 @@ pub fn apply_full_exit(
         args: OperationArguments {
             eth_address: Some(full_exit.eth_address),
             amount_packed: Some(Fr::zero()),
-            full_amount: Some(amount_to_exit),
+            full_amount: Some(full_exit.full_exit_amount),
             fee: Some(Fr::zero()),
             pub_nonce: Some(Fr::zero()),
             a: Some(a),
@@ -306,6 +295,107 @@ mod test {
         plasma_state.apply_full_exit_op(&full_exit_op);
         println!("node root hash after op: {:?}", plasma_state.root_hash());
 
+        let full_exit_witness =
+            apply_full_exit_tx(&mut witness_accum.account_tree, &full_exit_op, false);
+        let full_exit_operations = calculate_full_exit_operations_from_witness(&full_exit_witness);
+        let pubdata_from_witness = full_exit_witness.get_pubdata();
+
+        witness_accum.add_operation_with_pubdata(full_exit_operations, pubdata_from_witness);
+        witness_accum.collect_fees(&[]);
+        witness_accum.calculate_pubdata_commitment();
+
+        assert_eq!(
+            plasma_state.root_hash(),
+            witness_accum
+                .root_after_fees
+                .expect("witness accum after root hash empty"),
+            "root hash in state keeper and witness generation code mismatch"
+        );
+
+        check_circuit(witness_accum.into_circuit_instance());
+    }
+
+    #[test]
+    #[ignore]
+    #[should_panic(expected = "chunk number 0/execute_op/op_valid")]
+    fn test_full_exit_success_but_with_wrong_amount_in_pubdata_panic() {
+        let zksync_account = ZksyncAccount::rand();
+        let account_id = 1;
+        let account_address = zksync_account.address;
+        let account = {
+            let mut account = Account::default_with_address(&account_address);
+            account.add_balance(0, &BigDecimal::from(10));
+            account.pub_key_hash = zksync_account.pubkey_hash;
+            account
+        };
+
+        let (mut plasma_state, mut circuit_account_tree) =
+            test_genesis_plasma_state(vec![(account_id, account)]);
+        let fee_account_id = 0;
+        let mut witness_accum = WitnessBuilder::new(&mut circuit_account_tree, fee_account_id, 1);
+
+        let mut full_exit_op = FullExitOp {
+            priority_op: FullExit {
+                account_id,
+                eth_address: account_address,
+                token: 0,
+            },
+            withdraw_amount: Some(BigDecimal::from(10)),
+        };
+
+        println!("node root hash before op: {:?}", plasma_state.root_hash());
+        plasma_state.apply_full_exit_op(&full_exit_op);
+        println!("node root hash after op: {:?}", plasma_state.root_hash());
+
+        // here we try to withdraw more funds
+        full_exit_op.withdraw_amount = Some(BigDecimal::from(20));
+
+        let full_exit_witness =
+            apply_full_exit_tx(&mut witness_accum.account_tree, &full_exit_op, true);
+        let full_exit_operations = calculate_full_exit_operations_from_witness(&full_exit_witness);
+        let pubdata_from_witness = full_exit_witness.get_pubdata();
+
+        witness_accum.add_operation_with_pubdata(full_exit_operations, pubdata_from_witness);
+        witness_accum.collect_fees(&[]);
+        witness_accum.calculate_pubdata_commitment();
+
+        assert_eq!(
+            plasma_state.root_hash(),
+            witness_accum
+                .root_after_fees
+                .expect("witness accum after root hash empty"),
+            "root hash in state keeper and witness generation code mismatch"
+        );
+
+        check_circuit(witness_accum.into_circuit_instance());
+    }
+
+    #[test]
+    #[ignore]
+    #[should_panic(expected = "chunk number 0/execute_op/op_valid")]
+    fn test_full_exit_failure_but_with_wrong_amount_in_pubdata_panic() {
+        let zksync_account = ZksyncAccount::rand();
+        let account_id = 1;
+        let account_address = zksync_account.address;
+
+        let (mut plasma_state, mut circuit_account_tree) = test_genesis_plasma_state(Vec::new());
+        let fee_account_id = 0;
+        let mut witness_accum = WitnessBuilder::new(&mut circuit_account_tree, fee_account_id, 1);
+
+        let mut full_exit_op = FullExitOp {
+            priority_op: FullExit {
+                account_id,
+                eth_address: account_address,
+                token: 0,
+            },
+            withdraw_amount: None,
+        };
+
+        println!("node root hash before op: {:?}", plasma_state.root_hash());
+        plasma_state.apply_full_exit_op(&full_exit_op);
+        println!("node root hash after op: {:?}", plasma_state.root_hash());
+
+        full_exit_op.withdraw_amount = Some(10.into());
         let full_exit_witness =
             apply_full_exit_tx(&mut witness_accum.account_tree, &full_exit_op, false);
         let full_exit_operations = calculate_full_exit_operations_from_witness(&full_exit_witness);
