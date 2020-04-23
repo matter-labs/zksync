bb495db3186245bd4d1df30aba6385162a6e5a5c
Igor Aleksanov <popzxc@yandex.ru>
2020-04-23 07:25:46 +0300

    Update core/server/src/utils/shared_lru_cache.rs

--- core/server/src/utils/shared_lru_cache.rs
+++ core/server/src/utils/shared_lru_cache.rs
@@ -2,9 +2,12 @@ use lru_cache::LruCache;
 use std::hash::Hash;
 use std::sync::{Arc, Mutex};
 
-/// SharedLruCache is a analog of LruCache
-/// SharedLruCache can be shared between threads safely
-/// The interface differs from LruCache in getter function - here it returns clone of result value
+/// `SharedLruCache` is an thread-safe alternative of the `LruCache`.
+/// Unlike the `LruCache`, getter method returns a cloned value instead of the reference to
+/// fulfill the thread safety requirements.
+///
+/// Note that this structure uses `Mutex` internally, so it is not recommended to use it in
+/// single-threaded environment.
 #[derive(Clone, Debug)]
 pub struct SharedLruCache<K: Eq + Hash, V: Clone>(Arc<Mutex<LruCache<K, V>>>);
 
