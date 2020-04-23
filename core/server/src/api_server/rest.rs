40d9c8bda2c22c482ae566b08963ff9c6717e4c0
Igor Aleksanov <popzxc@yandex.ru>
2020-04-23 13:00:00 +0300

    Move caches set into a separate structure

--- core/server/src/api_server/rest.rs
+++ core/server/src/api_server/rest.rs
@@ -29,14 +29,32 @@ impl SharedNetworkStatus {
     }
 }
 
+/// Caches used by REST API server.
+#[derive(Debug, Clone)]
+struct Caches {
+    pub transaction_receipts: SharedLruCache<Vec<u8>, TxReceiptResponse>,
+    pub priority_op_receipts: SharedLruCache<u32, PriorityOpReceiptResponse>,
+    pub block_executed_ops: SharedLruCache<u32, Vec<ExecutedOperations>>,
+    pub blocks_info: SharedLruCache<u32, BlockDetails>,
+    pub blocks_by_height_or_hash: SharedLruCache<String, BlockDetails>,
+}
+
+impl Caches {
+    pub fn new(caches_size: usize) -> Self {
+        Self {
+            transaction_receipts: SharedLruCache::new(caches_size),
+            priority_op_receipts: SharedLruCache::new(caches_size),
+            block_executed_ops: SharedLruCache::new(caches_size),
+            blocks_info: SharedLruCache::new(caches_size),
+            blocks_by_height_or_hash: SharedLruCache::new(caches_size),
+        }
+    }
+}
+
 /// AppState is a collection of records cloned by each thread to shara data between them
 #[derive(Clone)]
 struct AppState {
-    cache_of_transaction_receipts: SharedLruCache<Vec<u8>, TxReceiptResponse>,
-    cache_of_priority_op_receipts: SharedLruCache<u32, PriorityOpReceiptResponse>,
-    cache_of_block_executed_ops: SharedLruCache<u32, Vec<ExecutedOperations>>,
-    cache_of_blocks_info: SharedLruCache<u32, BlockDetails>,
-    cache_blocks_by_height_or_hash: SharedLruCache<String, BlockDetails>,
+    caches: Caches,
     connection_pool: ConnectionPool,
     network_status: SharedNetworkStatus,
     contract_address: String,
@@ -107,27 +125,28 @@ impl AppState {
         &self,
         transaction_hash: Vec<u8>,
     ) -> Result<Option<TxReceiptResponse>, actix_web::error::Error> {
-        let res =
-            if let Some(tx_receipt) = self.cache_of_transaction_receipts.get(&transaction_hash) {
-                Some(tx_receipt)
-            } else {
-                let storage = self.access_storage()?;
-                let tx_receipt = storage
-                    .chain()
-                    .operations_ext_schema()
-                    .tx_receipt(transaction_hash.as_slice())
-                    .unwrap_or(None);
+        let res = if let Some(tx_receipt) = self.caches.transaction_receipts.get(&transaction_hash)
+        {
+            Some(tx_receipt)
+        } else {
+            let storage = self.access_storage()?;
+            let tx_receipt = storage
+                .chain()
+                .operations_ext_schema()
+                .tx_receipt(transaction_hash.as_slice())
+                .unwrap_or(None);
 
-                if let Some(tx_receipt) = tx_receipt.clone() {
-                    // Unverified blocks can still change, so we can't cache them.
-                    if tx_receipt.verified {
-                        self.cache_of_transaction_receipts
-                            .insert(transaction_hash, tx_receipt);
-                    }
+            if let Some(tx_receipt) = tx_receipt.clone() {
+                // Unverified blocks can still change, so we can't cache them.
+                if tx_receipt.verified {
+                    self.caches
+                        .transaction_receipts
+                        .insert(transaction_hash, tx_receipt);
                 }
+            }
 
-                tx_receipt
-            };
+            tx_receipt
+        };
         Ok(res)
     }
 
@@ -135,7 +154,7 @@ impl AppState {
         &self,
         id: u32,
     ) -> Result<PriorityOpReceiptResponse, actix_web::error::Error> {
-        let res = if let Some(receipt) = self.cache_of_priority_op_receipts.get(&id) {
+        let res = if let Some(receipt) = self.caches.priority_op_receipts.get(&id) {
             receipt
         } else {
             let storage = self.access_storage()?;
@@ -147,8 +166,7 @@ impl AppState {
 
             // Unverified blocks can still change, so we can't cache them.
             if receipt.verified {
-                self.cache_of_priority_op_receipts
-                    .insert(id, receipt.clone());
+                self.caches.priority_op_receipts.insert(id, receipt.clone());
             }
 
             receipt
@@ -160,7 +178,7 @@ impl AppState {
         &self,
         block_id: u32,
     ) -> Result<Vec<ExecutedOperations>, actix_web::error::Error> {
-        let res = if let Some(executed_ops) = self.cache_of_block_executed_ops.get(&block_id) {
+        let res = if let Some(executed_ops) = self.caches.block_executed_ops.get(&block_id) {
             executed_ops
         } else {
             let storage = self.access_storage()?;
@@ -174,7 +192,8 @@ impl AppState {
             {
                 // Unverified blocks can still change, so we can't cache them.
                 if !block_details.is_empty() && block_details[0].verified_at.is_some() {
-                    self.cache_of_block_executed_ops
+                    self.caches
+                        .block_executed_ops
                         .insert(block_id, executed_ops.clone());
                 }
             }
@@ -188,7 +207,7 @@ impl AppState {
         &self,
         block_id: u32,
     ) -> Result<Option<BlockDetails>, actix_web::error::Error> {
-        let res = if let Some(block) = self.cache_of_blocks_info.get(&block_id) {
+        let res = if let Some(block) = self.caches.blocks_info.get(&block_id) {
             Some(block)
         } else {
             let storage = self.access_storage()?;
@@ -199,8 +218,7 @@ impl AppState {
                 .map_err(|_| HttpResponse::InternalServerError().finish())?;
 
             if !blocks.is_empty() && blocks[0].verified_at.is_some() {
-                self.cache_of_blocks_info
-                    .insert(block_id, blocks[0].clone());
+                self.caches.blocks_info.insert(block_id, blocks[0].clone());
             }
 
             blocks.pop()
@@ -212,7 +230,7 @@ impl AppState {
         &self,
         query: String,
     ) -> Result<Option<BlockDetails>, actix_web::error::Error> {
-        let res = if let Some(block) = self.cache_blocks_by_height_or_hash.get(&query) {
+        let res = if let Some(block) = self.caches.blocks_by_height_or_hash.get(&query) {
             Some(block)
         } else {
             let storage = self.access_storage()?;
@@ -223,7 +241,7 @@ impl AppState {
 
             if let Some(block) = block.clone() {
                 if block.verified_at.is_some() {
-                    self.cache_blocks_by_height_or_hash.insert(query, block);
+                    self.caches.blocks_by_height_or_hash.insert(query, block);
                 }
             }
 
@@ -599,11 +617,7 @@ pub(super) fn start_server_thread_detached(
             let runtime = actix_rt::System::new("api-server");
 
             let state = AppState {
-                cache_of_transaction_receipts: SharedLruCache::new(api_requests_caches_size),
-                cache_of_priority_op_receipts: SharedLruCache::new(api_requests_caches_size),
-                cache_of_block_executed_ops: SharedLruCache::new(api_requests_caches_size),
-                cache_of_blocks_info: SharedLruCache::new(api_requests_caches_size),
-                cache_blocks_by_height_or_hash: SharedLruCache::new(api_requests_caches_size),
+                caches: Caches::new(api_requests_caches_size),
                 connection_pool,
                 network_status: SharedNetworkStatus::default(),
                 contract_address: format!("{:?}", contract_address),
