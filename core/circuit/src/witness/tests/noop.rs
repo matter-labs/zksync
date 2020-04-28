dd7f7273ee435c67214eadb328c6c854cc92207a
Igor Aleksanov <popzxc@yandex.ru>
2020-04-28 13:42:58 +0300

    Visually simplify the noop test

--- core/circuit/src/witness/tests/noop.rs
+++ core/circuit/src/witness/tests/noop.rs
@@ -44,39 +44,17 @@ fn generate_keys(
     pub_key_hash
 }
 
-/// Low-level test for circuit based on a non-operation execution.
-/// In this test we manually create an account tree, add a validator account with non-zero balance
-/// in tokens, and apply a `noop` operation to this account.
-/// After that, we check that circuit doesn't contain any unsatisfied constraints.
-#[test]
-#[ignore]
-fn test_noop() {
-    // Cryptographic utilities initialization.
-    let jubjub_params = &AltJubjubBn256::new();
-    let rescue_params = &Bn256RescueParams::new_2_into_1::<BlakeHasher>();
-    let p_g = FixedGenerators::SpendingKeyGenerator;
-    let rng = &mut XorShiftRng::from_seed([0x3dbe_6258, 0x8d31_3d76, 0x3237_db17, 0xe5bc_0654]);
-    let phasher = RescueHasher::<Bn256>::default();
-
-    // Account tree, which we'll manually fill
-    let mut tree: CircuitAccountTree = CircuitAccountTree::new(params::account_tree_depth());
-
-    // We'll create one block with number 1
-    let block_number = Fr::from_str("1").unwrap();
-
+fn insert_validator(
+    tree: &mut CircuitAccountTree,
+    rng: &mut XorShiftRng,
+    p_g: FixedGenerators,
+    jubjub_params: &AltJubjubBn256,
+    phasher: &RescueHasher<Bn256>,
+) -> (u32, Fr, Vec<Option<Fr>>) {
     // Validator account credentials
     let validator_address_number = 7;
     let validator_address = Fr::from_str(&validator_address_number.to_string()).unwrap();
     let validator_pub_key_hash = generate_keys(rng, p_g, &jubjub_params, &phasher);
-
-    // Sender account credentials.
-    let sender_address: u32 = rng.gen::<u32>() % tree.capacity() as u32;
-    let sender_balance_token_id: u32 = 2;
-    let sender_balance_value: u128 = 2000;
-    let sender_balance = Fr::from_str(&sender_balance_value.to_string()).unwrap();
-    let sender_pub_key_hash = generate_keys(rng, p_g, &jubjub_params, &phasher);
-
-    // Give some funds to sender and make zero balance for recipient (validator account)
 
     // Create a validator account as an account tree leaf.
     let validator_leaf = CircuitAccount::<Bn256> {
@@ -89,6 +67,29 @@ fn test_noop() {
     // Initialize all the validator balances as 0.
     let empty_balance = Some(Fr::zero());
     let validator_balances = vec![empty_balance; params::total_tokens()];
+
+    // Insert account into tree.
+    tree.insert(validator_address_number, validator_leaf);
+
+    (
+        validator_address_number,
+        validator_address,
+        validator_balances,
+    )
+}
+
+fn insert_sender(
+    tree: &mut CircuitAccountTree,
+    rng: &mut XorShiftRng,
+    p_g: FixedGenerators,
+    jubjub_params: &AltJubjubBn256,
+    phasher: &RescueHasher<Bn256>,
+) {
+    let sender_address: u32 = rng.gen::<u32>() % tree.capacity() as u32;
+    let sender_balance_token_id: u32 = 2;
+    let sender_balance_value: u128 = 2000;
+    let sender_balance = Fr::from_str(&sender_balance_value.to_string()).unwrap();
+    let sender_pub_key_hash = generate_keys(rng, p_g, &jubjub_params, &phasher);
 
     // Create a sender account as an account tree leaf.
     // Balance tree of this account will only contain one token with non-zero amount of funds.
@@ -108,8 +109,36 @@ fn test_noop() {
     };
 
     // Insert both accounts into a tree.
-    tree.insert(validator_address_number, validator_leaf);
     tree.insert(sender_address, sender_leaf_initial);
+}
+
+/// Low-level test for circuit based on a non-operation execution.
+/// In this test we manually create an account tree, add a validator account,
+/// an account with a non-zero balance in tokens, and apply
+/// a `noop` operation to the validator account.
+/// After that, we check that circuit doesn't contain any unsatisfied constraints.
+#[test]
+#[ignore]
+fn test_noop() {
+    // Cryptographic utilities initialization.
+    let jubjub_params = &AltJubjubBn256::new();
+    let rescue_params = &Bn256RescueParams::new_2_into_1::<BlakeHasher>();
+    let p_g = FixedGenerators::SpendingKeyGenerator;
+    let rng = &mut XorShiftRng::from_seed([0x3dbe_6258, 0x8d31_3d76, 0x3237_db17, 0xe5bc_0654]);
+    let phasher = RescueHasher::<Bn256>::default();
+
+    // Account tree, which we'll manually fill
+    let mut tree: CircuitAccountTree = CircuitAccountTree::new(params::account_tree_depth());
+
+    // We'll create one block with number 1
+    let block_number = Fr::from_str("1").unwrap();
+
+    // Validator account credentials
+    let (validator_address_number, validator_address, validator_balances) =
+        insert_validator(&mut tree, rng, p_g, &jubjub_params, &phasher);
+
+    // Insert sender into a tree.
+    insert_sender(&mut tree, rng, p_g, &jubjub_params, &phasher);
 
     // Perform the `noop` operation and collect the data required for circuit instance creation.
     let operation = noop_operation(&tree, validator_address_number);
