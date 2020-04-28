0ead29012465138864eb9bba334fa7370f80e915
Igor Aleksanov <popzxc@yandex.ru>
2020-04-28 10:42:46 +0300

    Add more positive tests for transfers

--- core/circuit/src/witness/tests/transfer.rs
+++ core/circuit/src/witness/tests/transfer.rs
@@ -15,43 +15,55 @@ use crate::witness::{
 #[test]
 #[ignore]
 fn test_transfer_success() {
-    // Input data.
-    let accounts = vec![
-        WitnessTestAccount::new(1, 10),
-        WitnessTestAccount::new_empty(2),
+    // Test vector of (initial_balance, transfer_amount, fee_amount).
+    let test_vector = vec![
+        (10, 7, 3),                // Basic transfer
+        (0, 0, 0),                 // Zero transfer
+        (std::u64::MAX, 1, 1),     // Small transfer from rich account,
+        (std::u64::MAX, 10000, 1), // Big transfer from rich account (too big values can't be used, since they're not packable),
+        (std::u64::MAX, 1, 10000), // Very big fee
     ];
-    let (account_from, account_to) = (&accounts[0], &accounts[1]);
-    let transfer_op = TransferOp {
-        tx: account_from
-            .zksync_account
-            .sign_transfer(
-                0,
-                "",
-                BigDecimal::from(7),
-                BigDecimal::from(3),
-                &account_to.account.address,
-                None,
-                true,
-            )
-            .0,
-        from: account_from.id,
-        to: account_to.id,
-    };
 
-    // Additional data required for performing the operation.
-    let input = SigDataInput::from_transfer_op(&transfer_op).expect("SigDataInput creation failed");
+    for (initial_balance, transfer_amount, fee_amount) in test_vector {
+        // Input data.
+        let accounts = vec![
+            WitnessTestAccount::new(1, initial_balance),
+            WitnessTestAccount::new_empty(2),
+        ];
+        let (account_from, account_to) = (&accounts[0], &accounts[1]);
+        let transfer_op = TransferOp {
+            tx: account_from
+                .zksync_account
+                .sign_transfer(
+                    0,
+                    "",
+                    BigDecimal::from(transfer_amount),
+                    BigDecimal::from(fee_amount),
+                    &account_to.account.address,
+                    None,
+                    true,
+                )
+                .0,
+            from: account_from.id,
+            to: account_to.id,
+        };
 
-    generic_test_scenario::<TransferWitness<Bn256>, _>(
-        &accounts,
-        transfer_op,
-        input,
-        |plasma_state, op| {
-            let (fee, _) = plasma_state
-                .apply_transfer_op(&op)
-                .expect("transfer should be success");
-            vec![fee]
-        },
-    );
+        // Additional data required for performing the operation.
+        let input =
+            SigDataInput::from_transfer_op(&transfer_op).expect("SigDataInput creation failed");
+
+        generic_test_scenario::<TransferWitness<Bn256>, _>(
+            &accounts,
+            transfer_op,
+            input,
+            |plasma_state, op| {
+                let (fee, _) = plasma_state
+                    .apply_transfer_op(&op)
+                    .expect("transfer should be success");
+                vec![fee]
+            },
+        );
+    }
 }
 
 /// Check for execution of `Transfer` operation with recipient same as sender in circuit.
--- core/circuit/src/witness/tests/transfer_to_new.rs
+++ core/circuit/src/witness/tests/transfer_to_new.rs
@@ -15,40 +15,51 @@ use crate::witness::{
 #[test]
 #[ignore]
 fn test_transfer_to_new_success() {
-    // Input data.
-    let accounts = vec![WitnessTestAccount::new(1, 10)];
-    let account_from = &accounts[0];
-    let account_to = WitnessTestAccount::new_empty(2); // Will not be included into state.
-    let transfer_op = TransferToNewOp {
-        tx: account_from
-            .zksync_account
-            .sign_transfer(
-                0,
-                "",
-                BigDecimal::from(7),
-                BigDecimal::from(3),
-                &account_to.account.address,
-                None,
-                true,
-            )
-            .0,
-        from: account_from.id,
-        to: account_to.id,
-    };
+    // Test vector of (initial_balance, transfer_amount, fee_amount).
+    let test_vector = vec![
+        (10, 7, 3),                // Basic transfer
+        (0, 0, 0),                 // Zero transfer
+        (std::u64::MAX, 1, 1),     // Small transfer from rich account,
+        (std::u64::MAX, 10000, 1), // Big transfer from rich account (too big values can't be used, since they're not packable),
+        (std::u64::MAX, 1, 10000), // Very big fee
+    ];
 
-    // Additional data required for performing the operation.
-    let input =
-        SigDataInput::from_transfer_to_new_op(&transfer_op).expect("SigDataInput creation failed");
+    for (initial_balance, transfer_amount, fee_amount) in test_vector {
+        // Input data.
+        let accounts = vec![WitnessTestAccount::new(1, initial_balance)];
+        let account_from = &accounts[0];
+        let account_to = WitnessTestAccount::new_empty(2); // Will not be included into state.
+        let transfer_op = TransferToNewOp {
+            tx: account_from
+                .zksync_account
+                .sign_transfer(
+                    0,
+                    "",
+                    BigDecimal::from(transfer_amount),
+                    BigDecimal::from(fee_amount),
+                    &account_to.account.address,
+                    None,
+                    true,
+                )
+                .0,
+            from: account_from.id,
+            to: account_to.id,
+        };
 
-    generic_test_scenario::<TransferToNewWitness<Bn256>, _>(
-        &accounts,
-        transfer_op,
-        input,
-        |plasma_state, op| {
-            let (fee, _) = plasma_state
-                .apply_transfer_to_new_op(&op)
-                .expect("transfer should be success");
-            vec![fee]
-        },
-    );
+        // Additional data required for performing the operation.
+        let input = SigDataInput::from_transfer_to_new_op(&transfer_op)
+            .expect("SigDataInput creation failed");
+
+        generic_test_scenario::<TransferToNewWitness<Bn256>, _>(
+            &accounts,
+            transfer_op,
+            input,
+            |plasma_state, op| {
+                let (fee, _) = plasma_state
+                    .apply_transfer_to_new_op(&op)
+                    .expect("transfer should be success");
+                vec![fee]
+            },
+        );
+    }
 }
