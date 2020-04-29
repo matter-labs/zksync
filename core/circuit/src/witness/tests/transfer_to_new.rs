6f0a5653d5916e0cdc871d48c6747ae0611f8236
Igor Aleksanov <popzxc@yandex.ru>
2020-04-29 08:52:57 +0300

    Add negative tests for transfer ops

--- core/circuit/src/witness/tests/transfer_to_new.rs
+++ core/circuit/src/witness/tests/transfer_to_new.rs
@@ -3,9 +3,13 @@ use bigdecimal::BigDecimal;
 use crypto_exports::franklin_crypto::bellman::pairing::bn256::Bn256;
 // Workspace deps
 use models::node::operations::TransferToNewOp;
+use plasma::state::CollectedFee;
 // Local deps
 use crate::witness::{
-    tests::test_utils::{corrupted_input_test_scenario, generic_test_scenario, WitnessTestAccount},
+    tests::test_utils::{
+        corrupted_input_test_scenario, generic_test_scenario, incorrect_op_test_scenario,
+        WitnessTestAccount,
+    },
     transfer_to_new::TransferToNewWitness,
     utils::SigDataInput,
 };
@@ -111,6 +115,174 @@ fn corrupted_ops_input() {
                     .apply_transfer_to_new_op(&op)
                     .expect("transfer should be success");
                 vec![fee]
+            },
+        );
+    }
+}
+
+/// Checks that executing a transfer operation with incorrect
+/// data (account `from` ID) results in an error.
+#[test]
+#[ignore]
+fn test_incorrect_transfer_account_from() {
+    const TOKEN_ID: u16 = 0;
+    const INITIAL_BALANCE: u64 = 10;
+    const TOKEN_AMOUNT: u64 = 7;
+    const FEE_AMOUNT: u64 = 3;
+
+    // Operation is not valid, since `from` ID is different from the tx body.
+    const ERR_MSG: &str = "op_valid is true/enforce equal to one";
+
+    let incorrect_from_account = WitnessTestAccount::new(3, INITIAL_BALANCE);
+
+    // Input data: transaction is signed by an incorrect account (address of account
+    // and ID of the `from` accounts differ).
+    let accounts = vec![WitnessTestAccount::new(1, INITIAL_BALANCE)];
+    let account_from = &accounts[0];
+    let account_to = WitnessTestAccount::new_empty(2); // Will not be included into state.
+    let transfer_op = TransferToNewOp {
+        tx: incorrect_from_account
+            .zksync_account
+            .sign_transfer(
+                TOKEN_ID,
+                "",
+                BigDecimal::from(TOKEN_AMOUNT),
+                BigDecimal::from(FEE_AMOUNT),
+                &account_to.account.address,
+                None,
+                true,
+            )
+            .0,
+        from: account_from.id,
+        to: account_to.id,
+    };
+
+    let input =
+        SigDataInput::from_transfer_to_new_op(&transfer_op).expect("SigDataInput creation failed");
+
+    incorrect_op_test_scenario::<TransferToNewWitness<Bn256>, _>(
+        &accounts,
+        transfer_op,
+        input,
+        ERR_MSG,
+        || {
+            vec![CollectedFee {
+                token: TOKEN_ID,
+                amount: FEE_AMOUNT.into(),
+            }]
+        },
+    );
+}
+
+/// Checks that executing a transfer operation with incorrect
+/// data (account `to` ID) results in an error.
+/// Tx should panic, since it is required that account to does not exist.
+#[test]
+#[ignore]
+#[should_panic(expected = "assertion failed: (acc.address == Fr::zero())")]
+fn test_incorrect_transfer_account_to() {
+    const TOKEN_ID: u16 = 0;
+    const INITIAL_BALANCE: u64 = 10;
+    const TOKEN_AMOUNT: u64 = 7;
+    const FEE_AMOUNT: u64 = 3;
+
+    // Error message doesn't really matter, since we expect test to panic.
+    const ERR_MSG: &str = "";
+
+    // Input data: account `to` exists.
+    let accounts = vec![
+        WitnessTestAccount::new(1, INITIAL_BALANCE),
+        WitnessTestAccount::new_empty(2),
+    ];
+    let (account_from, account_to) = (&accounts[0], &accounts[1]);
+    let transfer_op = TransferToNewOp {
+        tx: account_from
+            .zksync_account
+            .sign_transfer(
+                TOKEN_ID,
+                "",
+                BigDecimal::from(TOKEN_AMOUNT),
+                BigDecimal::from(FEE_AMOUNT),
+                &account_to.account.address,
+                None,
+                true,
+            )
+            .0,
+        from: account_from.id,
+        to: account_to.id,
+    };
+
+    let input =
+        SigDataInput::from_transfer_to_new_op(&transfer_op).expect("SigDataInput creation failed");
+
+    incorrect_op_test_scenario::<TransferToNewWitness<Bn256>, _>(
+        &accounts,
+        transfer_op,
+        input,
+        ERR_MSG,
+        || {
+            vec![CollectedFee {
+                token: TOKEN_ID,
+                amount: FEE_AMOUNT.into(),
+            }]
+        },
+    );
+}
+
+/// Checks that executing a transfer operation with incorrect
+/// data (insufficient funds) results in an error.
+#[test]
+#[ignore]
+fn test_incorrect_transfer_amount() {
+    const TOKEN_ID: u16 = 0;
+    // Balance check should fail.
+    // "balance-fee bits" is message for subtraction check in circuit.
+    // For details see `circuit.rs`.
+    const ERR_MSG: &str = "balance-fee bits";
+
+    // Test vector of (initial_balance, transfer_amount, fee_amount).
+    let test_vector = vec![
+        (10, 11, 3), // Transfer too big
+        (10, 7, 4),  // Fee to big
+        (0, 1, 1),   // Transfer from 0 balance
+    ];
+
+    for (initial_balance, transfer_amount, fee_amount) in test_vector {
+        // Input data: account does not have enough funds.
+        let accounts = vec![WitnessTestAccount::new(1, initial_balance)];
+        let account_from = &accounts[0];
+        let account_to = WitnessTestAccount::new_empty(2); // Will not be included into state.
+
+        let transfer_op = TransferToNewOp {
+            tx: account_from
+                .zksync_account
+                .sign_transfer(
+                    TOKEN_ID,
+                    "",
+                    BigDecimal::from(transfer_amount),
+                    BigDecimal::from(fee_amount),
+                    &account_to.account.address,
+                    None,
+                    true,
+                )
+                .0,
+            from: account_from.id,
+            to: account_to.id,
+        };
+
+        let input = SigDataInput::from_transfer_to_new_op(&transfer_op)
+            .expect("SigDataInput creation failed");
+
+        incorrect_op_test_scenario::<TransferToNewWitness<Bn256>, _>(
+            &accounts,
+            transfer_op,
+            input,
+            ERR_MSG,
+            || {
+                vec![CollectedFee {
+                    token: TOKEN_ID,
+                    amount: fee_amount.into(),
+                }]
             },
         );
     }
