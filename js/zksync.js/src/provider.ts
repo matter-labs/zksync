b3b58aa82aec8822911af4446c126bb4b79d4f1f
Igor Aleksanov <popzxc@yandex.ru>
2020-05-01 07:36:27 +0300

    Fix copy&paste error in zksync.js

--- js/zksync.js/src/provider.ts
+++ js/zksync.js/src/provider.ts
@@ -97,10 +97,8 @@ export class Provider {
         return await this.transport.request("ethop_info", [serialId]);
     }
 
-    async getConfirmationsForEthOpAmount(
-        serialId: number
-    ): Promise<PriorityOperationReceipt> {
-        return await this.transport.request("ethop_info", [serialId]);
+    async getConfirmationsForEthOpAmount(): Promise<number> {
+        return await this.transport.request("get_confirmations_for_eth_op_amount", []);
     }
 
     async notifyPriorityOp(
