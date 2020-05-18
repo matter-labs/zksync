7c13d8d272ee29e861c91c7ffab80faa2d6c3793
Oleg Syniakevych <olegsyniakevych@gmail.com>
2020-05-18 05:24:37 +0300

    Add comments to loggers.rs

--- core/server/src/api_server/loggers.rs
+++ core/server/src/api_server/loggers.rs
@@ -1,7 +1,13 @@
+//! Loggers for rest.rs, and HTTP/WS RPC.
+//!
+//! All logs report ip and country of the user, as well as headers in AUX_HEADERS constant.
+//! Rest logs also report request url, response duration in milliseconds,
+//! response code and size in bytes.
+//!
 //! To enable all logs, add to RUST_LOG env variable:
 //! actix_web=info,server::api_server::loggers=info
 //!
-//! You can also cherry-pick, like print only logs for websocket handshake
+//! You can also cherry-pick, e.g. print only logs for websocket handshake
 //! and not for http rpc or api_server:
 //! server::api_server::loggers::ws_rpc=info,
 
@@ -9,7 +15,7 @@
 /// This is the list of headers that are not too much relevant,
 /// but still will be printed at the end of log message.
 /// You can add headers to that constant, reorder them, delete them.
-const OTHER_HEADERS: [&str; 19] = [
+const AUX_HEADERS: [&str; 19] = [
     "user-agent",
     "cf-request-id",
     "host",
@@ -32,11 +38,11 @@ const OTHER_HEADERS: [&str; 19] = [
 ];
 
 pub mod rest {
-    use super::OTHER_HEADERS;
+    use super::AUX_HEADERS;
     use itertools::Itertools;
 
     pub fn get_logger_format() -> String {
-        let other_headers_formatted = OTHER_HEADERS
+        let aux_headers_formatted = AUX_HEADERS
             .iter()
             .map(|&h| format!("{}: \"%{{{}}}i\"", h, h))
             .join(", ");
@@ -46,20 +52,21 @@ pub mod rest {
         format!(
             "req {{ \"%r\" from %{{cf-connecting-ip}}i, %{{cf-ipcountry}}i }}, \
             resp {{ code: %s, bytes: %b, duration: %Dms }}, req headers {{ {} }}",
-            other_headers_formatted
+            aux_headers_formatted
         )
     }
 }
 
 pub mod http_rpc {
-    use super::OTHER_HEADERS;
+    use super::AUX_HEADERS;
     use itertools::Itertools;
-    use jsonrpc_http_server::hyper;
-    use jsonrpc_http_server::hyper::http::HeaderValue;
-    use jsonrpc_http_server::RequestMiddlewareAction;
+    use jsonrpc_http_server::{
+        hyper::{http::HeaderValue, Body, Request},
+        RequestMiddlewareAction,
+    };
     use log::Level;
 
-    pub fn request_middleware(request: hyper::Request<hyper::Body>) -> RequestMiddlewareAction {
+    pub fn request_middleware(request: Request<Body>) -> RequestMiddlewareAction {
         if log_enabled!(Level::Info) {
             let get_header = |header| {
                 request
@@ -71,7 +78,7 @@ pub mod http_rpc {
                     .unwrap_or("-")
             };
 
-            let other_headers_formatted = OTHER_HEADERS
+            let aux_headers_formatted = AUX_HEADERS
                 .iter()
                 .map(|&h| format!("{}: \"{}\"", h, get_header(h)))
                 .join(", ");
@@ -80,7 +87,7 @@ pub mod http_rpc {
                 "req from {}, {}, req headers {{ {} }}",
                 get_header("cf-connecting-ip"),
                 get_header("cf-ipcountry"),
-                other_headers_formatted,
+                aux_headers_formatted,
             );
         }
 
@@ -89,12 +96,11 @@ pub mod http_rpc {
 }
 
 pub mod ws_rpc {
-    use super::OTHER_HEADERS;
+    use super::AUX_HEADERS;
     use itertools::Itertools;
     use jsonrpc_ws_server::ws::{Request, Response};
     use log::Level;
-    use std::collections::HashMap;
-    use std::ops::Deref;
+    use std::{collections::HashMap, ops::Deref};
 
     pub fn request_middleware(request: &Request) -> Option<Response> {
         if log_enabled!(Level::Info) {
@@ -107,16 +113,16 @@ pub mod ws_rpc {
 
             let get_header = |header| headers.get(header).map(Deref::deref).unwrap_or("-");
 
-            let other_headers_formatted = OTHER_HEADERS
+            let aux_headers_formatted = AUX_HEADERS
                 .iter()
                 .map(|&h| format!("{}: \"{}\"", h, get_header(h)))
                 .join(", ");
 
             info!(
-                "handshake from {}, {}, other headers {{ {} }}",
+                "req from {}, {}, req headers {{ {} }}",
                 get_header("cf-connecting-ip"),
                 get_header("cf-ipcountry"),
-                other_headers_formatted,
+                aux_headers_formatted,
             );
         }
 
