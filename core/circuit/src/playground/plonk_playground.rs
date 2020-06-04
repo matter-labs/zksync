fdcac62fa8123152bd96e468c606e21df39abd03
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-06-04 17:06:12 +0300

    fix chunk-setup power mashup

--- core/circuit/src/playground/plonk_playground.rs
+++ core/circuit/src/playground/plonk_playground.rs
@@ -494,7 +494,7 @@ fn print_available_setup_powers() {
     };
 
     println!("chunks,gates,setup_power");
-    for chunk_range in (420..=720).step_by(2).collect::<Vec<_>>().chunks(32) {
+    for chunk_range in (400..=720).step_by(2).collect::<Vec<_>>().chunks(32) {
         let mut chunk_data = chunk_range
             .into_par_iter()
             .map(|chunk| {
@@ -508,7 +508,7 @@ fn print_available_setup_powers() {
             .find(|(_, _, setup_power)| *setup_power > 26)
             .is_some();
 
-        chunk_data.retain(|&(chunks, _, _)| chunks <= 26);
+        chunk_data.retain(|&(_, _, setup_power)| setup_power <= 26);
         for (chunks, gates, setup_power) in chunk_data {
             println!("{},{},{}", chunks, gates, setup_power);
         }
