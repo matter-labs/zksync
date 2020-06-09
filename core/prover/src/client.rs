87eda2b1921bed886574ad98251a17b8f32fe0d3
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-06-09 21:13:58 +0300

    Access storage fragile in prover, prover fails on timeout

--- core/prover/src/client.rs
+++ core/prover/src/client.rs
@@ -90,7 +90,10 @@ impl ApiClient {
                 )
             })
             .map_err(|e| match e {
-                backoff::Error::Permanent(e) | backoff::Error::Transient(e) => e,
+                backoff::Error::Permanent(e) | backoff::Error::Transient(e) => panic!(
+                    "Prover can't reach server, for the max elapsed time of the backoff: {}",
+                    e
+                ),
             })
     }
 
@@ -100,6 +103,7 @@ impl ApiClient {
         backoff.initial_interval = Duration::from_secs(1);
         backoff.multiplier = 1.5f64;
         backoff.max_interval = Duration::from_secs(10);
+        backoff.max_elapsed_time = Some(Duration::from_secs(2 * 60));
         backoff
     }
 
