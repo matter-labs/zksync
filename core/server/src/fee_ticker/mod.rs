17db68772a7b16b63cb24904304d183591b3929a
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-06-18 12:29:04 +0300

    subsidize transfer to new for a while

--- core/server/src/fee_ticker/mod.rs
+++ core/server/src/fee_ticker/mod.rs
@@ -45,7 +45,8 @@ mod ticker_info;
 
 // Base operation costs estimated via `gas_price` test.
 const BASE_TRANSFER_COST: u32 = 350;
-const BASE_TRANSFER_TO_NEW_COST: u32 = 850;
+/// TODO: change to real value after lauch settles: issue #743
+const BASE_TRANSFER_TO_NEW_COST: u32 = 350;
 const BASE_WITHDRAW_COST: u32 = 90_000;
 
 /// Type of the fee calculation pattern.
