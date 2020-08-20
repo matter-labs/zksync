bd217e25e48f8b95cc8da7c1a1128f8a65df9363
Vitalii Drohan <vitaliy.drogan@gmail.com>
2020-08-20 16:38:35 +0300

    Expose zksync.wallet binding

--- js/zksync.js/src/index.ts
+++ js/zksync.js/src/index.ts
@@ -3,6 +3,7 @@ import { Provider, ETHProxy, getDefaultProvider } from "./provider";
 import { Signer } from "./signer";
 import { closestPackableTransactionAmount, closestPackableTransactionFee } from "./utils";
 
+import * as wallet from "./wallet";
 import * as types from "./types";
 import * as utils from "./utils";
 import * as crypto from "./crypto";
@@ -17,5 +18,6 @@ export {
     getDefaultProvider,
     types,
     utils,
-    crypto
+    crypto,
+    wallet
 };
