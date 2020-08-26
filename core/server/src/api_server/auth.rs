7485318c7e1d1b5687cba9b970019d811c2dab0f
Vladyslav Bochok <vladbochok1@gmail.com>
2020-08-26 12:32:07 +0300

    Add endpoint to server

---
+++ core/server/src/api_server/auth.rs
@@ -0,0 +1,39 @@
+use jsonwebtoken::errors::Error;
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: usize,
+}
+
+/// Encode JsonWebToken with shared secret - secret,
+/// sub - message and exp - time until token will be valid
+pub fn encode_token(secret: &str, sub: &str, exp: usize) -> Result<String, Error> {
+    let claim = Claims {
+        sub: sub.to_string(),
+        exp: exp,
+    };
+    encode(
+        &Header::default(),
+        &claim,
+        &EncodingKey::from_secret(secret.as_ref()),
+    )
+}
+
+/// Validate JsonWebToken
+pub fn validate_token(token: &str) -> Result<bool, Error> {
+    let secret = std::env::var("SECRET_AUTH").expect("SECRET_AUTH must be set");
+    let token = decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(secret.as_ref()),
+        &Validation::default(),
+    );
+
+    let is_valid = match token {
+        Ok(data) => Ok(true),
+        Err(err) => Err(err),
+    };
+    is_valid
+}
---
+++ core/server/src/api_server/endpoint_server.rs
@@ -0,0 +1,122 @@
+use crate::api_server::auth;
+
+// Built-in deps
+use std::thread;
+
+// External uses
+use actix_web::dev::ServiceRequest;
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use actix_web_httpauth::extractors::{
+    bearer::{BearerAuth, Config},
+    AuthenticationError,
+};
+use actix_web_httpauth::middleware::HttpAuthentication;
+use futures::channel::mpsc;
+
+// Local uses
+use models::config_options::{ConfigurationOptions, ThreadPanicNotify};
+
+#[derive(Debug)]
+struct AppState {
+    connection_pool: storage::ConnectionPool,
+}
+
+impl AppState {
+    fn access_storage(&self) -> actix_web::Result<storage::StorageProcessor> {
+        self.connection_pool.access_storage_fragile().map_err(|e| {
+            vlog::warn!("Failed to access storage: {}", e);
+            actix_web::error::ErrorInternalServerError(e)
+        })
+    }
+}
+
+fn get_tokens(data: web::Data<AppState>) -> actix_web::Result<HttpResponse> {
+    let storage = data.access_storage()?;
+    let res = storage.tokens_schema().load_tokens().map_err(|e| {
+        vlog::warn!("could not get token: {}", e);
+        actix_web::error::ErrorInternalServerError("storage layer error")
+    })?;
+
+    let res = res.values().cloned().collect::<Vec<_>>();
+    Ok(HttpResponse::Ok().json(res))
+}
+
+fn add_token(
+    data: web::Data<AppState>,
+    r: web::Json<models::node::tokens::Token>,
+) -> actix_web::Result<()> {
+    let storage = data.access_storage()?;
+
+    storage.tokens_schema().store_token(r.0).map_err(|e| {
+        vlog::warn!("failed add token to database in progress request: {}", e);
+        actix_web::error::ErrorInternalServerError("storage layer error")
+    })
+}
+
+fn get_number_of_token(data: web::Data<AppState>) -> actix_web::Result<HttpResponse> {
+    let storage = data.access_storage()?;
+
+    let res = storage.tokens_schema().get_count().map_err(|e| {
+        vlog::warn!(
+            "failed get number of token from database in progress request: {}",
+            e
+        );
+        actix_web::error::ErrorInternalServerError("storage layer error")
+    })?;
+
+    Ok(HttpResponse::Ok().json(res))
+}
+
+fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.get_ref().clone())
+        .unwrap_or_else(Default::default);
+
+    let is_valid = match auth::validate_token(credentials.token()) {
+        Ok(res) => {
+            if res {
+                Ok(req)
+            } else {
+                Err(AuthenticationError::from(config).into())
+            }
+        }
+        Err(_) => Err(AuthenticationError::from(config).into()),
+    };
+    is_valid
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn start_endpoint_server(
+    config_options: &ConfigurationOptions,
+    connection_pool: storage::ConnectionPool,
+    panic_notify: mpsc::Sender<bool>,
+) {
+    let bind_to = config_options.endpoint_http_server_address;
+
+    thread::Builder::new()
+        .name("endpoint_server".to_string())
+        .spawn(move || {
+            HttpServer::new(move || {
+                let _panic_sentinel = ThreadPanicNotify(panic_notify.clone());
+
+                let app_state = AppState {
+                    connection_pool: connection_pool.clone(),
+                };
+
+                let auth = HttpAuthentication::bearer(validator);
+
+                App::new()
+                    .wrap(auth)
+                    .register_data(web::Data::new(app_state))
+                    .route("/count", web::get().to(get_number_of_token))
+                    .route("/tokens", web::post().to(add_token))
+                    .route("/tokens", web::get().to(get_tokens))
+            })
+            .bind(&bind_to)
+            .expect("failed to bind")
+            .run()
+            .expect("failed to run endpoint server");
+        })
+        .expect("failed to start endpoint server");
+}
