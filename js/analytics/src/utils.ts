506e97ee7b858ea495545b08e2789c022cd95916
Vladyslav-Bochok <41153528+Vladyslav-Bochok@users.noreply.github.com>
2020-09-16 11:52:08 +0300

    Update js/analytics/src/utils.ts
    Co-authored-by: Igor Aleksanov <popzxc@yandex.ru>

--- js/analytics/src/utils.ts
+++ js/analytics/src/utils.ts
@@ -21,7 +21,7 @@ export class TimePeriod {
     }
 
     public inTime(timeStamp: Date) {
-        return +this.timeFrom <= +timeStamp && +this.timeTo >= +timeStamp;
+        return +this.timeFrom <= +timeStamp && +timeStamp <= +this.timeTo;
     }
 
     public less(timeStamp: Date) {
