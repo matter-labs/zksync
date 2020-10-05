2ad56b64b4ee2268b942816ecb97b078ad639a50
Igor Aleksanov <popzxc@yandex.ru>
2020-10-05 13:25:35 +0300

    Document misc crates

--- core/lib/basic_types/src/lib.rs
+++ core/lib/basic_types/src/lib.rs
@@ -3,7 +3,11 @@
 
 pub use web3::types::{Address, Log, TransactionReceipt, H160, H256, U128, U256};
 
+/// Unique identifier of the token in the zkSync network.
 pub type TokenId = u16;
+/// Unique identifier of the account in the zkSync network.
 pub type AccountId = u32;
+/// zkSync network block sequential index.
 pub type BlockNumber = u32;
+/// zkSync account nonce.
 pub type Nonce = u32;
