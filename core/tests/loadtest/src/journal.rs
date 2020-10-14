05b86ad3a7b308a01a42cc39802dedc338c21f28
Aleksei Sidorov <gorthauer87@yandex.ru>
2020-10-14 10:17:00 +0300

    Improve five summary stats

--- core/tests/loadtest/src/journal.rs
+++ core/tests/loadtest/src/journal.rs
@@ -1,9 +1,11 @@
 // Built-in import
 use std::{
+    cmp::{max, min},
     collections::HashMap,
     time::{Duration, Instant},
 };
 // External uses
+use num::Num;
 use statrs::statistics::{Max, Median, Min, Statistics};
 // Workspace uses
 use zksync_types::tx::TxHash;
@@ -62,8 +64,8 @@ impl Journal {
             .iter()
             .map(|(tx_hash, result)| match result {
                 Ok(tx_lifecycle) => Ok((
-                    tx_lifecycle.sending_duration().as_millis() as f64,
-                    tx_lifecycle.committing_duration().as_millis() as f64,
+                    tx_lifecycle.sending_duration().as_millis(),
+                    tx_lifecycle.committing_duration().as_millis(),
                 )),
                 Err(err) => Err((*tx_hash, err.to_owned())),
             })
@@ -85,13 +87,15 @@ impl Journal {
         }
     }
 
-    fn print_stats(category: &str, data: &[f64]) {
+    fn print_stats(category: &str, data: &[u128]) {
         debug_assert!(data.len() >= 4);
 
-        let min = Min::min(data);
-        let max = Max::max(data);
-        let median = data.median();
-        let std_dev = data.std_dev();
+        let data2 = data.iter().map(|&x| x as f64).collect::<Vec<_>>();
+
+        let min: f64 = Min::min(data2.as_slice());
+        let max: f64 = Max::max(data2.as_slice());
+        let median = data2.median();
+        let std_dev = data2.std_dev();
 
         log::info!(
             "Statistics for `{}`: min: {}ms, median: {}ms, max: {}ms, std_dev: {}ms",
@@ -102,25 +106,57 @@ impl Journal {
             std_dev
         );
 
-        let mut sorted_data = data.iter().map(|&x| x as u64).collect::<Vec<_>>();
-        sorted_data.sort_unstable();
-
-        let idx = sorted_data.len() - 1;
-
-        let min = sorted_data[0];
-        let lower_quartile = sorted_data[idx / 4];
-        let median = sorted_data[idx / 2];
-        let upper_quartile = sorted_data[idx * 3 / 4];
-        let max = sorted_data[idx];
+        let five_stats_summary = FiveSummaryStats::from_data(data.iter().copied());
 
         log::info!(
-            "Statistics2 for `{}`: min: {}ms, median: {}ms, max: {}ms, lower_quartile: {}ms, upper_quartile: {}ms",
+            "Statistics2 for `{}`: min: {}ms, median: {}ms, max: {}ms, lower_quartile: {}ms, \
+            upper_quartile: {}ms, std_dev: {}ms",
             category,
-            min,
-            median,
-            max,
-            lower_quartile,
-            upper_quartile,
+            five_stats_summary.min,
+            five_stats_summary.median,
+            five_stats_summary.max,
+            five_stats_summary.lower_quartile,
+            five_stats_summary.upper_quartile,
+            five_stats_summary.std_dev
         );
+    }
+}
+
+struct FiveSummaryStats {
+    min: u128,
+    lower_quartile: u128,
+    median: u128,
+    upper_quartile: u128,
+    max: u128,
+    std_dev: f64,
+}
+
+impl FiveSummaryStats {
+    fn from_data(data: impl IntoIterator<Item = u128>) -> Self {
+        let mut data = data.into_iter().collect::<Vec<_>>();
+        data.sort_unstable();
+
+        assert!(data.len() >= 4);
+
+        // Compute std dev.
+        let n = data.len() as u128;
+        let m = data.iter().sum::<u128>() / data.len() as u128;
+
+        let square_sum = data.iter().fold(0, |acc, &x| {
+            let d = max(x, m) - min(x, m);
+            acc + d * d
+        });
+        let std_dev = (square_sum as f64 / n as f64).sqrt();
+
+        // Compute five summary stats
+        let idx = data.len() - 1;
+        Self {
+            min: data[0],
+            lower_quartile: data[idx / 4],
+            median: data[idx / 2],
+            upper_quartile: data[idx * 3 / 4],
+            max: data[idx],
+            std_dev,
+        }
     }
 }
