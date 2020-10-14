ff8ccff441b3cea8757443da3458cd6d52741c0e
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2020-10-14 11:33:50 +0300

    Refactor

--- infrastructure/zk/src/index.ts
+++ infrastructure/zk/src/index.ts
@@ -1,29 +1,35 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { sh } from './sh';
+import { spawn } from './sh';
 import fs from 'fs';
 import dotenv from 'dotenv';
 
 function loadEnv() {
-    const ZKSYNC_HOME = process.env.ZKSYNC_HOME;
-    if (!ZKSYNC_HOME) {
-        throw new Error('Please set $ZKSYNC_HOME to the root of ZkSync repo!');
-    }
-    process.chdir(ZKSYNC_HOME);
     const current = 'etc/env/current';
-    const env = fs.existsSync(current) ? fs.readFileSync(current) : 'dev';
-    const envFile = `etc/env/${env}.env`;
-    if (env == 'dev' && !fs.existsSync('etc/env/dev.env')) {
+    const ZKSYNC_ENV = process.env.ZKSYNC_ENV 
+        || (fs.existsSync(current) ? fs.readFileSync(current).toString() : 'dev');
+    const ENV_FILE = `etc/env/${ZKSYNC_ENV}.env`;
+    if (ZKSYNC_ENV == 'dev' && !fs.existsSync('etc/env/dev.env')) {
         fs.copyFileSync('etc/env/dev.env.example', 'etc/env/dev.env');
     }
-    if (!fs.existsSync(envFile)) {
-        throw new Error('ZkSync config file not found: ' + envFile);
+    if (!fs.existsSync(ENV_FILE)) {
+        throw new Error('ZkSync config file not found: ' + ENV_FILE);
     }
-    dotenv.config({ path: envFile });
+    process.env.ZKSYNC_ENV = ZKSYNC_ENV;
+    process.env.ENV_FILE = ENV_FILE;
+    dotenv.config({ path: ENV_FILE });
 }
 
 async function main() {
+    const ZKSYNC_HOME = process.env.ZKSYNC_HOME;
+
+    if (!ZKSYNC_HOME) {
+        throw new Error('Please set $ZKSYNC_HOME to the root of ZkSync repo!');
+    } else {
+        process.chdir(ZKSYNC_HOME);
+    }
+
     const program = new Command();
 
     program
@@ -36,11 +42,10 @@ async function main() {
         .description('start zksync server')
         .option('--genesis', 'generate genesis data via server')
         .action(async () => {
-            // TODO: make it continuously print logs
-            await sh('cargo run --bin zksync_server --release');
-        })
+            loadEnv();
+            await spawn('cargo run --bin zksync_server --release');
+        });
 
-    program.on('command:*', loadEnv);
     await program.parseAsync(process.argv);
 }
 
@@ -48,6 +53,6 @@ async function main() {
 main()
     .then(() => process.exit(0))
     .catch((err: Error) => {
-        console.error('Error:', err.message);
+        console.error(err);
         process.exit(1);
     });
--- infrastructure/zk/src/sh.ts
+++ infrastructure/zk/src/sh.ts
@@ -1,5 +1,20 @@
-import { exec } from 'child_process';
+import { exec as _exec, spawn as _spawn } from 'child_process';
 import { promisify } from 'util';
 
 // async executor of shell commands
-export const sh = promisify(exec);
+// spawns a new shell and can execute arbitrary commands, like "ls -la | grep .env"
+// returns { stdout, stderr }
+export const exec = promisify(_exec);
+
+
+// executes a signle command in a new process
+// pipes data to parent's stdout/stderr
+export function spawn(command: string) {
+    const child = _spawn(command, { stdio: 'inherit', shell: true });
+    return new Promise((resolve, reject) => {
+        child.on('error', reject);
+        child.on('close', (code) => {
+            code == 0 ? resolve() : reject(`Child process exited with code ${code}`);
+        });
+    });
+}
