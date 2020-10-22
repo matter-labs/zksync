995bd0b2eea14e78c866d7bb33c2475910361eae
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2020-10-22 21:10:21 +0300

    Fix prover

--- infrastructure/zk/src/prover.ts
+++ infrastructure/zk/src/prover.ts
@@ -1,28 +1,32 @@
 import { Command } from 'commander';
 import * as utils from './utils';
+import os from 'os';
 
 export async function prover(totalProvers: number) {
     let children: utils.ChildProcess[] = [];
+
     for (let id = 1; id <= totalProvers; id++) {
-        const name = `${process.env.HOSTNAME}_${id}_blocks`;
+        const name = `${os.hostname()}_${id}_blocks`;
         console.log('Started prover', name);
         const child = utils.background(`cargo run --release --bin plonk_step_by_step_prover ${name}`);
         children.push(child);
     }
+
     process.on('SIGINT', () => {
         console.log('Killing provers...');
         for (const child of children) {
             child.kill();
         }
+        process.exit();
     });
-    while (true) {
-        await utils.sleep(1000_000);
-    }
+
+    while (true) await utils.sleep(1_000_000);
 }
 
 export const command = new Command('prover')
     .description('run zksync prover')
     .arguments('[number_of_provers]')
-    .action(async (number_of_provers?: number) => {
-        await prover(number_of_provers || 1);
+    .action(async (provers?: string) => {
+        const totalProvers = provers ? parseInt(provers) : 1;
+        await prover(totalProvers);
     });
