050f9d3049308cf4ce2660bd7f34430bd89691b3
Igor Aleksanov <popzxc@yandex.ru>
2020-10-28 10:47:03 +0300

    Fix typo in the comment + data-restore check-existing

--- bin/all_integration_tests.sh
+++ bin/all_integration_tests.sh
@@ -8,9 +8,9 @@ zksync api-test || exit 1
 # zcli test
 yarn --cwd infrastructure/zcli test || exit 1
 
-# rust-sdk tesst
+# rust-sdk test
 cargo test -p zksync --release -- --ignored --test-threads=1 || exit 1
 
 # We have to kill the server before running data-restore
 killall zksync_server
-zksync data-restore || exit 1
+zksync data-restore check-existing || exit 1
