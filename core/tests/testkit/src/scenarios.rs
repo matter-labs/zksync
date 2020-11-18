8509e46bedcc700a4864c0c5a9800fe5c60139bf
Igor Aleksanov <popzxc@yandex.ru>
2020-11-18 21:38:41 +0300

    Add comment to a quasi-basic testkit function

--- core/tests/testkit/src/scenarios.rs
+++ core/tests/testkit/src/scenarios.rs
@@ -11,6 +11,90 @@ use web3::transports::Http;
 
 use super::*;
 
+/// Performs a fixed set of operations which covers most of the main server's functionality.
+/// Aim is to cover operations processed by state keeper, while manually simulating everything else around it.
+pub async fn perform_basic_tests() {
+    // This test is actually nowhere near "basic", and deserves a careful refactoring, but
+    // ain't nobody got time for that ¯\_(ツ)_/¯
+
+    let testkit_config = TestkitConfig::from_env();
+
+    let fee_account = ZkSyncAccount::rand();
+    let (sk_thread_handle, stop_state_keeper_sender, sk_channels) =
+        spawn_state_keeper(&fee_account.address);
+
+    let deploy_timer = Instant::now();
+    println!("deploying contracts");
+    let contracts = deploy_contracts(false, Default::default());
+    println!(
+        "contracts deployed {:#?}, {} secs",
+        contracts,
+        deploy_timer.elapsed().as_secs()
+    );
+
+    let transport = Http::new(&testkit_config.web3_url).expect("http transport start");
+    let (test_accounts_info, commit_account_info) = get_test_accounts();
+    let commit_account = EthereumAccount::new(
+        commit_account_info.private_key,
+        commit_account_info.address,
+        transport.clone(),
+        contracts.contract,
+        testkit_config.chain_id,
+        testkit_config.gas_price_factor,
+    );
+    let eth_accounts = test_accounts_info
+        .into_iter()
+        .map(|test_eth_account| {
+            EthereumAccount::new(
+                test_eth_account.private_key,
+                test_eth_account.address,
+                transport.clone(),
+                contracts.contract,
+                testkit_config.chain_id,
+                testkit_config.gas_price_factor,
+            )
+        })
+        .collect::<Vec<_>>();
+
+    let zksync_accounts = {
+        let mut zksync_accounts = Vec::new();
+        zksync_accounts.push(fee_account);
+        zksync_accounts.extend(eth_accounts.iter().map(|eth_account| {
+            let rng_zksync_key = ZkSyncAccount::rand().private_key;
+            ZkSyncAccount::new(
+                rng_zksync_key,
+                0,
+                eth_account.address,
+                eth_account.private_key,
+            )
+        }));
+        zksync_accounts
+    };
+
+    let accounts = AccountSet {
+        eth_accounts,
+        zksync_accounts,
+        fee_account_id: ZKSyncAccountId(0),
+    };
+
+    let mut test_setup = TestSetup::new(sk_channels, accounts, &contracts, commit_account);
+
+    let deposit_amount = parse_ether("1.0").unwrap();
+
+    for token in 0..=1 {
+        perform_basic_operations(
+            token,
+            &mut test_setup,
+            deposit_amount.clone(),
+            BlockProcessing::CommitAndVerify,
+        )
+        .await;
+    }
+
+    stop_state_keeper_sender.send(()).expect("sk stop send");
+    sk_thread_handle.join().expect("sk thread join");
+}
+
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum BlockProcessing {
     CommitAndVerify,
@@ -171,83 +255,4 @@ pub async fn perform_basic_operations(
     } else {
         test_setup.execute_commit_block().await.0.expect_success();
     }
-}
-
-pub async fn perform_basic_tests() {
-    let testkit_config = TestkitConfig::from_env();
-
-    let fee_account = ZkSyncAccount::rand();
-    let (sk_thread_handle, stop_state_keeper_sender, sk_channels) =
-        spawn_state_keeper(&fee_account.address);
-
-    let deploy_timer = Instant::now();
-    println!("deploying contracts");
-    let contracts = deploy_contracts(false, Default::default());
-    println!(
-        "contracts deployed {:#?}, {} secs",
-        contracts,
-        deploy_timer.elapsed().as_secs()
-    );
-
-    let transport = Http::new(&testkit_config.web3_url).expect("http transport start");
-    let (test_accounts_info, commit_account_info) = get_test_accounts();
-    let commit_account = EthereumAccount::new(
-        commit_account_info.private_key,
-        commit_account_info.address,
-        transport.clone(),
-        contracts.contract,
-        testkit_config.chain_id,
-        testkit_config.gas_price_factor,
-    );
-    let eth_accounts = test_accounts_info
-        .into_iter()
-        .map(|test_eth_account| {
-            EthereumAccount::new(
-                test_eth_account.private_key,
-                test_eth_account.address,
-                transport.clone(),
-                contracts.contract,
-                testkit_config.chain_id,
-                testkit_config.gas_price_factor,
-            )
-        })
-        .collect::<Vec<_>>();
-
-    let zksync_accounts = {
-        let mut zksync_accounts = Vec::new();
-        zksync_accounts.push(fee_account);
-        zksync_accounts.extend(eth_accounts.iter().map(|eth_account| {
-            let rng_zksync_key = ZkSyncAccount::rand().private_key;
-            ZkSyncAccount::new(
-                rng_zksync_key,
-                0,
-                eth_account.address,
-                eth_account.private_key,
-            )
-        }));
-        zksync_accounts
-    };
-
-    let accounts = AccountSet {
-        eth_accounts,
-        zksync_accounts,
-        fee_account_id: ZKSyncAccountId(0),
-    };
-
-    let mut test_setup = TestSetup::new(sk_channels, accounts, &contracts, commit_account);
-
-    let deposit_amount = parse_ether("1.0").unwrap();
-
-    for token in 0..=1 {
-        perform_basic_operations(
-            token,
-            &mut test_setup,
-            deposit_amount.clone(),
-            BlockProcessing::CommitAndVerify,
-        )
-        .await;
-    }
-
-    stop_state_keeper_sender.send(()).expect("sk stop send");
-    sk_thread_handle.join().expect("sk thread join");
 }
