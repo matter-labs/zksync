db89251fcf5c59af13dd9f0f2dbe88ffc311ce3c
Igor Aleksanov <popzxc@yandex.ru>
2020-11-18 09:15:19 +0300

    Add generating test vector for token formatting

--- infrastructure/sdk-test-vector-generator/src/vectors/utils-vector.ts
+++ infrastructure/sdk-test-vector-generator/src/vectors/utils-vector.ts
@@ -1,5 +1,6 @@
 import * as zksync from "zksync";
 import { TestVector, TestVectorEntry } from "../types";
+import { utils } from "ethers";
 
 /**
  * Utilities test vector consist of several independent test vectors.
@@ -7,6 +8,7 @@ import { TestVector, TestVectorEntry } from "../types";
 export interface UtilsVectors {
     amountPacking: TestVector<PackingTestEntry>;
     feePacking: TestVector<PackingTestEntry>;
+    tokenFormatting: TestVector<TokenFormattingEntry>;
 }
 
 /**
@@ -22,16 +24,39 @@ export interface PackingTestEntry extends TestVectorEntry {
         packable: boolean;
         // Closest packable value. May be the same as `inputs.value` if value is packable.
         closestPackable: string;
+        // Closest packable value packed into an byte array. Represented as a hexadecimal string.
+        packedValue: string;
+    };
+}
+
+/**
+ * Test vector for token formatting.
+ * Token formatting must be the same, since this algorithm is used in the Ethereum signature messages.
+ */
+export interface TokenFormattingEntry extends TestVectorEntry {
+    inputs: {
+        // Token symbol, e.g. "ETH"
+        token: string;
+        // Amount of token decimals, e.g. 6 or 18
+        decimals: number;
+        // Amount of token as a string.
+        amount: string;
+    };
+    outputs: {
+        // Formatted string, e.g. `0.001 ETH`.
+        formatted: string;
     };
 }
 
 export function generateUtilsVectors(): UtilsVectors {
     const amountPacking = generateAmountPackingVector();
     const feePacking = generateFeePackingVector();
+    const tokenFormatting = generateTokenFormattingVector();
 
     return {
         amountPacking,
         feePacking,
+        tokenFormatting,
     };
 }
 
@@ -41,6 +66,7 @@ function generateFeePackingVector(): TestVector<PackingTestEntry> {
     for (let value of test_vector) {
         const packable = zksync.utils.isTransactionFeePackable(value);
         const closestPackable = zksync.utils.closestPackableTransactionFee(value);
+        const packed = zksync.utils.packFeeChecked(closestPackable);
         items.push({
             inputs: {
                 value,
@@ -48,6 +74,7 @@ function generateFeePackingVector(): TestVector<PackingTestEntry> {
             outputs: {
                 packable,
                 closestPackable: closestPackable.toString(),
+                packedValue: utils.hexlify(packed),
             },
         });
     }
@@ -61,11 +88,12 @@ function generateFeePackingVector(): TestVector<PackingTestEntry> {
 }
 
 function generateAmountPackingVector(): TestVector<PackingTestEntry> {
-    const test_vector = ["0", "1000", "1111", "474732833474", "474732833400", "10000000000000"];
+    const testVector = ["0", "1000", "1111", "474732833474", "474732833400", "10000000000000"];
     const items = [];
-    for (let value of test_vector) {
+    for (const value of testVector) {
         const packable = zksync.utils.isTransactionAmountPackable(value);
         const closestPackable = zksync.utils.closestPackableTransactionAmount(value);
+        const packed = zksync.utils.packAmountChecked(closestPackable);
         items.push({
             inputs: {
                 value,
@@ -73,6 +101,7 @@ function generateAmountPackingVector(): TestVector<PackingTestEntry> {
             outputs: {
                 packable,
                 closestPackable: closestPackable.toString(),
+                packedValue: utils.hexlify(packed),
             },
         });
     }
@@ -83,4 +112,32 @@ function generateAmountPackingVector(): TestVector<PackingTestEntry> {
     };
 
     return vector;
+}
+
+function generateTokenFormattingVector(): TestVector<TokenFormattingEntry> {
+    const testVector = [
+        { token: "NNM", decimals: 0, amount: "1000000000000000100000", formatted: "1000000000000000100000.0 NNM" },
+        { token: "DAI", decimals: 6, amount: "1000000", formatted: "1.0 DAI" },
+        { token: "ZRO", decimals: 11, amount: "0", formatted: "0.0 ZRO" },
+        { token: "ETH", decimals: 18, amount: "1000000000000000100000", formatted: "1000.0000000000001 ETH" },
+    ];
+
+    const items = [];
+    for (const value of testVector) {
+        items.push({
+            inputs: {
+                token: value.token,
+                decimals: value.decimals,
+                amount: value.amount,
+            },
+            outputs: {
+                formatted: value.formatted,
+            },
+        });
+    }
+
+    return {
+        description: "Checks for token amount formatting",
+        items,
+    };
 }
