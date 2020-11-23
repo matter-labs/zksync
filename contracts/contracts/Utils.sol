c51f94c0e73d0523a533e552bc5b897417472c3f
Chris Sosnin <chris125_@live.com>
2020-11-23 14:10:26 +0300

    Fix compiler version

--- contracts/contracts/Utils.sol
+++ contracts/contracts/Utils.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.8;
 
 import "./IERC20.sol";
 import "./Bytes.sol";
--- contracts/contracts/ZkSync.sol
+++ contracts/contracts/ZkSync.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.8;
 
 import "./ReentrancyGuard.sol";
 import "./SafeMath.sol";
