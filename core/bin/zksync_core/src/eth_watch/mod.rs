3a17a33d97dbf51729d0456c66122660484427a6
Danil Lugovskoy <deniallugo@gmail.com>
2020-11-27 17:16:45 +0300

    Extract web3 and storage logics to trait

--- core/bin/zksync_core/src/eth_watch/mod.rs
+++ core/bin/zksync_core/src/eth_watch/mod.rs
@@ -8,35 +8,36 @@
 // Built-in deps
 use std::{
     collections::HashMap,
-    convert::TryFrom,
     time::{Duration, Instant},
 };
+
 // External uses
-use anyhow::format_err;
 use futures::{
     channel::{mpsc, oneshot},
     SinkExt, StreamExt,
 };
+
 use tokio::{task::JoinHandle, time};
-use web3::{
-    contract::{Contract, Options},
-    types::{Address, BlockNumber, Filter, FilterBuilder, H160},
-    Transport, Web3,
-};
+use web3::types::{Address, BlockNumber};
+
 // Workspace deps
 use zksync_config::ConfigurationOptions;
-use zksync_contracts::zksync_contract;
 use zksync_crypto::params::PRIORITY_EXPIRATION;
 use zksync_storage::ConnectionPool;
-use zksync_types::{
-    ethereum::CompleteWithdrawalsTx,
-    {Nonce, PriorityOp, PubKeyHash, ZkSyncPriorityOp},
-};
+use zksync_types::{Nonce, PriorityOp, PubKeyHash, ZkSyncPriorityOp};
+
 // Local deps
-use self::{eth_state::ETHState, received_ops::sift_outdated_ops};
+use self::{
+    eth_state::ETHState, received_ops::sift_outdated_ops, storage::Storage, worker::EthWorker,
+};
+
+pub use storage::DBStorage;
+pub use worker::EthWorkerHttp;
 
 mod eth_state;
 mod received_ops;
+mod storage;
+mod worker;
 
 /// As `infura` may limit the requests, upon error we need to wait for a while
 /// before repeating the request.
@@ -82,49 +83,35 @@ pub enum EthWatchRequest {
         resp: oneshot::Sender<Option<(EthBlockId, PriorityOp)>>,
     },
     GetPendingWithdrawalsQueueIndex {
-        resp: oneshot::Sender<Result<u32, anyhow::Error>>,
+        resp: oneshot::Sender<anyhow::Result<u32>>,
     },
 }
 
-pub struct EthWatch<T: Transport> {
-    zksync_contract: (ethabi::Contract, Contract<T>),
+pub struct EthWatch<W: EthWorker, S: Storage> {
+    worker: W,
+    storage: S,
     eth_state: ETHState,
-    web3: Web3<T>,
     /// All ethereum events are accepted after sufficient confirmations to eliminate risk of block reorg.
     number_of_confirmations_for_event: u64,
-
     mode: WatcherMode,
-
     eth_watch_req: mpsc::Receiver<EthWatchRequest>,
-
-    db_pool: ConnectionPool,
 }
 
-impl<T: Transport> EthWatch<T> {
+impl<W: EthWorker, S: Storage> EthWatch<W, S> {
     pub fn new(
-        web3: Web3<T>,
-        zksync_contract_addr: H160,
+        worker: W,
+        storage: S,
         number_of_confirmations_for_event: u64,
         eth_watch_req: mpsc::Receiver<EthWatchRequest>,
-        db_pool: ConnectionPool,
     ) -> Self {
-        let zksync_contract = {
-            (
-                zksync_contract(),
-                Contract::new(web3.eth(), zksync_contract_addr, zksync_contract()),
-            )
-        };
-
         Self {
-            zksync_contract,
+            worker,
+            storage,
             eth_state: ETHState::default(),
-            web3,
             eth_watch_req,
 
             mode: WatcherMode::Working,
             number_of_confirmations_for_event,
-
-            db_pool,
         }
     }
 
@@ -133,131 +120,10 @@ impl<T: Transport> EthWatch<T> {
         self.eth_state = new_state;
     }
 
-    fn get_priority_op_event_filter(&self, from: BlockNumber, to: BlockNumber) -> Filter {
-        let priority_op_event_topic = self
-            .zksync_contract
-            .0
-            .event("NewPriorityRequest")
-            .expect("main contract abi error")
-            .signature();
-        FilterBuilder::default()
-            .address(vec![self.zksync_contract.1.address()])
-            .from_block(from)
-            .to_block(to)
-            .topics(Some(vec![priority_op_event_topic]), None, None, None)
-            .build()
-    }
-
-    fn get_complete_withdrawals_event_filter(&self, from: BlockNumber, to: BlockNumber) -> Filter {
-        let complete_withdrawals_event_topic = self
-            .zksync_contract
-            .0
-            .event("PendingWithdrawalsComplete")
-            .expect("main contract abi error")
-            .signature();
-        FilterBuilder::default()
-            .address(vec![self.zksync_contract.1.address()])
-            .from_block(from)
-            .to_block(to)
-            .topics(
-                Some(vec![complete_withdrawals_event_topic]),
-                None,
-                None,
-                None,
-            )
-            .build()
-    }
-
-    /// Filters and parses the priority operation events from the Ethereum
-    /// within the provided range of blocks.
-    /// Returns the list of priority operations together with the block
-    /// numbers.
-    async fn get_priority_op_events_with_blocks(
-        &self,
-        from: BlockNumber,
-        to: BlockNumber,
-    ) -> Result<Vec<(EthBlockId, PriorityOp)>, anyhow::Error> {
-        let start = Instant::now();
-        let filter = self.get_priority_op_event_filter(from, to);
-        let result = self
-            .web3
-            .eth()
-            .logs(filter)
-            .await?
-            .into_iter()
-            .map(|event| {
-                let block_number: u64 = event
-                    .block_number
-                    .ok_or_else(|| {
-                        anyhow::format_err!("No block number set in the queue event log")
-                    })?
-                    .as_u64();
-
-                let priority_op = PriorityOp::try_from(event).map_err(|e| {
-                    format_err!("Failed to parse priority queue event log from ETH: {:?}", e)
-                })?;
-
-                Ok((block_number, priority_op))
-            })
-            .collect();
-
-        metrics::histogram!(
-            "eth_watcher.get_priority_op_events_with_blocks",
-            start.elapsed()
-        );
-        result
-    }
-
-    async fn get_priority_op_events(
-        &self,
-        from: BlockNumber,
-        to: BlockNumber,
-    ) -> Result<Vec<PriorityOp>, anyhow::Error> {
-        let start = Instant::now();
-        let filter = self.get_priority_op_event_filter(from, to);
-        let result = self
-            .web3
-            .eth()
-            .logs(filter)
-            .await?
-            .into_iter()
-            .map(|event| {
-                PriorityOp::try_from(event).map_err(|e| {
-                    format_err!("Failed to parse priority queue event log from ETH: {:?}", e)
-                })
-            })
-            .collect();
-        metrics::histogram!("eth_watcher.get_priority_op_events", start.elapsed());
-        result
-    }
-
-    async fn get_complete_withdrawals_event(
-        &self,
-        from: BlockNumber,
-        to: BlockNumber,
-    ) -> Result<Vec<CompleteWithdrawalsTx>, anyhow::Error> {
-        let start = Instant::now();
-        let filter = self.get_complete_withdrawals_event_filter(from, to);
-        let result = self
-            .web3
-            .eth()
-            .logs(filter)
-            .await?
-            .into_iter()
-            .map(CompleteWithdrawalsTx::try_from)
-            .collect();
-
-        metrics::histogram!(
-            "eth_watcher.get_complete_withdrawals_event",
-            start.elapsed()
-        );
-        result
-    }
-
     async fn get_unconfirmed_ops(
         &mut self,
         current_ethereum_block: u64,
-    ) -> Result<Vec<(EthBlockId, PriorityOp)>, anyhow::Error> {
+    ) -> anyhow::Result<Vec<(EthBlockId, PriorityOp)>> {
         // We want to scan the interval of blocks from the latest one up to the oldest one which may
         // have unconfirmed priority ops.
         // `+ 1` is added because if we subtract number of confirmations, we'll obtain the last block
@@ -269,45 +135,21 @@ impl<T: Transport> EthWatch<T> {
         let block_to = BlockNumber::Latest;
 
         let pending_events = self
-            .get_priority_op_events_with_blocks(block_from, block_to)
+            .worker
+            .get_priority_op_events(block_from, block_to)
             .await?;
 
         // Collect the unconfirmed operations.
         let mut unconfirmed_ops = Vec::new();
 
-        for (block_number, priority_op) in pending_events.into_iter() {
-            unconfirmed_ops.push((block_number, priority_op));
+        for priority_op in pending_events.into_iter() {
+            unconfirmed_ops.push((priority_op.eth_block, priority_op));
         }
 
         Ok(unconfirmed_ops)
     }
 
-    async fn store_complete_withdrawals(
-        &mut self,
-        complete_withdrawals_txs: Vec<CompleteWithdrawalsTx>,
-    ) -> Result<(), anyhow::Error> {
-        let mut storage = self
-            .db_pool
-            .access_storage()
-            .await
-            .map_err(|e| format_err!("Can't access storage: {}", e))?;
-        let mut transaction = storage.start_transaction().await?;
-        for tx in complete_withdrawals_txs {
-            transaction
-                .chain()
-                .operations_schema()
-                .add_complete_withdrawals_transaction(tx)
-                .await?;
-        }
-        transaction.commit().await?;
-
-        Ok(())
-    }
-
-    async fn restore_state_from_eth(
-        &mut self,
-        last_ethereum_block: u64,
-    ) -> Result<(), anyhow::Error> {
+    async fn restore_state_from_eth(&mut self, last_ethereum_block: u64) -> anyhow::Result<()> {
         let current_ethereum_block =
             last_ethereum_block.saturating_sub(self.number_of_confirmations_for_event);
 
@@ -321,16 +163,20 @@ impl<T: Transport> EthWatch<T> {
 
         // restore complete withdrawals events
         let complete_withdrawals_txs = self
+            .worker
             .get_complete_withdrawals_event(
                 BlockNumber::Number(previous_block_with_accepted_events.into()),
                 BlockNumber::Number(new_block_with_accepted_events.into()),
             )
             .await?;
-        self.store_complete_withdrawals(complete_withdrawals_txs)
+
+        self.storage
+            .store_complete_withdrawals(complete_withdrawals_txs)
             .await?;
 
         // restore priority queue
         let prior_queue_events = self
+            .worker
             .get_priority_op_events(
                 BlockNumber::Number(previous_block_with_accepted_events.into()),
                 BlockNumber::Number(new_block_with_accepted_events.into()),
@@ -350,7 +196,7 @@ impl<T: Transport> EthWatch<T> {
         Ok(())
     }
 
-    async fn process_new_blocks(&mut self, last_ethereum_block: u64) -> Result<(), anyhow::Error> {
+    async fn process_new_blocks(&mut self, last_ethereum_block: u64) -> anyhow::Result<()> {
         debug_assert!(self.eth_state.last_ethereum_block() < last_ethereum_block);
 
         let previous_block_with_accepted_events = (self.eth_state.last_ethereum_block() + 1)
@@ -360,16 +206,20 @@ impl<T: Transport> EthWatch<T> {
 
         // Get new complete withdrawals events
         let complete_withdrawals_txs = self
+            .worker
             .get_complete_withdrawals_event(
                 BlockNumber::Number(previous_block_with_accepted_events.into()),
                 BlockNumber::Number(new_block_with_accepted_events.into()),
             )
             .await?;
-        self.store_complete_withdrawals(complete_withdrawals_txs)
+
+        self.storage
+            .store_complete_withdrawals(complete_withdrawals_txs)
             .await?;
 
         // Get new priority ops
         let priority_op_events = self
+            .worker
             .get_priority_op_events(
                 BlockNumber::Number(previous_block_with_accepted_events.into()),
                 BlockNumber::Number(new_block_with_accepted_events.into()),
@@ -397,14 +247,14 @@ impl<T: Transport> EthWatch<T> {
     }
 
     fn get_priority_requests(&self, first_serial_id: u64, max_chunks: usize) -> Vec<PriorityOp> {
-        let mut res = Vec::new();
+        let mut result = Vec::new();
 
         let mut used_chunks = 0;
         let mut current_priority_op = first_serial_id;
 
         while let Some(op) = self.eth_state.priority_queue().get(&current_priority_op) {
             if used_chunks + op.as_ref().data.chunks() <= max_chunks {
-                res.push(op.as_ref().clone());
+                result.push(op.as_ref().clone());
                 used_chunks += op.as_ref().data.chunks();
                 current_priority_op += 1;
             } else {
@@ -412,7 +262,7 @@ impl<T: Transport> EthWatch<T> {
             }
         }
 
-        res
+        result
     }
 
     async fn is_new_pubkey_hash_authorized(
@@ -420,54 +270,17 @@ impl<T: Transport> EthWatch<T> {
         address: Address,
         nonce: Nonce,
         pub_key_hash: &PubKeyHash,
-    ) -> Result<bool, anyhow::Error> {
-        let auth_fact: Vec<u8> = self
-            .zksync_contract
-            .1
-            .query(
-                "authFacts",
-                (address, u64::from(nonce)),
-                None,
-                Options::default(),
-                None,
-            )
-            .await
-            .map_err(|e| format_err!("Failed to query contract authFacts: {}", e))?;
+    ) -> anyhow::Result<bool> {
+        let auth_fact = self.worker.get_auth_fact(address, nonce).await?;
         Ok(auth_fact.as_slice() == tiny_keccak::keccak256(&pub_key_hash.data[..]))
     }
 
-    async fn pending_withdrawals_queue_index(&self) -> Result<u32, anyhow::Error> {
-        let first_pending_withdrawal_index: u32 = self
-            .zksync_contract
-            .1
-            .query(
-                "firstPendingWithdrawalIndex",
-                (),
-                None,
-                Options::default(),
-                None,
-            )
-            .await
-            .map_err(|e| {
-                format_err!(
-                    "Failed to query contract firstPendingWithdrawalIndex: {}",
-                    e
-                )
-            })?;
-        let number_of_pending_withdrawals: u32 = self
-            .zksync_contract
-            .1
-            .query(
-                "numberOfPendingWithdrawals",
-                (),
-                None,
-                Options::default(),
-                None,
-            )
-            .await
-            .map_err(|e| {
-                format_err!("Failed to query contract numberOfPendingWithdrawals: {}", e)
-            })?;
+    async fn pending_withdrawals_queue_index(&self) -> anyhow::Result<u32> {
+        let first_pending_withdrawal_index =
+            self.worker.get_first_pending_withdrawal_index().await?;
+
+        let number_of_pending_withdrawals = self.worker.get_number_of_pending_withdrawals().await?;
+
         Ok(first_pending_withdrawal_index + number_of_pending_withdrawals)
     }
 
@@ -494,9 +307,9 @@ impl<T: Transport> EthWatch<T> {
             .collect()
     }
 
-    async fn poll_eth_node(&mut self) -> Result<(), anyhow::Error> {
+    async fn poll_eth_node(&mut self) -> anyhow::Result<()> {
         let start = Instant::now();
-        let last_block_number = self.web3.eth().block_number().await?.as_u64();
+        let last_block_number = self.worker.block_number().await?;
 
         if last_block_number > self.eth_state.last_ethereum_block() {
             self.process_new_blocks(last_block_number).await?;
@@ -536,11 +349,11 @@ impl<T: Transport> EthWatch<T> {
         // block number.
         // Normally, however, this loop is not expected to last more than one iteration.
         let block = loop {
-            let block = self.web3.eth().block_number().await;
+            let block = self.worker.block_number().await;
 
             match block {
                 Ok(block) => {
-                    break block.as_u64();
+                    break block;
                 }
                 Err(error) => {
                     log::warn!(
@@ -635,13 +448,15 @@ pub fn start_eth_watch(
 ) -> JoinHandle<()> {
     let transport = web3::transports::Http::new(&config_options.web3_url).unwrap();
     let web3 = web3::Web3::new(transport);
+    let eth_worker = EthWorkerHttp::new(web3, config_options.contract_eth_addr);
+
+    let storage = DBStorage::new(db_pool);
 
     let eth_watch = EthWatch::new(
-        web3,
-        config_options.contract_eth_addr,
+        eth_worker,
+        storage,
         config_options.confirmations_for_eth_event,
         eth_req_receiver,
-        db_pool,
     );
     tokio::spawn(eth_watch.run());
 
---
+++ core/bin/zksync_core/src/eth_watch/worker.rs
@@ -0,0 +1,196 @@
+use anyhow::format_err;
+use ethabi::Hash;
+use std::convert::TryFrom;
+use std::time::Instant;
+use web3::contract::{Contract, Options};
+use web3::transports::Http;
+use web3::types::{BlockNumber, Filter, FilterBuilder};
+use web3::Web3;
+use zksync_contracts::zksync_contract;
+use zksync_types::ethereum::CompleteWithdrawalsTx;
+use zksync_types::{Address, Nonce, PriorityOp, H160};
+
+#[async_trait::async_trait]
+pub trait EthWorker {
+    /// Filters and parses the priority operation events from the Ethereum
+    /// within the provided range of blocks.
+    /// Returns the list of priority operations together with the block
+    /// numbers.
+    async fn get_priority_op_events(
+        &self,
+        from: BlockNumber,
+        to: BlockNumber,
+    ) -> anyhow::Result<Vec<PriorityOp>>;
+    async fn get_complete_withdrawals_event(
+        &self,
+        from: BlockNumber,
+        to: BlockNumber,
+    ) -> anyhow::Result<Vec<CompleteWithdrawalsTx>>;
+    async fn block_number(&self) -> anyhow::Result<u64>;
+    async fn get_auth_fact(&self, address: Address, nonce: Nonce) -> anyhow::Result<Vec<u8>>;
+    async fn get_first_pending_withdrawal_index(&self) -> anyhow::Result<u32>;
+    async fn get_number_of_pending_withdrawals(&self) -> anyhow::Result<u32>;
+}
+
+pub struct EthWorkerHttp {
+    web3: Web3<Http>,
+    zksync_contract: (ethabi::Contract, Contract<Http>),
+}
+
+impl EthWorkerHttp {
+    pub fn new(web3: Web3<Http>, zksync_contract_addr: H160) -> Self {
+        let zksync_contract = {
+            (
+                zksync_contract(),
+                Contract::new(web3.eth(), zksync_contract_addr, zksync_contract()),
+            )
+        };
+        Self {
+            zksync_contract,
+            web3,
+        }
+    }
+}
+
+fn create_filter(
+    address: Address,
+    from: BlockNumber,
+    to: BlockNumber,
+    topics: Vec<Hash>,
+) -> Filter {
+    FilterBuilder::default()
+        .address(vec![address])
+        .from_block(from)
+        .to_block(to)
+        .topics(Some(topics), None, None, None)
+        .build()
+}
+
+#[async_trait::async_trait]
+impl EthWorker for EthWorkerHttp {
+    async fn get_priority_op_events(
+        &self,
+        from: BlockNumber,
+        to: BlockNumber,
+    ) -> anyhow::Result<Vec<PriorityOp>> {
+        let start = Instant::now();
+
+        let priority_op_event_topic = self
+            .zksync_contract
+            .0
+            .event("NewPriorityRequest")
+            .expect("main contract abi error")
+            .signature();
+        let filter = create_filter(
+            self.zksync_contract.1.address(),
+            from,
+            to,
+            vec![priority_op_event_topic],
+        );
+        let result = self
+            .web3
+            .eth()
+            .logs(filter)
+            .await?
+            .into_iter()
+            .map(|event| {
+                PriorityOp::try_from(event).map_err(|e| {
+                    format_err!("Failed to parse priority queue event log from ETH: {:?}", e)
+                })
+            })
+            .collect();
+        metrics::histogram!("eth_watcher.get_priority_op_events", start.elapsed());
+        result
+    }
+
+    async fn get_complete_withdrawals_event(
+        &self,
+        from: BlockNumber,
+        to: BlockNumber,
+    ) -> anyhow::Result<Vec<CompleteWithdrawalsTx>> {
+        let start = Instant::now();
+
+        let complete_withdrawals_event_topic = self
+            .zksync_contract
+            .0
+            .event("PendingWithdrawalsComplete")
+            .expect("main contract abi error")
+            .signature();
+        let filter = create_filter(
+            self.zksync_contract.1.address(),
+            from,
+            to,
+            vec![complete_withdrawals_event_topic],
+        );
+        let result = self
+            .web3
+            .eth()
+            .logs(filter)
+            .await?
+            .into_iter()
+            .map(CompleteWithdrawalsTx::try_from)
+            .collect();
+
+        metrics::histogram!(
+            "eth_watcher.get_complete_withdrawals_event",
+            start.elapsed()
+        );
+        result
+    }
+
+    async fn block_number(&self) -> anyhow::Result<u64> {
+        Ok(self.web3.eth().block_number().await?.as_u64())
+    }
+
+    async fn get_auth_fact(&self, address: Address, nonce: u32) -> anyhow::Result<Vec<u8>> {
+        Ok(self
+            .zksync_contract
+            .1
+            .query(
+                "authFacts",
+                (address, u64::from(nonce)),
+                None,
+                Options::default(),
+                None,
+            )
+            .await
+            .map_err(|e| format_err!("Failed to query contract authFacts: {}", e))?)
+    }
+
+    async fn get_first_pending_withdrawal_index(&self) -> anyhow::Result<u32> {
+        Ok(self
+            .zksync_contract
+            .1
+            .query(
+                "firstPendingWithdrawalIndex",
+                (),
+                None,
+                Options::default(),
+                None,
+            )
+            .await
+            .map_err(|e| {
+                format_err!(
+                    "Failed to query contract firstPendingWithdrawalIndex: {}",
+                    e
+                )
+            })?)
+    }
+
+    async fn get_number_of_pending_withdrawals(&self) -> anyhow::Result<u32> {
+        Ok(self
+            .zksync_contract
+            .1
+            .query(
+                "numberOfPendingWithdrawals",
+                (),
+                None,
+                Options::default(),
+                None,
+            )
+            .await
+            .map_err(|e| {
+                format_err!("Failed to query contract numberOfPendingWithdrawals: {}", e)
+            })?)
+    }
+}
