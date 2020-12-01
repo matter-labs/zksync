a4fe493d280ff678f86189cf174a5e70962747ae
Chris Sosnin <chris125_@live.com>
2020-12-01 15:14:46 +0300

    Tests for config module

--- core/lib/storage/src/tests/config.rs
+++ core/lib/storage/src/tests/config.rs
@@ -1,1 +1,13 @@
-// TODO: add tests (#1055).
+// External imports
+// Workspace imports
+// Local imports
+use crate::tests::db_test;
+use crate::{QueryResult, StorageProcessor};
+
+/// Server config should be loaded without errors.
+#[db_test]
+async fn test_load_config(mut storage: StorageProcessor<'_>) -> QueryResult<()> {
+    storage.config_schema().load_config().await?;
+
+    Ok(())
+}
