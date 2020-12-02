6c75643634d41b4bc95453f1d8e6253fd9dd83af
Chris Sosnin <chris125_@live.com>
2020-12-02 13:57:03 +0300

    Check for query result in config test

--- core/lib/storage/src/tests/config.rs
+++ core/lib/storage/src/tests/config.rs
@@ -7,7 +7,9 @@ use crate::{QueryResult, StorageProcessor};
 /// Server config should be loaded without errors.
 #[db_test]
 async fn test_load_config(mut storage: StorageProcessor<'_>) -> QueryResult<()> {
-    storage.config_schema().load_config().await?;
+    let config = storage.config_schema().load_config().await?;
+    assert!(config.contract_addr.is_some());
+    assert!(config.gov_contract_addr.is_some());
 
     Ok(())
 }
