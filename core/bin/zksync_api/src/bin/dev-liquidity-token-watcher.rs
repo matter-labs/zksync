b0e3e62a1d20a3b6c16040a4af39e4536aa0645e
Danil Lugovskoy <deniallugo@gmail.com>
2020-12-08 12:00:04 +0300

    Load tokens from disk

--- core/bin/zksync_api/src/bin/dev-liquidity-token-watcher.rs
+++ core/bin/zksync_api/src/bin/dev-liquidity-token-watcher.rs
@@ -1,28 +1,44 @@
 //! Token watcher implementation for dev environment
 //!
-//! Implements Uniswap API
+//! Implements Uniswap API for token which are deployed in localhost network
 
 use actix_cors::Cors;
 use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
 use bigdecimal::BigDecimal;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TokenData {
+    address: String,
+    name: String,
+    volume: BigDecimal,
+}
+
+type Tokens = HashMap<String, TokenData>;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GrqaphqlQuery {
     query: String,
 }
 
-async fn handle_graphql(params: web::Json<GrqaphqlQuery>) -> Result<HttpResponse> {
+async fn handle_graphql(
+    params: web::Json<GrqaphqlQuery>,
+    tokens: web::Data<Tokens>,
+) -> Result<HttpResponse> {
     // Now, we support only one graphql query, we will add full
     let query_parser = Regex::new(r#"\{token\(id:\s"(?P<address>.*?)"\).*"#).expect("Right regexp");
     let caps = query_parser.captures(&params.query).unwrap();
     let address = &caps["address"];
-    let volume = match address {
-        "0x6b175474e89094c44da98b954eedeac495271d0f" => BigDecimal::from(30000),
-        "0x38A2fDc11f526Ddd5a607C1F251C065f40fBF2f7" => BigDecimal::from(50),
-        _ => BigDecimal::from(0),
+    let volume = if let Some(token) = tokens.get(address) {
+        token.volume.clone()
+    } else {
+        BigDecimal::from(0)
     };
 
     let response = json!({
@@ -35,16 +51,46 @@ async fn handle_graphql(params: web::Json<GrqaphqlQuery>) -> Result<HttpResponse
     Ok(HttpResponse::Ok().json(response))
 }
 
+fn load_tokens(path: &Path) -> Tokens {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    let values: Vec<Value> = serde_json::from_reader(reader).unwrap();
+    let mut tokens: Tokens = Default::default();
+    for value in values {
+        if let Value::Object(value) = value {
+            tokens.insert(
+                value["address"].to_string(),
+                TokenData {
+                    address: value["address"].to_string(),
+                    name: value["name"].to_string(),
+                    volume: BigDecimal::from(500),
+                },
+            );
+        };
+    }
+    let phnx_token = TokenData {
+        address: "0x6b175474e89094c44da98b954eedeac495271d0f".to_string(),
+
+        name: "PHNX".to_string(),
+        volume: BigDecimal::from(30),
+    };
+    tokens.insert(phnx_token.address.clone(), phnx_token);
+    tokens
+}
+
 fn main() {
     env_logger::init();
 
     let mut runtime = actix_rt::System::new("dev-liquidity-token-watcher");
 
+    let tokens = load_tokens(Path::new(&"etc/tokens/localhost.json".to_string()));
     runtime.block_on(async {
         HttpServer::new(move || {
             App::new()
                 .wrap(middleware::Logger::default())
                 .wrap(Cors::new().send_wildcard().max_age(3600).finish())
+                .data(tokens.clone())
                 .route("/graphql", web::post().to(handle_graphql))
         })
         .bind("0.0.0.0:9975")
