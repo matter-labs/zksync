a5b535cd177cbb9dc1c065d297a743e34414a77d
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2020-12-09 17:37:22 +0200

    Remove unused constants

--- infrastructure/explorer/src/constants.js
+++ infrastructure/explorer/src/constants.js
@@ -5,20 +5,6 @@ export const TX_BATCH_SIZE = 50;
 
 export const BLOCK_STORAGE_CONSTANT = 'BLOCK_STORAGE';
 export const BLOCK_TRANSACTIONS_STORAGE_CONSTANT = 'BLOCK_TX_STORAGE';
-export const TOKEN_STORAGE_CONSTANT = 'TOKEN_STORAGE';
-export const TRANSACTION_STORAGE_CONSTANT = 'TRANSACTION_STORAGE';
-
-export function getBlockStorageSlot(blockNumber) {
-    return BLOCK_STORAGE_CONSTANT + blockNumber;
-}
-
-export function getBlockTxStorageSlot(blockNumber) {
-    return BLOCK_TRANSACTIONS_STORAGE_CONSTANT + blockNumber;
-}
-
-export function getTransactionStorageSlot(hash) {
-    return TRANSACTION_STORAGE_CONSTANT + hash;   
-}
 
 export const blockchainExplorerTx = store.network === 'localhost' ? 'http://localhost:8000'
          : store.network === 'mainnet' ? `https://etherscan.io/tx`
