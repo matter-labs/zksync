874a6f46230540557f58b1ea68d4a6f83868d800
Aleksei Sidorov <gorthauer87@yandex.ru>
2020-12-11 14:03:11 +0300

    Rename GLM to tGLM and add tGLM alias into the tokens list

--- core/bin/zksync_api/src/api_server/rest/v1/test_utils.rs
+++ core/bin/zksync_api/src/api_server/rest/v1/test_utils.rs
@@ -186,7 +186,7 @@ impl TestServerConfig {
             .tokens_schema()
             .store_token(Token::new(
                 16,
-                Address::from_str("d94e3dc39d4cad1dad634e7eb585a57a19dc7efe ").unwrap(),
+                Address::from_str("d94e3dc39d4cad1dad634e7eb585a57a19dc7efe").unwrap(),
                 "GNT",
                 18,
             ))
