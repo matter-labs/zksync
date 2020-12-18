b00a1a9dc49415fcdb171039bff12c8eb01e07e6
Alex Z <a.zarudnyy@matterlabs.dev>
2020-12-18 15:39:32 +0200

    Improve the incorrect transaction signature error message

--- core/lib/types/src/tx/mod.rs
+++ core/lib/types/src/tx/mod.rs
@@ -30,3 +30,9 @@ pub use self::primitives::{
 };
 
 pub(crate) use self::primitives::signature_cache::VerifiedSignatureCache;
+
+pub(crate) static TRANSACTION_SIGNATURE_ERROR: &str = r#"
+The transaction signature is incorrect.
+Check if the sender address matches the private key,
+the recipient address is not zero,
+and the amount is correct and packable"#;
