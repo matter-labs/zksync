31cd30ae31d0beffa726317e44179e4a4759f6de
perekopskiy <perekopskiy@dlit.dp.ua>
2020-12-18 17:29:43 +0200

    Add verify proof method for 2 types of smt

--- core/lib/crypto/src/merkle_tree/tests.rs
+++ core/lib/crypto/src/merkle_tree/tests.rs
@@ -1,45 +1,39 @@
-use super::hasher::Hasher;
 use crate::merkle_tree::{parallel_smt, sequential_smt, RescueHasher};
-use crate::primitives::GetBits;
 use crate::rand::{Rng, SeedableRng, XorShiftRng};
 use crate::{Engine, Fr};
 use serde::{Deserialize, Serialize};
 
-/// Applies the proof for the element and compares it against the expected
-/// root hash.
-fn verify_proof<T>(
-    element_index: u64,
-    element: T,
-    hasher: RescueHasher<Engine>,
-    merkle_proof: Vec<(Fr, bool)>,
-    expected_root: Fr,
-) where
-    T: GetBits,
-{
-    // To check the proof, we fold it starting from the hash of the value
-    // and updating with the hashes from the proof.
-    // We should obtain the root hash at the end if the proof is correct.
-    let mut proof_index = 0;
-    let mut aggregated_hash = hasher.hash_bits(element.get_bits_le());
-    for (level, (hash, dir)) in merkle_proof.into_iter().enumerate() {
-        let (lhs, rhs) = if dir {
-            proof_index |= 1 << level;
-            (hash, aggregated_hash)
-        } else {
-            (aggregated_hash, hash)
-        };
+/// Checks if verify_proof method works correctly
+#[test]
+fn test_verify_proof() {
+    let depth = 4;
 
-        aggregated_hash = hasher.compress(&lhs, &rhs, level);
+    let mut rng = XorShiftRng::from_seed([1, 2, 3, 4]);
+    let elements = rng.gen_iter::<u64>().take(1 << depth);
+
+    let mut par_tree = parallel_smt::SparseMerkleTree::<u64, Fr, RescueHasher<Engine>>::new(depth);
+    let mut seq_tree =
+        sequential_smt::SparseMerkleTree::<u64, Fr, RescueHasher<Engine>>::new(depth);
+
+    let elements: Vec<u64> = elements.collect();
+    for (idx, item) in elements.iter().enumerate() {
+        let idx = idx as u32;
+        par_tree.insert(idx, *item);
+        seq_tree.insert(idx as usize, *item);
+
+        let par_merkle_path = par_tree.merkle_path(idx);
+        let seq_merkle_path = seq_tree.merkle_path(idx as usize);
+
+        assert!(par_tree.verify_proof(idx, *item, par_merkle_path));
+        assert!(seq_tree.verify_proof(idx, *item, seq_merkle_path));
     }
+    let par_merkle_path = par_tree.merkle_path(0);
+    let seq_merkle_path = seq_tree.merkle_path(0);
 
-    assert_eq!(
-        proof_index, element_index,
-        "Got unexpected element index while verifying the proof"
-    );
-    assert_eq!(
-        aggregated_hash, expected_root,
-        "Got unexpected root hash while verifying the proof"
-    );
+    assert!(!par_tree.verify_proof(0, elements[1], par_merkle_path.clone()));
+    assert!(!par_tree.verify_proof(1, elements[0], par_merkle_path));
+    assert!(!seq_tree.verify_proof(0, elements[1], seq_merkle_path.clone()));
+    assert!(!seq_tree.verify_proof(1, elements[0], seq_merkle_path));
 }
 
 /// Verifies that for a randomly-chosen sequence of elements
@@ -80,21 +74,8 @@ fn cross_trees_merkle_path_comparison() {
         );
 
         // Check that verifying proofs provides expected results.
-        verify_proof(
-            idx as u64,
-            item,
-            seq_tree.hasher.clone(),
-            seq_merkle_path,
-            seq_tree.root_hash(),
-        );
-
-        verify_proof(
-            idx as u64,
-            item,
-            par_tree.hasher.clone(),
-            par_merkle_path,
-            par_tree.root_hash(),
-        );
+        assert!(seq_tree.verify_proof(idx, item, seq_merkle_path));
+        assert!(par_tree.verify_proof(idx, item, par_merkle_path));
     }
 }
 
@@ -137,24 +118,11 @@ fn simulate_transfer_to_new_par_tree_seq_tree() {
     assert_eq!(par_audit_to_before, seq_audit_to_before);
 
     // Check the sequential tree proof.
-    let element_idx = to_account_id as u64;
+    let element_idx = to_account_id;
     let element: u64 = 0;
-    verify_proof(
-        element_idx,
-        element,
-        seq_tree.hasher.clone(),
-        seq_audit_to_before,
-        seq_tree.root_hash(),
-    );
 
-    // Check the parallel tree proof.
-    verify_proof(
-        element_idx,
-        element,
-        par_tree.hasher.clone(),
-        par_audit_to_before,
-        par_tree.root_hash(),
-    );
+    assert!(seq_tree.verify_proof(element_idx, element, seq_audit_to_before));
+    assert!(par_tree.verify_proof(element_idx, element, par_audit_to_before));
 }
 
 /// Checks if root and path from middle leaf of merkle tree with pre-defined elements is correct.
