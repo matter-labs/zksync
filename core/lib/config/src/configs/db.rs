ff8e471433142977fdc4187cd5ac45752d799b70
Igor Aleksanov <popzxc@yandex.ru>
2020-12-18 13:59:46 +0300

    Add test for db config

--- core/lib/config/src/configs/db.rs
+++ core/lib/config/src/configs/db.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 use crate::envy_load;
 
 /// Used database configuration.
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct DBConfig {
     /// Amount of open connections to the database held by server in the pool.
     pub pool_size: usize,
@@ -15,5 +15,30 @@ pub struct DBConfig {
 impl DBConfig {
     pub fn from_env() -> Self {
         envy_load!("db", "DB_")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::configs::test_utils::set_env;
+
+    fn expected_config() -> DBConfig {
+        DBConfig {
+            pool_size: 10,
+            url: "postgres://postgres@localhost/plasma".into(),
+        }
+    }
+
+    #[test]
+    fn from_env() {
+        let config = r#"
+DB_POOL_SIZE="10"
+DB_URL="postgres://postgres@localhost/plasma"
+        "#;
+        set_env(config);
+
+        let actual = DBConfig::from_env();
+        assert_eq!(actual, expected_config());
     }
 }
