581f586e2bfca0423761171a97fa973ab4bac592
Danil Lugovskoy <deniallugo@gmail.com>
2021-01-11 17:18:59 +0300

    Add cache for nodes

--- infrastructure/ci-scripts/cache.sh
+++ infrastructure/ci-scripts/cache.sh
@@ -6,3 +6,10 @@ if [ ! -d "$CACHE_DIR/$rust_hash" ]; then
 fi
 rm -rf $CACHE_DIR/$rust_hash/target
 cp -r ./target $CACHE_DIR/$rust_hash/target
+
+node_hash=$(md5sum yarn.lock | awk '{ print $1 }')
+if [ -d "$CACHE_DIR/$node_hash" ]; then
+  mkdir -p "$CACHE_DIR/$node_hash"
+fi
+rm -rf $CACHE_DIR/$node_hash/node_modules
+cp -r  ./node_modules $CACHE_DIR/$node_hash/node_modules
