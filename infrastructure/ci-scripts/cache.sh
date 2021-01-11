b4fb1a827c7e07138e7b97887f6bcad95e6e74c3
Danil Lugovskoy <deniallugo@gmail.com>
2021-01-11 17:37:32 +0300

    Fix cache

--- infrastructure/ci-scripts/cache.sh
+++ infrastructure/ci-scripts/cache.sh
@@ -8,7 +8,7 @@ rm -rf $CACHE_DIR/$rust_hash/target
 cp -r ./target $CACHE_DIR/$rust_hash/target
 
 node_hash=$(md5sum yarn.lock | awk '{ print $1 }')
-if [ -d "$CACHE_DIR/$node_hash" ]; then
+if [ ! -d "$CACHE_DIR/$node_hash" ]; then
   mkdir -p "$CACHE_DIR/$node_hash"
 fi
 rm -rf $CACHE_DIR/$node_hash/node_modules
--- infrastructure/ci-scripts/run_services.sh
+++ infrastructure/ci-scripts/run_services.sh
@@ -3,7 +3,7 @@ nohup zk f $ZKSYNC_HOME/target/release/zksync_server &>$ZKSYNC_HOME/server.log &
 sleep 1
 
 echo "Launching dummy-prover..."
-nohup zk f $ZKSYNC_HOME/target/release/dummy_prover "dummy-prover-instance" &>$ZKSYNC_HOME/dummy_prover.log &
+nohup zk f $ZKSYNC_HOME/target/release/dummy_prover &>$ZKSYNC_HOME/dummy_prover.log &
 
 # Wait for server to start
 sleep 10
