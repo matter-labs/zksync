eac2fc6f38a30f352d37b8472e012cd26ef12cf9
Danil Lugovskoy <deniallugo@gmail.com>
2021-01-12 12:28:34 +0300

    rerun dummy prover

--- infrastructure/ci-scripts/run_services.sh
+++ infrastructure/ci-scripts/run_services.sh
@@ -1,9 +1,9 @@
 echo "Launching server..."
-nohup zk server &>$ZKSYNC_HOME/server.log &
+zk server &>server.log &
 sleep 1
 
 echo "Launching dummy-prover..."
-nohup zk dummy_prover run &>$ZKSYNC_HOME/dummy_prover.log &
+zk dummy-prover run &>dummy_prover.log &
 
 # Wait for server to start
 sleep 10
