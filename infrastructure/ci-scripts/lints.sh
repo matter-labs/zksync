114ecb184c177fb06a24951e85769b44e91ceefc
Igor Aleksanov <popzxc@yandex.ru>
2021-01-13 12:42:19 +0300

    Remove lints script

--- infrastructure/ci-scripts/lints.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-zk fmt --check
-ulimit -S -c unlimited
-cargo fmt --all -- --check
-zk f cargo clippy --tests --benches -- -D warnings
-#cd sdk/zksync-crypto && cargo fmt -- --check
-#cd sdk/zksync-crypto && cargo clippy --all --tests --benches -- -D warnings
