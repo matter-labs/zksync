4249490c4e1b2b8f51c0eb2d1e74ddd3bbaf0816
Danil Lugovskoy <deniallugo@gmail.com>
2021-01-19 18:59:24 +0300

    Update macros and documentation

--- core/lib/vlog/src/lib.rs
+++ core/lib/vlog/src/lib.rs
@@ -1,25 +1,13 @@
 //! A set of logging macros that print not only timestamp and log level,
 //! but also filename, line and column.
 //!
-//! They behave just like usual log::warn, vlog::info, etc.
+//! They behave just like usual tracing::warn, tracing::info, etc.
+//! For warn and error macros we are adding file line and column to tracing variables
 //!
+//! The format of the logs in stdout can be `plain` or` json` and is set by the `MISC_LOG_FORMAT` env variable.
 //!
-//! In fact, printing file, line and column can be done with a custom formatter for env_logger, like so:
-//!
-//!```ignore
-//! use env_logger::Builder;
-//! use std::io::Write;
-//!
-//! env_logger::builder()
-//!     .format(|buf, record| {
-//!         writeln!(buf, "{:?}", record.file());
-//!         writeln!(buf, "{}", record.args())
-//!     })
-//!     .init();
-//!```
-//!
-//! But I couldn't easily replicate its default behavior in my custom logger.
-//!
+//! Full documentation for the `tracing` crate here https://docs.rs/tracing/
+
 pub use tracing as __tracing;
 pub use tracing::{debug, info, log, trace};
 
@@ -27,11 +15,10 @@ pub use tracing::{debug, info, log, trace};
 macro_rules! warn {
     ($($arg:tt)*) => {
         vlog::__tracing::warn!(
-            "[{}:{}:{}] {}",
-            file!(),
-            line!(),
-            column!(),
-            format!($($arg)*)
+            file=file!(),
+            line=line!(),
+            column=column!(),
+            $($arg)*
         );
     };
 }
@@ -40,11 +27,10 @@ macro_rules! warn {
 macro_rules! error {
     ($($arg:tt)*) => {
         vlog::__tracing::error!(
-            "[{}:{}:{}] {}",
-            file!(),
-            line!(),
-            column!(),
-            format!($($arg)*)
+            file=file!(),
+            line=line!(),
+            column=column!(),
+            $($arg)*
         );
     };
 }
