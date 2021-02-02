c302b211a0c848b4fd211acecc454e94db657fcb
Vitaly Drogan <vitaliy.drogan@gmail.com>
2021-02-02 10:37:38 +0200

    add one migrations

--- core/lib/storage/migrations/2020-11-26-222750_multiple_batch_signatures/down.sql
+++
@@ -1,4 +0,0 @@
--- This might not help as long as batch_id is not necessarily unique which
--- makes this change not completely revertible.
-ALTER TABLE txs_batches_signatures DROP COLUMN id;
-ALTER TABLE txs_batches_signatures ADD PRIMARY KEY (batch_id);
--- core/lib/storage/migrations/2021-01-27-120015_eth_parameters_for_aggregate_op/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-ALTER TABLE eth_parameters RENAME COLUMN "commit_ops" TO "last_committed_block";
-ALTER TABLE eth_parameters RENAME COLUMN "verify_ops" TO "last_verified_block";
-ALTER TABLE eth_parameters RENAME COLUMN "withdraw_ops" TO "last_executed_block";
-
-UPDATE eth_parameters
-SET last_executed_block = (SELECT last_verified_block FROM eth_parameters WHERE id = true);
