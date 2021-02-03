85a6f2b11871ab45db0f742323b6f3703f89581c
Vladyslav-Bochok <vladbochok1@gmail.com>
2021-02-03 17:29:15 +0200

    Use more appropriate names

--- core/lib/types/src/tx/mod.rs
+++ core/lib/types/src/tx/mod.rs
@@ -26,8 +26,8 @@ pub use self::{
 
 // Re-export primitives associated with transactions.
 pub use self::primitives::{
-    batch_sign_data::BatchSignData, batch_signature::EthBatchSignatures,
-    eip1271_signature::EIP1271Signature, eth_signature::TxEthSignature,
+    eip1271_signature::EIP1271Signature, eth_batch_sign_data::EthBatchSignData,
+    eth_batch_signature::EthBatchSignatures, eth_signature::TxEthSignature,
     packed_eth_signature::PackedEthSignature, packed_public_key::PackedPublicKey,
     packed_signature::PackedSignature, signature::TxSignature, time_range::TimeRange,
     tx_hash::TxHash,
