b82a8986e462629d24ae0ec5cd70826a61c063af
Vladyslav-Bochok <vladbochok1@gmail.com>
2021-02-25 15:35:36 +0200

    Add notifier

--- core/lib/notifier/src/matter_most_notifier.rs
+++ core/lib/notifier/src/matter_most_notifier.rs
@@ -1,23 +1,21 @@
 use reqwest::{Client, Url};
 
 pub struct MatterMostNotifier {
-    username: String,
     webhook_url: Url,
     client: Client,
 }
 
 impl MatterMostNotifier {
-    pub fn new(username: String, webhook_url: Url) -> Self {
+    pub fn new(webhook_url: Url) -> Self {
         Self {
-            username,
             webhook_url,
             client: Client::new(),
         }
     }
 
-    pub async fn send_notify(&self, text: &str) -> anyhow::Result<()> {
+    pub async fn send_notify(&self, username: &str, text: &str) -> anyhow::Result<()> {
         let parameters = serde_json::json!({
-            "username": serde_json::to_value(&self.username)?,
+            "username": serde_json::to_value(&username)?,
             "text": serde_json::to_value(text)?,
         });
 
--- core/lib/utils/src/lib.rs
+++ core/lib/utils/src/lib.rs
@@ -3,7 +3,6 @@
 mod convert;
 mod env_tools;
 mod format;
-mod matter_most_notifier;
 pub mod panic_notify;
 mod serde_wrappers;
 mod string;
@@ -11,6 +10,5 @@ mod string;
 pub use convert::*;
 pub use env_tools::*;
 pub use format::*;
-pub use matter_most_notifier::*;
 pub use serde_wrappers::*;
 pub use string::*;
