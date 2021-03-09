cab3a42bda5e480cf5dc294652b09a47e8f892a1
perekopskiy <perekopskiy@dlit.dp.ua>
2021-03-09 20:10:02 +0400

    Create api v0.2 actor

---
+++ core/bin/zksync_api/src/api_server/rest/v02/foo.rs
@@ -0,0 +1,40 @@
+//! Foo part of API implementation.
+
+// Built-in uses
+
+// External uses
+use actix_web::{web, Scope};
+
+// Workspace uses
+use zksync_config::ZkSyncConfig;
+use zksync_types::network::Network;
+
+// Local uses
+use super::Json;
+
+/// Shared data between `api/v0.2/foo` endpoints.
+#[derive(Debug, Clone)]
+struct ApiFooData {
+    network: Network,
+}
+
+impl ApiFooData {
+    fn new(config: &ZkSyncConfig) -> Self {
+        Self {
+            network: config.chain.eth.network,
+        }
+    }
+}
+
+// Server implementation
+async fn network(data: web::Data<ApiFooData>) -> Json<Network> {
+    Json(data.network)
+}
+
+pub fn api_scope(config: &ZkSyncConfig) -> Scope {
+    let data = ApiFooData::new(config);
+
+    web::scope("foo")
+        .data(data)
+        .route("network", web::get().to(network))
+}
---
+++ core/bin/zksync_api/src/api_server/rest/v02/mod.rs
@@ -0,0 +1,19 @@
+//! First stable API implementation.
+
+// External uses
+use actix_web::{
+    web::{self, Json},
+    Scope,
+};
+
+// Workspace uses
+use zksync_config::ZkSyncConfig;
+
+// Local uses
+use crate::api_server::tx_sender::TxSender;
+
+mod foo;
+
+pub(crate) fn api_scope(_tx_sender: TxSender, zk_config: &ZkSyncConfig) -> Scope {
+    web::scope("/api/v0.2").service(foo::api_scope(&zk_config))
+}
