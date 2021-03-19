9e85ce93693727899689185f6e1eb5a09e1f5d9e
Chris Sosnin <chris125_@live.com>
2021-03-19 13:54:26 +0300

    Update config

--- core/lib/config/src/configs/database.rs
+++ core/lib/config/src/configs/database.rs
@@ -18,13 +18,15 @@ pub struct DBConfig {
     pub rejected_transactions_max_age: u64,
     /// Sleep time (in hours) of the actor responsible for deleting failed transactions from the database.
     pub rejected_transactions_cleaner_interval: u64,
+    /// Channel name to accept event notifications on.
+    pub listen_channel_name: String,
 }
 
 impl DBConfig {
     const SECS_PER_HOUR: u64 = 3600;
 
     pub fn from_env() -> Self {
-        envy_load!("contracts", "DATABASE_")
+        envy_load!("database", "DATABASE_")
     }
 
     pub fn rejected_transactions_max_age(&self) -> chrono::Duration {
@@ -47,6 +49,7 @@ mod tests {
             url: "postgres://postgres@localhost/plasma".into(),
             rejected_transactions_max_age: 336,
             rejected_transactions_cleaner_interval: 24,
+            listen_channel_name: "channel".into(),
         }
     }
 
@@ -57,6 +60,7 @@ DATABASE_POOL_SIZE="10"
 DATABASE_URL="postgres://postgres@localhost/plasma"
 DATABASE_REJECTED_TRANSACTIONS_MAX_AGE="336"
 DATABASE_REJECTED_TRANSACTIONS_CLEANER_INTERVAL="24"
+DATABASE_LISTEN_CHANNEL_NAME="channel"
         "#;
         set_env(config);
 
