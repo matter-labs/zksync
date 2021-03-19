5f42e4e83795b85b8decf1ddbbe4edfc1b1e811b
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2021-03-19 18:00:16 +0200

    Add swaps stubs for circuit and storage

--- core/lib/storage/src/chain/block/conversion.rs
+++ core/lib/storage/src/chain/block/conversion.rs
@@ -163,6 +163,7 @@ impl NewExecutedTransaction {
                     serde_json::from_value(tx["target"].clone()).unwrap(),
                     serde_json::from_value(tx["target"].clone()).unwrap(),
                 ),
+                ZkSyncTx::Swap(_) => todo!(),
             };
 
         let from_account: Vec<u8> = hex::decode(cut_prefix(&from_account_hex)).unwrap();
