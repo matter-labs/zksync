459a09287bc8df66f12b74f463bf744effae57c7
Igor Aleksanov <popzxc@yandex.ru>
2021-04-03 19:43:16 +0300

    Split report_collector in multiple files

--- core/tests/loadnext/src/report_collector/metrics_collector.rs
+++ core/tests/loadnext/src/report_collector/metrics_collector.rs
@@ -3,9 +3,7 @@ use std::{
     time::Duration,
 };
 
-use futures::{channel::mpsc::Receiver, StreamExt};
-
-use crate::report::{ActionType, Report};
+use crate::report::ActionType;
 
 #[derive(Debug, Clone)]
 pub struct TimeHistogram {
@@ -94,32 +92,6 @@ impl MetricsCollector {
         self.action_stats
             .entry(action)
             .and_modify(|hist| hist.add_metric(time));
-    }
-}
-
-#[derive(Debug)]
-pub struct ReportCollector {
-    reports_stream: Receiver<Report>,
-    metrics_collector: MetricsCollector,
-}
-
-impl ReportCollector {
-    pub fn new(reports_stream: Receiver<Report>) -> Self {
-        Self {
-            reports_stream,
-            metrics_collector: MetricsCollector::new(),
-        }
-    }
-
-    pub async fn run(mut self) {
-        while let Some(report) = self.reports_stream.next().await {
-            vlog::trace!("Report: {:?}", &report);
-
-            self.metrics_collector
-                .add_metric(report.action, report.time);
-
-            todo!()
-        }
     }
 }
 
---
+++ core/tests/loadnext/src/report_collector/mod.rs
@@ -0,0 +1,31 @@
+use futures::{channel::mpsc::Receiver, StreamExt};
+
+use crate::{report::Report, report_collector::metrics_collector::MetricsCollector};
+
+mod metrics_collector;
+
+#[derive(Debug)]
+pub struct ReportCollector {
+    reports_stream: Receiver<Report>,
+    metrics_collector: MetricsCollector,
+}
+
+impl ReportCollector {
+    pub fn new(reports_stream: Receiver<Report>) -> Self {
+        Self {
+            reports_stream,
+            metrics_collector: MetricsCollector::new(),
+        }
+    }
+
+    pub async fn run(mut self) {
+        while let Some(report) = self.reports_stream.next().await {
+            vlog::trace!("Report: {:?}", &report);
+
+            self.metrics_collector
+                .add_metric(report.action, report.time);
+
+            todo!()
+        }
+    }
+}
