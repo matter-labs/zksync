0872cd5a75d8eb9b90df08fd1efd77cbe4e7005f
Igor Aleksanov <popzxc@yandex.ru>
2021-04-07 09:18:46 +0300

    Add README.md

--- core/tests/loadnext/src/main.rs
+++ core/tests/loadnext/src/main.rs
@@ -1,6 +1,6 @@
 //! Loadtest: an utility to stress-test the zkSync server.
 //!
-//! In order to launch it, you must provide required environmental variables, for details see `config.rs`.
+//! In order to launch it, you must provide required environmental variables, for details see `README.md`.
 //! Without required variables provided, test is launched in the localhost/development mode with some hard-coded
 //! values to check the local zkSync deployment.
 
