fba305e9f3dd5d32ee04809226dd243923c57ca7
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2021-04-16 17:33:57 +0300

    Sign message

--- infrastructure/regen-root-hash/src/main.rs
+++ infrastructure/regen-root-hash/src/main.rs
@@ -2,31 +2,39 @@ mod account;
 mod hasher;
 #[cfg(test)]
 mod tests;
+mod utils;
 
+use std::env;
+use utils::sign_update_message;
 use zksync_circuit::witness::utils::fr_from_bytes;
 
 use account::read_accounts;
 use hasher::{get_state_root_hash, BALANCE_TREE_11, BALANCE_TREE_32};
 
 fn main() {
-    let accounts = read_accounts(
-        String::from("./sample/accounts"),
-        String::from("./sample/balances"),
-    )
-    .unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    let current_hash_str = "2bd61f42837c0fa77fc113b3b341c520edb1ffadefc48c2b907901aaaf42b906";
-    let current_hash_bytes = hex::decode(current_hash_str).unwrap();
-    let current_hash = fr_from_bytes(current_hash_bytes);
+    let current_hash = args[1].clone();
+    let accounts_file_path = args[2].clone();
+    let balances_file_path = args[3].clone();
+    let private_key = args[4].clone();
 
-    let hash_11 = get_state_root_hash(&accounts, &BALANCE_TREE_11);
-    print!("hash11: {} \n", hash_11.to_string());
+    let accounts = read_accounts(accounts_file_path, balances_file_path).unwrap();
+
+    let current_hash_bytes = hex::decode(current_hash).unwrap();
+    let current_hash_fr = fr_from_bytes(current_hash_bytes);
+
+    let old_hash = get_state_root_hash(&accounts, &BALANCE_TREE_11);
+    print!("OldHash: {} \n", old_hash.to_string());
 
     assert_eq!(
-        hash_11, current_hash,
-        "The recalculated hash is not equal to the current one."
+        old_hash, current_hash_fr,
+        "The recalculated hash is not equal to the current one.\n"
     );
 
-    let hash_32 = get_state_root_hash(&accounts, &BALANCE_TREE_32);
-    print!("hash32: {} \n", hash_32.to_string());
+    let new_hash = get_state_root_hash(&accounts, &BALANCE_TREE_32);
+    print!("NewHash: {} \n", new_hash.to_string());
+
+    let signature = sign_update_message(private_key, old_hash, new_hash);
+    print!("Sigтature: {}\n", signature);
 }
---
+++ infrastructure/regen-root-hash/src/utils.rs
@@ -0,0 +1,31 @@
+use parity_crypto::{publickey::sign, Keccak256};
+use zksync_crypto::{
+    ff::{PrimeField, PrimeFieldRepr},
+    Fr,
+};
+use zksync_types::H256;
+
+fn fr_to_hex(scalar: Fr) -> String {
+    let mut be_bytes = [0u8; 32];
+    scalar
+        .into_repr()
+        .write_be(be_bytes.as_mut())
+        .expect("Write commit bytes");
+    hex::encode(be_bytes)
+}
+
+pub fn sign_update_message(private_key_str: String, old_hash: Fr, new_hash: Fr) -> String {
+    let pk_bytes = hex::decode(private_key_str).unwrap();
+    let pk = H256::from_slice(&pk_bytes);
+
+    let old_hash_str = fr_to_hex(old_hash);
+    let new_hash_str = fr_to_hex(new_hash);
+
+    let message = format!("OldRootHash:{},NewRootHash:{}", old_hash_str, new_hash_str);
+
+    let message_hash: H256 = message.as_bytes().keccak256().into();
+
+    let signature = sign(&pk.into(), &message_hash).unwrap();
+
+    signature.to_string()
+}
