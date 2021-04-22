f73ff9b1bcd36afbf8578bbe13c9cb5c6d7d309e
perekopskiy <perekopskiy@dlit.dp.ua>
2021-04-22 16:53:09 +0300

    Add db transaction to hashes migration

--- core/bin/hashes_migration/src/main.rs
+++ core/bin/hashes_migration/src/main.rs
@@ -1,18 +1,20 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let mut storage = zksync_storage::StorageProcessor::establish_connection().await?;
-    storage
+    let mut transaction = storage.start_transaction().await?;
+    transaction
         .chain()
         .operations_schema()
         .calculate_priority_ops_hashes()
         .await?;
 
-    storage
+    transaction
         .chain()
         .operations_schema()
         .calculate_batch_hashes()
         .await?;
 
+    transaction.commit().await?;
     println!("Tx hashes for priority ops and batches are successfully calculated");
     Ok(())
 }
