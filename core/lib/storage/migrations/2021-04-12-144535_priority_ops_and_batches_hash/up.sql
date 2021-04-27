dd2efe804392072034f57ff5996f56e6830aee44
perekopskiy <perekopskiy@dlit.dp.ua>
2021-04-27 13:30:27 +0300

    Fix migration

--- core/lib/storage/migrations/2021-04-12-144535_priority_ops_and_batches_hash/up.sql
+++ core/lib/storage/migrations/2021-04-12-144535_priority_ops_and_batches_hash/up.sql
@@ -20,19 +20,21 @@ BEGIN
     SELECT MAX(batch_id) FROM executed_transactions 
 	    WHERE batch_id IS NOT NULL 
   	        INTO max_batch_id;
-    FOR i IN 0..max_batch_id
-    LOOP
-        agg_hashes = '';
-        FOR rec in SELECT tx_hash FROM executed_transactions 
-                    WHERE batch_id = i
-                    ORDER BY created_at
+    IF max_batch_id IS NOT NULL THEN
+        FOR i IN 0..max_batch_id
         LOOP
-            agg_hashes = agg_hashes || rec.tx_hash;
+            agg_hashes = '';
+            FOR rec in SELECT tx_hash FROM executed_transactions
+                        WHERE batch_id = i
+                        ORDER BY created_at
+            LOOP
+                agg_hashes = agg_hashes || rec.tx_hash;
+            END LOOP;
+            IF length(agg_hashes) != 0 THEN
+                INSERT INTO txs_batches_hashes (batch_id, batch_hash)
+                VALUES (i, sha256(agg_hashes));
+            END IF;
         END LOOP;
-        IF length(agg_hashes) != 0 THEN
-            INSERT INTO txs_batches_hashes (batch_id, batch_hash)
-            VALUES (i, sha256(agg_hashes));
-        END IF;
-    END LOOP;
+    END IF;
 END;
 $$;
