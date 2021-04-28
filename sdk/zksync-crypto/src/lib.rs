466877342e4e3d0e58aba499684988ae5ee9ef13
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2021-04-29 02:44:46 +0300

    Add orders hashing func to zksync-crypto

--- sdk/zksync-crypto/src/lib.rs
+++ sdk/zksync-crypto/src/lib.rs
@@ -124,6 +124,11 @@ pub fn rescue_hash_tx_msg(msg: &[u8]) -> Vec<u8> {
     utils::rescue_hash_tx_msg(msg)
 }
 
+#[wasm_bindgen(js_name = "rescueHashOrders")]
+pub fn rescue_hash_orders(msg: &[u8]) -> Vec<u8> {
+    utils::rescue_hash_orders(msg)
+}
+
 #[wasm_bindgen]
 /// We use musig Schnorr signature scheme.
 /// It is impossible to restore signer for signature, that is why we provide public key of the signer
