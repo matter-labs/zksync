be4bbf75346a7e0a873fd18194439ced4df8eae5
deniallugo <deniallugo@gmail.com>
2021-05-30 10:20:42 +0300

    Fix error
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- core/lib/state/src/handler/mint_nft.rs
+++ core/lib/state/src/handler/mint_nft.rs
@@ -83,7 +83,7 @@ impl TxHandler<MintNFT> for ZkSyncState {
         let old_balance = creator_account.get_balance(op.tx.fee_token);
         let nonce = creator_account.nonce;
         invariant!(
-            old_balance >= + &op.tx.fee,
+            old_balance >= op.tx.fee,
             MintNFTOpError::InsufficientBalance
         );
         creator_account.sub_balance(op.tx.fee_token, &op.tx.fee);
