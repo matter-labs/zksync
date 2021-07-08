01ec5e27d123e349e22064dc8771433849969452
deniallugo <deniallugo@gmail.com>
2021-07-08 18:33:34 +0300

    Write genesis root to file
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- core/bin/zksync_core/src/state_keeper/mod.rs
+++ core/bin/zksync_core/src/state_keeper/mod.rs
@@ -1,5 +1,8 @@
 use std::collections::{HashMap, VecDeque};
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::time::{Instant, SystemTime, UNIX_EPOCH};
+
 // External uses
 use futures::{
     channel::{mpsc, oneshot},
@@ -591,7 +594,18 @@ impl ZkSyncStateKeeper {
             .await
             .expect("Unable to commit transaction in statekeeper");
         vlog::info!("Genesis block created, state: {}", state.root_hash());
-        println!("CONTRACTS_GENESIS_ROOT=0x{}", ff::to_hex(&root_hash));
+        let genesis_root = format!("CONTRACTS_GENESIS_ROOT=0x{}", ff::to_hex(&root_hash));
+        println!("{}", &genesis_root);
+        // Write updated genesis root to the separate file
+        // Currently it's used by loadtest github action to update deployment configmap.
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open("deployed_contracts.log")
+            .unwrap();
+        if let Err(e) = writeln!(file, "{}", genesis_root) {
+            println!("Couldn't write to file: {}", e);
+        }
         metrics::histogram!("state_keeper.create_genesis_block", start.elapsed());
     }
 
