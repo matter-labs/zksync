eceadb3a42245654c723446033205fdead414306
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2021-07-14 22:23:08 +0300

    Make sure there is no direct import from a subfolder

--- sdk/zksync.js/src/typechain/GovernanceFactory.ts
+++ sdk/zksync.js/src/typechain/GovernanceFactory.ts
@@ -2,9 +2,10 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { Signer } from "ethers";
-import { Provider, TransactionRequest } from "@ethersproject/providers";
-import { Contract, ContractFactory, Overrides } from "@ethersproject/contracts";
+import { Signer, providers, Contract, ContractFactory, Overrides } from "ethers";
+
+type TransactionRequest = providers.TransactionRequest;
+type Provider = providers.Provider;
 
 import type { Governance } from "./Governance";
 
