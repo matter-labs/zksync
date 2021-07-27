bee9a87201f90fb7c7e8dd2dcb25eced3f953d2e
deniallugo <deniallugo@gmail.com>
2021-07-27 20:28:33 +0300

    Change place
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -78,12 +78,12 @@ async function main() {
         await deployer.deployProxiesAndGatekeeper({ gasPrice, nonce: args.nonce });
     }
 
-    if (args.contract === 'ZkSyncNFTFactory' || args.contract == null) {
-        await deployer.deployNFTFactory({ gasPrice, nonce: args.nonce });
-    }
-
     if (args.contract === 'TokenGovernance' || args.contract == null) {
         await deployer.deployTokenGovernance({ gasPrice, nonce: args.nonce });
+    }
+
+    if (args.contract === 'ZkSyncNFTFactory' || args.contract == null) {
+        await deployer.deployNFTFactory({ gasPrice, nonce: args.nonce });
     }
 
     if (args.contract === 'ForcedExit' || args.contract == null) {
