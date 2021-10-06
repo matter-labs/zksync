1022422e76778d8b65b28a619ebfe9b7ed3bbd52
perekopskiy <perekopskiy@dlit.dp.ua>
2021-10-06 17:39:21 +0300

    Update sdk

--- sdk/zksync.js/src/rest-provider.ts
+++ sdk/zksync.js/src/rest-provider.ts
@@ -130,19 +130,21 @@ export class RestProvider extends SyncProvider {
 
     async accountTxsDetailed(
         idOrAddress: number | types.Address,
-        paginationQuery: types.PaginationQuery<string>
+        paginationQuery: types.PaginationQuery<string>,
+        token?: types.TokenLike
     ): Promise<Response<types.Paginated<types.ApiTransaction, string>>> {
-        return await this.get(
-            `${this.address}/accounts/${idOrAddress}/transactions?from=${paginationQuery.from}` +
-                `&limit=${paginationQuery.limit}&direction=${paginationQuery.direction}`
-        );
+        let url = `${this.address}/accounts/${idOrAddress}/transactions?from=${paginationQuery.from}` +
+            `&limit=${paginationQuery.limit}&direction=${paginationQuery.direction}`;
+        if(token) url += `&token=${token}`;
+        return await this.get(url);
     }
 
     async accountTxs(
         idOrAddress: number | types.Address,
-        paginationQuery: types.PaginationQuery<string>
+        paginationQuery: types.PaginationQuery<string>,
+        token?: types.TokenLike
     ): Promise<types.Paginated<types.ApiTransaction, string>> {
-        return this.parseResponse(await this.accountTxsDetailed(idOrAddress, paginationQuery));
+        return this.parseResponse(await this.accountTxsDetailed(idOrAddress, paginationQuery, token));
     }
 
     async accountPendingTxsDetailed(
