232f987fbf47c015fbe31798e6c1d04d627a89ec
perekopskiy <perekopskiy@dlit.dp.ua>
2021-10-18 16:38:17 +0300

    Add hash index

--- core/lib/storage/migrations/2021-10-14-093424_tx-filter-table/down.sql
+++ core/lib/storage/migrations/2021-10-14-093424_tx-filter-table/down.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS tx_filters;
+DROP INDEX IF EXISTS tx_filters_address_idx;
--- core/lib/storage/migrations/2021-10-14-093424_tx-filter-table/up.sql
+++ core/lib/storage/migrations/2021-10-14-093424_tx-filter-table/up.sql
@@ -1,7 +1,8 @@
-CREATE TABLE tx_filters
+CREATE TABLE IF NOT EXISTS tx_filters
 (
     address bytea NOT NULL,
     token INTEGER NOT NULL,
     tx_hash bytea NOT NULL,
     PRIMARY KEY (address, token, tx_hash)
 );
+CREATE INDEX IF NOT EXISTS tx_filters_address_idx ON "tx_filters" USING hash (address);
