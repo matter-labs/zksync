7c839a6f61132ea0e457345c6aa681f181c2fdf0
Igor Aleksanov <popzxc@yandex.ru>
2022-01-26 11:33:55 +0300

    Remove methods implemented in AbstractWallet from Wallet

--- sdk/zksync.js/src/wallet.ts
+++ sdk/zksync.js/src/wallet.ts
@@ -52,14 +52,13 @@ import {
     getToggle2FAMessage
 } from './utils';
 import { Transaction, ETHOperation, submitSignedTransaction } from './operations';
+import { AbstractWallet } from './abstract-wallet';
 
 export { Transaction, ETHOperation, submitSignedTransaction, submitSignedTransactionsBatch } from './operations';
 
 const EthersErrorCode = ErrorCode;
 
-export class Wallet {
-    public provider: SyncProvider;
-
+export class Wallet extends AbstractWallet {
     private constructor(
         public ethSigner: ethers.Signer,
         public ethMessageSigner: EthMessageSigner,
@@ -67,11 +66,8 @@ export class Wallet {
         public signer?: Signer,
         public accountId?: number,
         public ethSignerType?: EthSignerType
-    ) {}
-
-    connect(provider: SyncProvider) {
-        this.provider = provider;
-        return this;
+    ) {
+        super(cachedAddress, accountId);
     }
 
     async processBatchBuilderTransactions(
@@ -153,17 +149,6 @@ export class Wallet {
         };
     }
 
-    async verifyNetworks() {
-        if (this.provider.network != undefined && this.ethSigner.provider != undefined) {
-            const ethNetwork = await this.ethSigner.provider.getNetwork();
-            if (l1ChainId(this.provider.network) !== ethNetwork.chainId) {
-                throw new Error(
-                    `ETH network ${ethNetwork.name} and ZkSync network ${this.provider.network} don't match`
-                );
-            }
-        }
-    }
-
     static async fromEthSigner(
         ethWallet: ethers.Signer,
         provider: SyncProvider,
@@ -239,10 +224,6 @@ export class Wallet {
         });
     }
 
-    batchBuilder(nonce?: Nonce): BatchBuilder {
-        return BatchBuilder.fromWallet(this, nonce);
-    }
-
     async getTransfer(transfer: {
         to: Address;
         token: TokenLike;
@@ -308,24 +289,6 @@ export class Wallet {
         return {
             tx: signedTransferTransaction,
             ethereumSignature
-        };
-    }
-
-    async signRegisterFactory(factoryAddress: Address): Promise<{
-        signature: TxEthSignature;
-        accountId: number;
-        accountAddress: Address;
-    }> {
-        await this.setRequiredAccountIdFromServer('Sign register factory');
-        const signature = await this.ethMessageSigner.ethSignRegisterFactoryMessage(
-            factoryAddress,
-            this.accountId,
-            this.address()
-        );
-        return {
-            signature,
-            accountId: this.accountId,
-            accountAddress: this.address()
         };
     }
 
@@ -947,24 +910,6 @@ export class Wallet {
         return submitSignedTransaction(signedWithdrawTransaction, this.provider, withdraw.fastProcessing);
     }
 
-    async isCorrespondingSigningKeySet(): Promise<boolean> {
-        if (!this.signer) {
-            throw new Error('ZKSync signer is required for current pubkey calculation.');
-        }
-        const currentPubKeyHash = await this.getCurrentPubKeyHash();
-        const signerPubKeyHash = await this.signer.pubKeyHash();
-        return currentPubKeyHash === signerPubKeyHash;
-    }
-
-    async isSigningKeySet(): Promise<boolean> {
-        if (!this.signer) {
-            throw new Error('ZKSync signer is required for current pubkey calculation.');
-        }
-        const currentPubKeyHash = await this.getCurrentPubKeyHash();
-        const zeroPubKeyHash = 'sync:0000000000000000000000000000000000000000';
-        return zeroPubKeyHash !== currentPubKeyHash;
-    }
-
     async getChangePubKey(changePubKey: {
         feeToken: TokenLike;
         fee: BigNumberish;
@@ -997,28 +942,6 @@ export class Wallet {
         });
 
         return changePubKeyTx;
-    }
-
-    async getToggle2FA(enable: boolean, pubKeyHash?: PubKeyHash): Promise<Toggle2FARequest> {
-        const accountId = await this.getAccountId();
-        const timestamp = new Date().getTime();
-        const signature = await this.ethMessageSigner.getEthMessageSignature(
-            getToggle2FAMessage(enable, timestamp, pubKeyHash)
-        );
-
-        return {
-            accountId,
-            signature,
-            timestamp,
-            enable,
-            pubKeyHash
-        };
-    }
-
-    async toggle2FA(enable: boolean, pubKeyHash?: PubKeyHash): Promise<boolean> {
-        await this.setRequiredAccountIdFromServer('Toggle 2FA');
-
-        return await this.provider.toggle2FA(await this.getToggle2FA(enable, pubKeyHash));
     }
 
     async signSetSigningKey(changePubKey: {
@@ -1242,315 +1165,5 @@ export class Wallet {
             stringFee,
             target: forcedExit.target
         });
-    }
-
-    async isOnchainAuthSigningKeySet(nonce: Nonce = 'committed'): Promise<boolean> {
-        const mainZkSyncContract = this.getZkSyncMainContract();
-
-        const numNonce = await this.getNonce(nonce);
-        try {
-            const onchainAuthFact = await mainZkSyncContract.authFacts(this.address(), numNonce);
-            return onchainAuthFact !== '0x0000000000000000000000000000000000000000000000000000000000000000';
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    async onchainAuthSigningKey(
-        nonce: Nonce = 'committed',
-        ethTxOptions?: ethers.providers.TransactionRequest
-    ): Promise<ContractTransaction> {
-        if (!this.signer) {
-            throw new Error('ZKSync signer is required for current pubkey calculation.');
-        }
-
-        const currentPubKeyHash = await this.getCurrentPubKeyHash();
-        const newPubKeyHash = await this.signer.pubKeyHash();
-
-        if (currentPubKeyHash === newPubKeyHash) {
-            throw new Error('Current PubKeyHash is the same as new');
-        }
-
-        const numNonce = await this.getNonce(nonce);
-
-        const mainZkSyncContract = this.getZkSyncMainContract();
-
-        try {
-            return mainZkSyncContract.setAuthPubkeyHash(newPubKeyHash.replace('sync:', '0x'), numNonce, {
-                gasLimit: BigNumber.from('200000'),
-                ...ethTxOptions
-            });
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    async getCurrentPubKeyHash(): Promise<PubKeyHash> {
-        return (await this.provider.getState(this.address())).committed.pubKeyHash;
-    }
-
-    async getNonce(nonce: Nonce = 'committed'): Promise<number> {
-        if (nonce === 'committed') {
-            return (await this.provider.getState(this.address())).committed.nonce;
-        } else if (typeof nonce === 'number') {
-            return nonce;
-        }
-    }
-
-    async getAccountId(): Promise<number | undefined> {
-        return (await this.provider.getState(this.address())).id;
-    }
-
-    address(): Address {
-        return this.cachedAddress;
-    }
-
-    async getAccountState(): Promise<AccountState> {
-        return this.provider.getState(this.address());
-    }
-
-    async getNFT(tokenId: number, type: 'committed' | 'verified' = 'committed'): Promise<NFT> {
-        const accountState = await this.getAccountState();
-        let token: NFT;
-        if (type === 'committed') {
-            token = accountState.committed.nfts[tokenId];
-        } else {
-            token = accountState.verified.nfts[tokenId];
-        }
-        return token;
-    }
-
-    async getBalance(token: TokenLike, type: 'committed' | 'verified' = 'committed'): Promise<BigNumber> {
-        const accountState = await this.getAccountState();
-        const tokenSymbol = this.provider.tokenSet.resolveTokenSymbol(token);
-        let balance: BigNumberish;
-        if (type === 'committed') {
-            balance = accountState.committed.balances[tokenSymbol] || '0';
-        } else {
-            balance = accountState.verified.balances[tokenSymbol] || '0';
-        }
-        return BigNumber.from(balance);
-    }
-
-    async getEthereumBalance(token: TokenLike): Promise<BigNumber> {
-        try {
-            return await getEthereumBalance(this.ethSigner.provider, this.provider, this.cachedAddress, token);
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    async isERC20DepositsApproved(
-        token: TokenLike,
-        erc20ApproveThreshold: BigNumber = ERC20_APPROVE_TRESHOLD
-    ): Promise<boolean> {
-        if (isTokenETH(token)) {
-            throw Error('ETH token does not need approval.');
-        }
-        const tokenAddress = this.provider.tokenSet.resolveTokenAddress(token);
-        const erc20contract = new Contract(tokenAddress, IERC20_INTERFACE, this.ethSigner);
-        try {
-            const currentAllowance = await erc20contract.allowance(
-                this.address(),
-                this.provider.contractAddress.mainContract
-            );
-            return BigNumber.from(currentAllowance).gte(erc20ApproveThreshold);
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    async approveERC20TokenDeposits(
-        token: TokenLike,
-        max_erc20_approve_amount: BigNumber = MAX_ERC20_APPROVE_AMOUNT
-    ): Promise<ContractTransaction> {
-        if (isTokenETH(token)) {
-            throw Error('ETH token does not need approval.');
-        }
-        const tokenAddress = this.provider.tokenSet.resolveTokenAddress(token);
-        const erc20contract = new Contract(tokenAddress, IERC20_INTERFACE, this.ethSigner);
-
-        try {
-            return erc20contract.approve(this.provider.contractAddress.mainContract, max_erc20_approve_amount);
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    async depositToSyncFromEthereum(deposit: {
-        depositTo: Address;
-        token: TokenLike;
-        amount: BigNumberish;
-        ethTxOptions?: ethers.providers.TransactionRequest;
-        approveDepositAmountForERC20?: boolean;
-    }): Promise<ETHOperation> {
-        const gasPrice = await this.ethSigner.provider.getGasPrice();
-
-        const mainZkSyncContract = this.getZkSyncMainContract();
-
-        let ethTransaction;
-
-        if (isTokenETH(deposit.token)) {
-            try {
-                ethTransaction = await mainZkSyncContract.depositETH(deposit.depositTo, {
-                    value: BigNumber.from(deposit.amount),
-                    gasLimit: BigNumber.from(ETH_RECOMMENDED_DEPOSIT_GAS_LIMIT),
-                    gasPrice,
-                    ...deposit.ethTxOptions
-                });
-            } catch (e) {
-                this.modifyEthersError(e);
-            }
-        } else {
-            const tokenAddress = this.provider.tokenSet.resolveTokenAddress(deposit.token);
-            // ERC20 token deposit
-            const erc20contract = new Contract(tokenAddress, IERC20_INTERFACE, this.ethSigner);
-            let nonce: number;
-            if (deposit.approveDepositAmountForERC20) {
-                try {
-                    const approveTx = await erc20contract.approve(
-                        this.provider.contractAddress.mainContract,
-                        deposit.amount
-                    );
-                    nonce = approveTx.nonce + 1;
-                } catch (e) {
-                    this.modifyEthersError(e);
-                }
-            }
-            const args = [
-                tokenAddress,
-                deposit.amount,
-                deposit.depositTo,
-                {
-                    nonce,
-                    gasPrice,
-                    ...deposit.ethTxOptions
-                } as ethers.providers.TransactionRequest
-            ];
-
-            // We set gas limit only if user does not set it using ethTxOptions.
-            const txRequest = args[args.length - 1] as ethers.providers.TransactionRequest;
-            if (txRequest.gasLimit == null) {
-                try {
-                    const gasEstimate = await mainZkSyncContract.estimateGas.depositERC20(...args).then(
-                        (estimate) => estimate,
-                        () => BigNumber.from('0')
-                    );
-                    const isMainnet = (await this.ethSigner.getChainId()) == 1;
-                    let recommendedGasLimit =
-                        isMainnet && ERC20_DEPOSIT_GAS_LIMIT[tokenAddress]
-                            ? BigNumber.from(ERC20_DEPOSIT_GAS_LIMIT[tokenAddress])
-                            : ERC20_RECOMMENDED_DEPOSIT_GAS_LIMIT;
-                    txRequest.gasLimit = gasEstimate.gte(recommendedGasLimit) ? gasEstimate : recommendedGasLimit;
-                    args[args.length - 1] = txRequest;
-                } catch (e) {
-                    this.modifyEthersError(e);
-                }
-            }
-
-            try {
-                ethTransaction = await mainZkSyncContract.depositERC20(...args);
-            } catch (e) {
-                this.modifyEthersError(e);
-            }
-        }
-
-        return new ETHOperation(ethTransaction, this.provider);
-    }
-
-    async resolveAccountId(): Promise<number> {
-        if (this.accountId !== undefined) {
-            return this.accountId;
-        } else {
-            const accountState = await this.getAccountState();
-            if (!accountState.id) {
-                throw new Error("Can't resolve account id from the zkSync node");
-            }
-            return accountState.id;
-        }
-    }
-
-    async emergencyWithdraw(withdraw: {
-        token: TokenLike;
-        accountId?: number;
-        ethTxOptions?: ethers.providers.TransactionRequest;
-    }): Promise<ETHOperation> {
-        const gasPrice = await this.ethSigner.provider.getGasPrice();
-
-        let accountId: number = withdraw.accountId != null ? withdraw.accountId : await this.resolveAccountId();
-
-        const mainZkSyncContract = this.getZkSyncMainContract();
-
-        const tokenAddress = this.provider.tokenSet.resolveTokenAddress(withdraw.token);
-        try {
-            const ethTransaction = await mainZkSyncContract.requestFullExit(accountId, tokenAddress, {
-                gasLimit: BigNumber.from('500000'),
-                gasPrice,
-                ...withdraw.ethTxOptions
-            });
-            return new ETHOperation(ethTransaction, this.provider);
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    async emergencyWithdrawNFT(withdrawNFT: {
-        tokenId: number;
-        accountId?: number;
-        ethTxOptions?: ethers.providers.TransactionRequest;
-    }): Promise<ETHOperation> {
-        const gasPrice = await this.ethSigner.provider.getGasPrice();
-
-        let accountId: number = withdrawNFT.accountId != null ? withdrawNFT.accountId : await this.resolveAccountId();
-
-        const mainZkSyncContract = this.getZkSyncMainContract();
-
-        try {
-            const ethTransaction = await mainZkSyncContract.requestFullExitNFT(accountId, withdrawNFT.tokenId, {
-                gasLimit: BigNumber.from('500000'),
-                gasPrice,
-                ...withdrawNFT.ethTxOptions
-            });
-            return new ETHOperation(ethTransaction, this.provider);
-        } catch (e) {
-            this.modifyEthersError(e);
-        }
-    }
-
-    getZkSyncMainContract() {
-        return new ethers.Contract(
-            this.provider.contractAddress.mainContract,
-            SYNC_MAIN_CONTRACT_INTERFACE,
-            this.ethSigner
-        );
-    }
-
-    private modifyEthersError(error: any): never {
-        if (this.ethSigner instanceof ethers.providers.JsonRpcSigner) {
-            // List of errors that can be caused by user's actions, which have to be forwarded as-is.
-            const correct_errors = [
-                EthersErrorCode.NONCE_EXPIRED,
-                EthersErrorCode.INSUFFICIENT_FUNDS,
-                EthersErrorCode.REPLACEMENT_UNDERPRICED,
-                EthersErrorCode.UNPREDICTABLE_GAS_LIMIT
-            ];
-            if (!correct_errors.includes(error.code)) {
-                // This is an error which we don't expect
-                error.message = `Ethereum smart wallet JSON RPC server returned the following error while executing an operation: "${error.message}". Please contact your smart wallet support for help.`;
-            }
-        }
-
-        throw error;
-    }
-
-    private async setRequiredAccountIdFromServer(actionName: string) {
-        if (this.accountId === undefined) {
-            const accountIdFromServer = await this.getAccountId();
-            if (accountIdFromServer == null) {
-                throw new Error(`Failed to ${actionName}: Account does not exist in the zkSync network`);
-            } else {
-                this.accountId = accountIdFromServer;
-            }
-        }
     }
 }
