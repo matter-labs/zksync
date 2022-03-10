076419e20406ec30553c0ca1a7c7060da1721e27
deniallugo <deniallugo@gmail.com>
2022-03-10 10:21:00 +0300

    Add indexes to priority ops and nfts
    Signed-off-by: deniallugo <deniallugo@gmail.com>

---
+++ core/lib/storage/migrations/2022-03-10-071338_new_indexes/up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX IF NOT EXISTS executed_priority_operations_tx_hash_idx ON  executed_priority_operations (tx_hash);
+CREATE INDEX IF NOT EXISTS mint_nft_updates_account_id_block_number ON  mint_nft_updates (creator_account_id, block_number);
+CREATE INDEX IF NOT EXISTS nft_creator_id_idx ON  nft (creator_account_id);
