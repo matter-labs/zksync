671c33bf75b3da8018a639446985ac58dad73aa4
deniallugo <deniallugo@gmail.com>
2022-03-10 10:56:41 +0300

    Create index for aggregation_opertaions
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- core/lib/storage/migrations/2022-03-10-071338_new_indexes/down.sql
+++ core/lib/storage/migrations/2022-03-10-071338_new_indexes/down.sql
@@ -1,3 +1,4 @@
 DROP INDEX IF EXISTS executed_priority_operations_tx_hash_idx;
 DROP INDEX IF EXISTS mint_nft_updates_account_id_block_number;
 DROP INDEX IF EXISTS nft_creator_id_idx;
+DROP INDEX IF EXISTS aggregate_operations_type_idx;
--- core/lib/storage/migrations/2022-03-10-071338_new_indexes/up.sql
+++ core/lib/storage/migrations/2022-03-10-071338_new_indexes/up.sql
@@ -1,3 +1,4 @@
 CREATE INDEX IF NOT EXISTS executed_priority_operations_tx_hash_idx ON  executed_priority_operations (tx_hash);
 CREATE INDEX IF NOT EXISTS mint_nft_updates_account_id_block_number ON  mint_nft_updates (creator_account_id, block_number);
 CREATE INDEX IF NOT EXISTS nft_creator_id_idx ON  nft (creator_account_id);
+CREATE INDEX IF NOT EXISTS aggregate_operations_type_idx ON aggregate_operations (action_type);
