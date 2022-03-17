7ab7f5af85810d6b1d64441e8cb2a968cc0464de
deniallugo <deniallugo@gmail.com>
2022-03-17 09:21:45 +0300

    Check core status regurally
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- core/tests/ts-tests/api-types/status.ts
+++ core/tests/ts-tests/api-types/status.ts
@@ -5,11 +5,11 @@ export type Interface = {
     total_transactions: number;
     outstanding_txs: number;
     mempool_size: number;
-    core_status: null | CoreStatus
+    core_status: null | CoreStatus;
 };
 
 export type CoreStatus = {
     main_database_status: boolean;
     replica_database_status: boolean;
     eth_status: boolean;
-}
+};
