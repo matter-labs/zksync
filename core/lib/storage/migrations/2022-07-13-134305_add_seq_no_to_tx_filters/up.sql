c405f64bc34c34dbe797bcc13c08a3400f54d7df
deniallugo <deniallugo@gmail.com>
2022-07-15 07:23:24 +0200

    Fix migration
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- core/lib/storage/migrations/2022-07-13-134305_add_seq_no_to_tx_filters/up.sql
+++ core/lib/storage/migrations/2022-07-13-134305_add_seq_no_to_tx_filters/up.sql
@@ -1,13 +1,13 @@
 ALTER TABLE tx_filters ADD COLUMN sequence_number BIGINT;
 ALTER TABLE tx_filters ADD COLUMN is_priority bool;
 
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS uq_executed_transactions_sequence_number ON public.executed_transactions USING btree (sequence_number);
+CREATE UNIQUE INDEX IF NOT EXISTS uq_executed_transactions_sequence_number ON public.executed_transactions USING btree (sequence_number);
 DROP INDEX IF EXISTS executed_transactions_sequence_number;
 DROP INDEX IF EXISTS ix_executed_transactions_tx_hash_sequence_number;
 
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS uq_executed_priority_operations_sequence_number ON public.executed_priority_operations USING btree (sequence_number);
+CREATE UNIQUE INDEX IF NOT EXISTS uq_executed_priority_operations_sequence_number ON public.executed_priority_operations USING btree (sequence_number);
 DROP INDEX IF EXISTS executed_priority_operations_sequence_number;
 DROP INDEX IF EXISTS ix_executed_priority_operations_tx_hash_sequence_number;
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ix_tx_filters_address_sequence_number ON public.tx_filters USING btree (address, sequence_number) include(is_priority);
+CREATE INDEX IF NOT EXISTS ix_tx_filters_address_sequence_number ON public.tx_filters USING btree (address, sequence_number) include(is_priority);
 DROP INDEX IF EXISTS ix_tx_filters_address_tx_hash;
