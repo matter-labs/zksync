42d45e06eaf4f700113bdf24e7265967bef9b452
deniallugo <deniallugo@gmail.com>
2022-08-16 19:04:02 +0200

    Fix deploy
    Signed-off-by: deniallugo <deniallugo@gmail.com>

--- contracts/scripts/deploy-erc20.ts
+++ contracts/scripts/deploy-erc20.ts
@@ -13,10 +13,7 @@ const testConfigPath = path.join(process.env.ZKSYNC_HOME as string, `etc/test_co
 const ethTestConfig = JSON.parse(fs.readFileSync(`${testConfigPath}/eth.json`, { encoding: 'utf-8' }));
 
 const provider = web3Provider();
-const wallet = Wallet.fromMnemonic(
-    process.env.MNEMONIC ? process.env.MNEMONIC : ethTestConfig.mnemonic,
-    "m/44'/60'/0'/0/1"
-).connect(provider);
+const wallet = Wallet.fromMnemonic(ethTestConfig.mnemonic, "m/44'/60'/0'/0/1").connect(provider);
 
 type Token = {
     address: string | null;
@@ -39,10 +36,9 @@ async function deployToken(token: TokenDescription): Promise<Token> {
     );
     await erc20.mint(wallet.address, parseEther('3000000000'));
     for (let i = 0; i < 10; ++i) {
-        const testWallet = Wallet.fromMnemonic(
-            process.env.MNEMONIC ? process.env.MNEMONIC : ethTestConfig.mnemonic,
-            "m/44'/60'/0'/0/" + i
-        ).connect(provider);
+        const testWallet = Wallet.fromMnemonic(ethTestConfig.test_mnemonic as string, "m/44'/60'/0'/0/" + i).connect(
+            provider
+        );
         await erc20.mint(testWallet.address, parseEther('3000000000'));
     }
     token.address = erc20.address;
