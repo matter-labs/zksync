0a388849b0d25a91f19571aacd7e558cdd779110
Danil <deniallugo@gmail.com>
2023-01-20 11:24:32 +0000

    Fix down migration
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/storage/migrations/2023-01-18-164812_txs_count/down.sql
+++ core/lib/storage/migrations/2023-01-18-164812_txs_count/down.sql
@@ -1,4 +1,8 @@
 DROP TABLE txs_count;
 
-DROP TRIGGER update_txs_count_for_address_tr ON tx_filters;
-DROP FUNCTION update_txs_count_for_address;
+DROP TRIGGER increase_txs_count_for_address_tr ON tx_filters;
+DROP TRIGGER decrease_txs_count_for_address_tr ON tx_filters;
+
+
+DROP FUNCTION increase_txs_count_for_address;
+DROP FUNCTION decrease_txs_count_for_address;
