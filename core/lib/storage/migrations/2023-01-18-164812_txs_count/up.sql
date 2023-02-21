968cb5a676bd6317423deb6c13d3011bd20d585a
Danil <deniallugo@gmail.com>
2023-02-21 20:47:49 +0000

    Make an additional migration
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/storage/migrations/2023-01-18-164812_txs_count/up.sql
+++ core/lib/storage/migrations/2023-01-18-164812_txs_count/up.sql
@@ -10,33 +10,43 @@ BEGIN
     IF (TG_OP = 'DELETE') THEN
         -- Postgresql doesn't support unique indexes for nullable fields, so we have to use
         -- artificial token which means no token
-        UPDATE txs_count SET count = txs_count.count -
-                                     CASE WHEN (SELECT count(*) = 1  FROM tx_filters WHERE address = OLD.address AND tx_hash = OLD.tx_hash) THEN 1 ELSE 0 END
-        WHERE address=OLD.address AND token = -1;
+        UPDATE txs_count
+        SET count = txs_count.count - CASE WHEN NOT EXISTS(SELECT 1 FROM tx_filters WHERE address = OLD.address AND tx_hash = OLD.tx_hash)
+            THEN 1 ELSE 0 END
+        WHERE address=OLD.address AND token=-1;
 
-        UPDATE txs_count SET count = txs_count.count -
-                                     CASE WHEN (SELECT count(*) = 1 FROM tx_filters WHERE address = OLD.address AND tx_hash = OLD.tx_hash AND token = OLD.token) THEN 1 ELSE 0 END
-        WHERE address=OLD.address AND token=OLD.token;
-
+       UPDATE txs_count
+       SET count = txs_count.count - CASE WHEN NOT EXISTS(SELECT 1 FROM tx_filters WHERE address = OLD.address AND tx_hash = OLD.tx_hash AND token = OLD.token)
+           THEN 1 ELSE 0 END
+       WHERE address=OLD.address AND token=OLD.token;
     END IF;
-    RETURN OLD;
+    RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION increase_txs_count_for_address() RETURNS TRIGGER AS $$
+DECLARE
+    _dummy bigint;
 BEGIN
+    /*
+    REQUIRED IF DEADLOCK WILL APPEAR
+    SELECT count(*) INTO _dummy
+    FROM txs_count
+    WHERE address = NEW.address AND ( token IS NULL OR token = NEW.token )
+    FOR UPDATE;
+    */
+
     IF (TG_OP = 'INSERT') THEN
-        INSERT INTO txs_count (address, token, count) VALUES (NEW.address, -1       , 0) ON CONFLICT (address, token) DO NOTHING;
-        INSERT INTO txs_count (address, token, count) VALUES (NEW.address, NEW.token, 0) ON CONFLICT (address, token) DO NOTHING;
 
-        UPDATE txs_count SET count = txs_count.count +
-                                     CASE WHEN EXISTS(SELECT 1 FROM tx_filters WHERE address = NEW.address AND tx_hash = NEW.tx_hash FOR UPDATE) THEN 0 ELSE 1 END
-        WHERE address = NEW.address AND token = -1;
+        -- Postgresql doesn't support unique indexes for nullable fields, so we have to use
+        -- artificial token which means no token
+        INSERT INTO txs_count (address, token, count) VALUES (NEW.address, -1, 1)
+        ON CONFLICT (address, token) DO UPDATE
+            SET count = txs_count.count + CASE WHEN EXISTS(SELECT 1 FROM tx_filters WHERE address = NEW.address AND tx_hash = NEW.tx_hash) THEN 0 ELSE 1 END;
 
-        UPDATE txs_count SET count = txs_count.count +
-                                     CASE WHEN EXISTS(SELECT 1 FROM tx_filters WHERE address = NEW.address AND tx_hash = NEW.tx_hash AND token = NEW.token) THEN 0 ELSE 1 END
-        WHERE address = NEW.address AND token = NEW.token;
-
+        INSERT INTO txs_count (address, token, count) VALUES (NEW.address, NEW.token, 1)
+        ON CONFLICT (address, token) DO UPDATE
+            SET count = txs_count.count + CASE WHEN EXISTS(SELECT 1 FROM tx_filters WHERE address = NEW.address AND tx_hash = NEW.tx_hash AND token = NEW.token) THEN 0 ELSE 1 END;
     END IF;
     RETURN NEW;
 END;
@@ -45,5 +55,5 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER increase_txs_count_for_address_tr BEFORE INSERT ON tx_filters
     FOR EACH ROW  EXECUTE FUNCTION increase_txs_count_for_address();
 
-CREATE TRIGGER decrease_txs_count_for_address_tr BEFORE DELETE ON tx_filters
+CREATE TRIGGER decrease_txs_count_for_address_tr AFTER DELETE ON tx_filters
     FOR EACH ROW  EXECUTE FUNCTION decrease_txs_count_for_address();
