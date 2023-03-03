212db765e77a4ca40a714ec0e53600ca5a2f2a6e
Danil <deniallugo@gmail.com>
2023-03-03 19:18:43 +0000

    Remove problematic code from migration (#2336)
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/storage/migrations/2022-01-24-155551_serial_id_for_executed_tx/up.sql
+++ core/lib/storage/migrations/2022-01-24-155551_serial_id_for_executed_tx/up.sql
@@ -7,13 +7,5 @@ ALTER TABLE executed_priority_operations
 
 CREATE SEQUENCE executed_operations_seq_number;
 
--- -- After this manipulations the new transactions will be added with correct seq numbers
-BEGIN;
-
--- The number of transactions in this table is guaranteed to exceed the number of transactions in both executed_transactions and executed_priority_operations.
-LOCK TABLE tx_filters IN SHARE MODE;
-SELECT setval('executed_operations_seq_number', (SELECT count(*) FROM tx_filters )+1);
 ALTER TABLE executed_transactions ALTER COLUMN sequence_number SET DEFAULT nextval('executed_operations_seq_number');
 ALTER TABLE executed_priority_operations ALTER COLUMN sequence_number SET DEFAULT nextval('executed_operations_seq_number');
-
-COMMIT;
